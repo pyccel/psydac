3624d2f1a8128b8f457f8c7fe32b17e42f50d1d1
Yaman Güçlü <yaman.guclu@gmail.com>
2018-03-08 16:54:14 +0100

    [linalg] Add abstract base classes 'Vector' and 'LinearOperator'.

--- spl/linalg/basic.py
+++ spl/linalg/basic.py
@@ -1,3 +1,65 @@
 # coding: utf-8
+#
+# Copyright 2018 Yaman Güçlü
 
+from abc   import ABCMeta, abstractmethod
+from numpy import ndarray
 
+__all__ = ['Vector', 'LinearOperator']
+
+#===============================================================================
+class Vector( metaclass=ABCMeta ):
+    """
+    Generic element of a (normed) vector space V.
+
+    """
+    @property
+    @abstractmethod
+    def shape( self ):
+        pass
+
+    @abstractmethod
+    def dot( self, v ):
+        pass
+
+    @abstractmethod
+    def copy( self ):
+        pass
+
+    @abstractmethod
+    def __mul__( self, a ):
+        pass
+
+    @abstractmethod
+    def __rmul__( self, a ):
+        pass
+
+    @abstractmethod
+    def __add__( self, v ):
+        pass
+
+    @abstractmethod
+    def __sub__( self, v ):
+        pass
+
+Vector.register( ndarray )
+
+#===============================================================================
+class LinearOperator( metaclass=ABCMeta ):
+    """
+    Linear operator acting on a (normed) vector space V.
+
+    """
+    @property
+    @abstractmethod
+    def shape( self ):
+        pass
+
+    @abstractmethod
+    def dot( self, v, out=None ):
+        pass
+
+LinearOperator.register( ndarray )
+
+#===============================================================================
+del ABCMeta, abstractmethod, ndarray
