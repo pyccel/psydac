71ecce4b600e9a01fadea74f300e092a4c058bbc
Yaman Güçlü <yaman.guclu@gmail.com>
2018-03-20 18:01:11 +0100

    [ddm] MPICart2D: make all attributes private, access through properties.

--- spl/ddm/mpi_cart.py
+++ spl/ddm/mpi_cart.py
@@ -10,87 +10,87 @@ class MPICart2D():
         assert( len( npts ) == len( pads ) == len( periods ) )
 
         # ...
-        self.ndims = len( npts )
+        self._ndims = len( npts )
         # ...
 
         # ...
-        self.pads    = pads
-        self.periods = periods
-        self.reorder = reorder
+        self._pads    = pads
+        self._periods = periods
+        self._reorder = reorder
         # ...
 
         # ...
-        self.comm = comm
-        self.size = comm.Get_size()
-        self.rank = comm.Get_rank()
+        self._comm = comm
+        self._size = comm.Get_size()
+        self._rank = comm.Get_rank()
         # ...
 
         # ...
         # Know the number of processes along each direction
-        self.dims = MPI.Compute_dims( self.size, self.ndims )
+        self._dims = MPI.Compute_dims( self._size, self._ndims )
         # ...
 
         # ...
         # Create a 2D MPI cart
-        self.comm_cart = comm.Create_cart(
-            dims    = self.dims,
-            periods = self.periods,
-            reorder = self.reorder
+        self._comm_cart = comm.Create_cart(
+            dims    = self._dims,
+            periods = self._periods,
+            reorder = self._reorder
         )
 
         # Know my coordinates in the topology
-        self.rank_in_topo = self.comm_cart.Get_rank()
-        self.coords       = self.comm_cart.Get_coords( rank = self.rank_in_topo )
+        self._rank_in_topo = self._comm_cart.Get_rank()
+        self._coords       = self._comm_cart.Get_coords( rank=self._rank_in_topo )
 
         # Start/end values of global indices (without ghost regions)
-        self.starts = tuple( ( c   *n)//d   for n,d,c in zip( npts, self.dims, self.coords ) )
-        self.ends   = tuple( ((c+1)*n)//d-1 for n,d,c in zip( npts, self.dims, self.coords ) )
+        self._starts = tuple( ( c   *n)//d   for n,d,c in zip( npts, self._dims, self._coords ) )
+        self._ends   = tuple( ((c+1)*n)//d-1 for n,d,c in zip( npts, self._dims, self._coords ) )
 
         # List of 1D global indices (without ghost regions)
-        self.grids = tuple( range(s,e+1) for s,e in zip( self.starts, self.ends ) )
+        self._grids = tuple( range(s,e+1) for s,e in zip( self._starts, self._ends ) )
 
         # N-dimensional global indices (without ghost regions)
-        self.indices = product( *self.grids )
+        self._indices = product( *self._grids )
 
         # Compute shape of local arrays in topology (with ghost regions)
-        self.shape = tuple( e-s+1+2*p for s,e,p in zip( self.starts, self.ends, self.pads ) )
+        self._shape = tuple( e-s+1+2*p for s,e,p in zip( self._starts, self._ends, self._pads ) )
 
         # Extended grids with ghost regions
-        self.extended_grids = tuple( range(s-p,e+p+1) for s,e,p in zip( self.starts, self.ends, self.pads ) )
+        self._extended_grids = tuple( range(s-p,e+p+1) for s,e,p in zip( self._starts, self._ends, self._pads ) )
 
         # N-dimensional global indices with ghost regions
-        self.extended_indices = product( *self.extended_grids )
+        self._extended_indices = product( *self._extended_grids )
 
         # Create (N-1)-dimensional communicators within the Cartesian topology
-        self.subcomm = [None]*self.ndims
-        for i in range(self.ndims):
-            remain_dims     = [i==j for j in range( self.ndims )]
-            self.subcomm[i] = self.comm_cart.Sub( remain_dims )
+        self._subcomm = [None]*self._ndims
+        for i in range(self._ndims):
+            remain_dims     = [i==j for j in range( self._ndims )]
+            self._subcomm[i] = self._comm_cart.Sub( remain_dims )
 
         # Buffers for communicating with neighbors
-        self.send_buffers = {}
-        self.recv_buffers = {}
-        zero_shift = tuple( [0]*self.ndims )
-        for shift in product( [-1,0,1], repeat=self.ndims ):
+        self._send_buffers = {}
+        self._recv_buffers = {}
+        zero_shift = tuple( [0]*self._ndims )
+        for shift in product( [-1,0,1], repeat=self._ndims ):
 
             if shift == zero_shift:
                 continue
 
             info = self.get_sendrecv_info( shift )
-            self.send_buffers[shift] = np.zeros( info['buf_shape'] )
-            self.recv_buffers[shift] = np.zeros( info['buf_shape'] )
+            self._send_buffers[shift] = np.zeros( info['buf_shape'] )
+            self._recv_buffers[shift] = np.zeros( info['buf_shape'] )
 
         # DEBUG >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-        if self.rank == 0:
+        if self._rank == 0:
             print( "", flush=True )
 
-        zero_shift = tuple( [0]*self.ndims )
-        for k in range( self.size ):
-            if self.rank == k:
+        zero_shift = tuple( [0]*self._ndims )
+        for k in range( self._size ):
+            if self._rank == k:
                 print( "="*40 )
-                print( "RANK = {}".format( self.rank ) )
+                print( "RANK = {}".format( self._rank ) )
                 print( "="*40 )
-                for shift in product( [-1,0,1], repeat=self.ndims ):
+                for shift in product( [-1,0,1], repeat=self._ndims ):
                     if shift == zero_shift:
                         continue
                     print( "sendrecv_info: shift = {}".format( shift ) )
@@ -98,33 +98,50 @@ class MPICart2D():
                     print( "" )
                 print( "", flush=True )
 
-            self.comm.Barrier()
+            self._comm.Barrier()
 
-        if self.rank == 0:
+        if self._rank == 0:
             print( "", flush=True )
         # <<<<<<<<<<<<<<<<<<<<<<<<<< END DEBUG
 
     #---------------------------------------------------------------------------
+    @property
+    def starts( self ):
+        return self._starts
+
+    @property
+    def ends( self ):
+        return self._ends
+
+    @property
+    def pads( self ):
+        return self._pads
+
+    @property
+    def coords( self ):
+        return self._coords
+
+    #---------------------------------------------------------------------------
     def coords_exist( self, coords ):
 
-        return all( P or (0 <= c < d) for P,c,d in zip( self.periods, coords, self.dims ) )
+        return all( P or (0 <= c < d) for P,c,d in zip( self._periods, coords, self._dims ) )
 
     #---------------------------------------------------------------------------
     def get_sendrecv_info( self, shift ):
-        assert( len( shift ) == self.ndims )
+        assert( len( shift ) == self._ndims )
 
         # Compute coordinates of destination and source
-        coords_dest   = [c+h for c,h in zip( self.coords, shift )]
-        coords_source = [c-h for c,h in zip( self.coords, shift )]
+        coords_dest   = [c+h for c,h in zip( self._coords, shift )]
+        coords_source = [c-h for c,h in zip( self._coords, shift )]
 
         # Convert coordinates to rank, taking care of non-periodic dimensions
         if self.coords_exist( coords_dest ):
-            rank_dest = self.comm_cart.Get_cart_rank( coords_dest )
+            rank_dest = self._comm_cart.Get_cart_rank( coords_dest )
         else:
             rank_dest = MPI.PROC_NULL
 
         if self.coords_exist( coords_source ):
-            rank_source = self.comm_cart.Get_cart_rank( coords_source )
+            rank_source = self._comm_cart.Get_cart_rank( coords_source )
         else:
             rank_source = MPI.PROC_NULL
 
@@ -132,7 +149,7 @@ class MPICart2D():
         indx_recv = []
         indx_send = []
         buf_shape = []
-        for s,e,p,h in zip( self.starts, self.ends, self.pads, shift ):
+        for s,e,p,h in zip( self._starts, self._ends, self._pads, shift ):
             if h == 0:
                 slice_recv = slice( p, -p )
                 slice_send = slice( p, -p )
@@ -163,37 +180,37 @@ class MPICart2D():
     def __del__(self):
 
         # Destroy sub-communicators
-        for s in self.subcomm:
+        for s in self._subcomm:
             s.Free()
 
         # Destroy Cartesian communicator
-        self.comm_cart.Free()
+        self._comm_cart.Free()
 
     #---------------------------------------------------------------------------
     def communicate(self, u):
 
-        assert( self.shape == u.shape )
+        assert( self._shape == u.shape )
 
         tag    = 1435
         status = MPI.Status()
 
         # ... Communication
-        zero_shift = tuple( [0]*self.ndims )
-        for shift in product( [-1,0,1], repeat=self.ndims ):
+        zero_shift = tuple( [0]*self._ndims )
+        for shift in product( [-1,0,1], repeat=self._ndims ):
 
             if shift == zero_shift:
                 continue
 
             info = self.get_sendrecv_info( shift )
 
-            sendbuf = self.send_buffers[shift]
-            recvbuf = self.recv_buffers[shift]
+            sendbuf = self._send_buffers[shift]
+            recvbuf = self._recv_buffers[shift]
 
             # Copy data from u to contiguous send buffer
             sendbuf[...] = u[info['indx_send']]
 
             # Send and receive data
-            self.comm_cart.Sendrecv(
+            self._comm_cart.Sendrecv(
                 sendbuf = sendbuf,
                 dest    = info['rank_dest'],
                 sendtag = tag,
@@ -214,6 +231,6 @@ class MPICart2D():
     #---------------------------------------------------------------------------
     def reduce(self, x):
 
-        global_x = comm_cart.allreduce( x, op=MPI.SUM )
+        global_x = self._comm_cart.allreduce( x, op=MPI.SUM )
 
         print(global_x, x)
