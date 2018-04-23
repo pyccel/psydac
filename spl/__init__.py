eddd101774a3eb3da5fee4dd6ea88dfad5c4e982
Jalal Lakhlili <jalal.lakhlili@ipp.mpg.de>
2018-04-23 15:03:35 +0200

    fix merging conflict

--- spl/__init__.py
+++ spl/__init__.py
@@ -1,14 +1,12 @@
 # -*- coding: UTF-8 -*-
 __version__ = "0.1"
-__all__     = ['core','ddm','feec','linalg','utilities']
+__all__     = ['core','ddm','feec', 'fem', 'linalg','utilities']
 
-<<<<<<< HEAD
-from .core      import *
 
 from spl import core
 from spl import ddm
 from spl import feec
-from spl import linalg
 from spl import fem
+from spl import linalg
 #from . import mapping
 from spl import utilities
