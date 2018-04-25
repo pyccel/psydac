a3c2a7b12648924c5f763724e2e3dbc53a4a4cfb
Yaman Güçlü <yaman.guclu@gmail.com>
2018-04-25 12:42:46 +0200

    [linalg] Extensively test StencilMatrix in serial mode.

--- spl/linalg/tests/test_stencil_matrix.py
+++ spl/linalg/tests/test_stencil_matrix.py
@@ -8,12 +8,28 @@ from spl.linalg.stencil import StencilVectorSpace, StencilVector, StencilMatrix
 #===============================================================================
 # SERIAL TESTS
 #===============================================================================
+@pytest.mark.parametrize( 's1', [0,1] )
+@pytest.mark.parametrize( 's2', [0,1] )
+@pytest.mark.parametrize( 'e1', [7,15] )
+@pytest.mark.parametrize( 'e2', [8,12] )
+@pytest.mark.parametrize( 'p1', [1,2,3] )
+@pytest.mark.parametrize( 'p2', [1,2,3] )
+
+def test_stencil_matrix_2d_serial_shape( s1,s2, e1,e2, p1,p2 ):
+
+    V = StencilVectorSpace( [s1,s2], [e1,e2], [p1,p2] )
+    M = StencilMatrix( V, V )
+
+    assert M._data.shape == (1+e1-s1, 1+e2-s2, 1+2*p1, 1+2*p2)
+    assert M.shape == ((1+e1-s1)*(1+e2-s2),(1+e1-s1)*(1+e2-s2))
+
+#===============================================================================
 @pytest.mark.parametrize( 'n1', [7,15] )
 @pytest.mark.parametrize( 'n2', [8,12] )
 @pytest.mark.parametrize( 'p1', [1,2,3] )
 @pytest.mark.parametrize( 'p2', [1,2,3] )
 
-def test_stencil_matrix_2d_serial_data( n1, n2, p1, p2 ):
+def test_stencil_matrix_2d_serial_toarray( n1,n2, p1,p2 ):
 
     # Select non-zero values based on diagonal index
     nonzero_values = dict()
@@ -21,19 +37,17 @@ def test_stencil_matrix_2d_serial_data( n1, n2, p1, p2 ):
         for k2 in range(-p2,p2+1):
             nonzero_values[k1,k2] = 10*k1 + k2
 
+    # Create vector space and stencil matrix
     V = StencilVectorSpace( [0,0], [n1,n2], [p1,p2] )
     M = StencilMatrix( V, V )
 
-    # Fill stencil matrix values
+    # Fill in stencil matrix values
     for k1 in range(-p1,p1+1):
         for k2 in range(-p2,p2+1):
             M[:,:,k1,k2] = nonzero_values[k1,k2]
 
+    # Convert stencil matrix to 2D array
     Ma = M.toarray()
-
-    print (">>> M shape: ", M._data.shape)
-    print (">>> M = ")
-    print (Ma)
 
     # Construct exact matrix by hand
     A = np.zeros( M.shape )
@@ -47,43 +61,50 @@ def test_stencil_matrix_2d_serial_data( n1, n2, p1, p2 ):
                     j  = j1*(n2+1) + j2
                     A[i,j] = nonzero_values[k1,k2]
 
-    assert M._data.shape == (1+n1, 1+n2, 1+2*p1, 1+2*p2)
-    assert M.shape == ((1+n1)*(1+n2),(1+n1)*(1+n2))
-    assert M.shape == Ma.shape
+    # Check shape and data in 2D array
+    assert Ma.shape == M.shape
     assert np.all( Ma == A )
 
 #===============================================================================
-def test_stencil_matrix_2d_serial_dot():
+@pytest.mark.parametrize( 'n1', [7,15] )
+@pytest.mark.parametrize( 'n2', [8,12] )
+@pytest.mark.parametrize( 'p1', [1,2,3] )
+@pytest.mark.parametrize( 'p2', [1,2,3] )
 
-    nx = ny = 2
-    px = py = 1
+def test_stencil_matrix_2d_serial_dot( n1,n2, p1,p2 ):
 
-    V = StencilVectorSpace([0, 0], [nx, ny], [px, py])
-    x = StencilVector( V )
+    # Select non-zero values based on diagonal index
+    nonzero_values = dict()
+    for k1 in range(-p1,p1+1):
+        for k2 in range(-p2,p2+1):
+            nonzero_values[k1,k2] = 10*k1 + k2
+
+    # Create vector space, stencil matrix, and stencil vector
+    V = StencilVectorSpace( [0,0], [n1,n2], [p1,p2] )
     M = StencilMatrix( V, V )
+    x = StencilVector( V )
 
-    for ix in range(nx+1):
-        for iy in range(ny+1):
-            M[ix, iy, 0, 0] =  4.
-            M[ix, iy, 1, 0] =  1.
-            M[ix, iy,-1, 0] = -1.
-            M[ix, iy, 0, 1] =  2.
-            M[ix, iy, 0,-1] = -2.
+    # Fill in stencil matrix values
+    for k1 in range(-p1,p1+1):
+        for k2 in range(-p2,p2+1):
+            M[:,:,k1,k2] = nonzero_values[k1,k2]
 
+    # Fill in vector values
     x[:,:] = 1.
 
+    # Compute matrix-vector product
     y = M.dot(x)
 
-    print (">>> M shape = ", M._data.shape)
-    print (">>> M = ")
-    print ( M.toarray() )
+    # Convert stencil objects to Numpy arrays
+    Ma = M.toarray()
+    xa = x.toarray()
+    ya = y.toarray()
 
-    print (">>> x shape = ", x._data.shape)
-    print (">>> x = ", x.toarray() )
+    # Exact result using Numpy dot product
+    ya_exact = np.dot( Ma, xa )
 
-    print (">>> y (M dot ones) = ", y.toarray() )
-
-    assert np.all( y.toarray() == 4.0 )
+    # Check data in 1D array
+    assert np.all( ya == ya_exact )
 
 #===============================================================================
 # PARALLEL TESTS
