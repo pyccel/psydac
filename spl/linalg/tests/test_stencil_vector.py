7c0dfa4d7ce1752e95c1810e683b0a4f911b2490
Yaman Güçlü <yaman.guclu@gmail.com>
2018-04-26 17:25:08 +0200

    [linalg] Extensively test serial version of StencilVector.

--- spl/linalg/tests/test_stencil_vector.py
+++ spl/linalg/tests/test_stencil_vector.py
@@ -6,87 +6,136 @@ import numpy as np
 from spl.linalg.stencil import StencilVectorSpace, StencilVector
 
 #===============================================================================
-def test_1():
+# SERIAL TESTS
+#===============================================================================
+@pytest.mark.parametrize( 'n1', [1,7] )
+@pytest.mark.parametrize( 'n2', [1,5] )
+@pytest.mark.parametrize( 'p1', [1,2] )
+@pytest.mark.parametrize( 'p2', [1,2] )
 
-    print( "", "-"*40, "Test 1", "-"*40, "", sep="\n" )
+def test_stencil_vector_2d_serial_init( n1, n2, p1, p2 ):
 
-    n1 = n2 = 4
-    p1 = p2 = 1
+
     V = StencilVectorSpace( [n1,n2], [p1,p2] )
-
     x = StencilVector( V )
+
+    assert x.space is V
+    assert x._data.shape == (n1+2*p1, n2+2*p2)
+
+#===============================================================================
+@pytest.mark.parametrize( 'n1', [1,7] )
+@pytest.mark.parametrize( 'n2', [1,5] )
+@pytest.mark.parametrize( 'p1', [1,2] )
+@pytest.mark.parametrize( 'p2', [1,2] )
+
+def test_stencil_vector_2d_serial_copy( n1, n2, p1, p2 ):
+
+
+    V = StencilVectorSpace( [n1,n2], [p1,p2] )
+    x = StencilVector( V )
+
     for i1 in range(n1):
         for i2 in range(n2):
-            for k1 in range(-p1,p1+1):
-                for k2 in range(-p2,p2+1):
-                    j1 = k1+i1
-                    j2 = k2+i2
-                    x[j1,j2] = j1+j2
+            x[i1,i2] = 10*i1 + i2
+
     z = x.copy()
 
-    print('>>> x shape: ', x._data.shape)
-    print(">>> x = ")
-    print(x)
-    print(">>> x.toarray() = ",  x.toarray())
-    print(">>> z = x.copy() = ")
-    print(z)
+    assert isinstance( z, StencilVector )
+    assert z.space is V
+    assert z._data is not x._data
+    assert np.all( z[:,:] == x[:,:] )
 
 #===============================================================================
-def test_2():
+@pytest.mark.parametrize( 'n1', [1,7] )
+@pytest.mark.parametrize( 'n2', [1,5] )
+@pytest.mark.parametrize( 'p1', [1,2] )
+@pytest.mark.parametrize( 'p2', [1,2] )
 
-    print( "", "-"*40, "Test 2", "-"*40, "", sep="\n" )
+def test_stencil_matrix_2d_serial_toarray( n1, n2, p1, p2 ):
 
-    n1 = n2 = 3
-    p1 = p2 = 1
     V = StencilVectorSpace( [n1,n2], [p1,p2] )
+    x = StencilVector( V )
 
+    for i1 in range(n1):
+        for i2 in range(n2):
+            x[i1,i2] = 10*i1 + i2
+
+    xa = x.toarray()
+
+    z = np.zeros( (n1*n2) )
+    for i1 in range(n1):
+        for i2 in range(n2):
+            z[i1*n2+i2] = 10*i1 + i2
+
+    assert xa.shape == (n1*n2,)
+    assert np.all( xa == z )
+
+#===============================================================================
+@pytest.mark.parametrize( 'n1', [1,7] )
+@pytest.mark.parametrize( 'n2', [1,5] )
+@pytest.mark.parametrize( 'p1', [1,2] )
+@pytest.mark.parametrize( 'p2', [1,2] )
+
+def test_stencil_vector_2d_serial_math( n1, n2, p1, p2 ):
+
+
+    V = StencilVectorSpace( [n1,n2], [p1,p2] )
     x = StencilVector( V )
     y = StencilVector( V )
 
-    x[:,:] = 42.
-    y[:,:] = 10.
+    for i1 in range(n1):
+        for i2 in range(n2):
+            x[i1,i2] = 10*i1 + i2
 
-    a = x + y
-    b = x - y
-    c = 2 * x
+    y[:,:] = 42.0
 
-    print('>>> x shape: ', x._data.shape)
-    print(">>> x = ")
-    print(x)
-    print('>>> y shape: ', y._data.shape)
-    print(">>> y = ")
-    print(y)
-    print(">>> x+y = ")
-    print(a)
-    print(">>> x-y = ")
-    print(b)
-    print(">>> 2*x = ")
-    print(c)
+    r1 = x + y
+    r2 = x - y
+    r3 = 2 * x
+    r4 = x * 2
+
+    xa = x.toarray()
+    ya = y.toarray()
+
+    r1_exact = xa + ya
+    r2_exact = xa - ya
+    r3_exact = 2  * xa
+    r4_exact = xa * 2
+
+    for (r,rex) in zip( [r1,r2,r3,r4], [r1_exact,r2_exact,r3_exact,r4_exact] ):
+
+        assert isinstance( r, StencilVector )
+        assert r.space is V
+        assert np.all( r.toarray() == rex )
 
 #===============================================================================
-def test_3():
+@pytest.mark.parametrize( 'n1', [1,7] )
+@pytest.mark.parametrize( 'n2', [1,5] )
+@pytest.mark.parametrize( 'p1', [1,2] )
+@pytest.mark.parametrize( 'p2', [1,2] )
 
-    print( "", "-"*40, "Test 3", "-"*40, "", sep="\n" )
+def test_stencil_vector_2d_serial_dot( n1, n2, p1, p2 ):
 
-    n1 = n2 = 3
-    p1 = p2 = 1
+
     V = StencilVectorSpace( [n1,n2], [p1,p2] )
-
     x = StencilVector( V )
     y = StencilVector( V )
 
-    x[:, :] = 2.
-    y[:, :] = 5.
+    for i1 in range(n1):
+        for i2 in range(n2):
+            x[i1,i2] = 10*i1 + i2
+            y[i1,i2] = 10*i2 - i1
 
-    z = x.dot(y)
+    z1 = x.dot( y )
+    z2 = y.dot( x )
 
-    print('>>> x shape: ', x._data.shape)
-    print(">>> x.toarray() = ", x.toarray())
-    print('>>> y shape: ', y._data.shape)
-    print(">>> y.toarray() = ", y.toarray())
-    print(">>> dot(x,y) = ")
-    print(z)
+    z_exact = np.dot( x.toarray(), y.toarray() )
 
+    assert z1 == z_exact
+    assert z2 == z_exact
+
+#===============================================================================
+# PARALLEL TESTS
 #===============================================================================
 @pytest.mark.parametrize( 'n1', [2,10,23] )
 @pytest.mark.parametrize( 'n2', [2,12,25] )
@@ -94,7 +143,7 @@ def test_3():
 @pytest.mark.parametrize( 'p2', [1,3,4] )
 @pytest.mark.parallel
 
-def test_vector_parallel_dot( n1, n2, p1, p2 ):
+def test_stencil_vector_2d_parallel_dot( n1, n2, p1, p2 ):
 
     from mpi4py       import MPI
     from spl.ddm.cart import Cart
