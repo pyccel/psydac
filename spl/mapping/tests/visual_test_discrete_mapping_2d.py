287861d5d3de1c3465a2b8e3167dc3cd4d9f8bb3
Yaman Güçlü <yaman.guclu@gmail.com>
2018-08-23 19:14:48 +0200

    [mapping] Add command line options to visual test for 2D spline mapping.

--- spl/mapping/tests/visual_test_discrete_mapping_2d.py
+++ spl/mapping/tests/visual_test_discrete_mapping_2d.py
@@ -3,20 +3,26 @@
 # Copyright 2018 Yaman Güçlü
 
 from mpi4py import MPI
-import numpy as np
-import matplotlib.pyplot as plt
-
-from spl.fem.basic                  import FemField
-from spl.fem.splines                import SplineSpace
-from spl.fem.tensor                 import TensorFemSpace
-from spl.mapping.discrete           import SplineMapping
-from spl.mapping.analytical_gallery import *
-from spl.utilities.utils            import refine_array_1d
 
 #==============================================================================
 # Driver
 #==============================================================================
-def main( map_analytic=Target(), p1=2, p2=2, nc1=2, nc2=5 ):
+def main( mapping='Target', degree=(2,2), ncells=(2,5), **kwargs ):
+
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    from spl.fem.basic                  import FemField
+    from spl.fem.splines                import SplineSpace
+    from spl.fem.tensor                 import TensorFemSpace
+    from spl.mapping.discrete           import SplineMapping
+    from spl.mapping.analytical_gallery import Annulus, Target, Czarny
+    from spl.utilities.utils            import refine_array_1d
+
+    # Input parameters
+    map_analytic = locals()[mapping]( **kwargs )
+    p1 , p2      = degree
+    nc1, nc2     = ncells
 
     # Create tensor spline space, distributed
     V1 = SplineSpace( grid=np.linspace( 0, 1,       nc1+1 ), degree=p1, periodic=False )
@@ -59,12 +65,55 @@ def main( map_analytic=Target(), p1=2, p2=2, nc1=2, nc2=5 ):
 # Parser
 #==============================================================================
 def parse_input_arguments():
-    # TODO
-    return {}
+
+    import argparse
+
+    parser = argparse.ArgumentParser(
+        formatter_class = argparse.ArgumentDefaultsHelpFormatter,
+        description     = 'Interpolate 2D analytical mapping with tensor-product'
+                          ' spline and plot results.'
+    )
+
+    parser.add_argument( '-m',
+        type    = str,
+        choices =('Annulus', 'Target', 'Czarny'),
+        default = 'Annulus',
+        dest    = 'mapping',
+        help    = 'Analytical mapping'
+    )
+
+    parser.add_argument( '-d',
+        type    = int,
+        nargs   = 2,
+        default = [2,2],
+        metavar = ('P1','P2'),
+        dest    = 'degree',
+        help    = 'Spline degree along each dimension'
+    )
+
+    parser.add_argument( '-n',
+        type    = int,
+        nargs   = 2,
+        default = [2,5],
+        metavar = ('N1','N2'),
+        dest    = 'ncells',
+        help    = 'Number of grid cells (elements) along each dimension'
+    )
+
+    return parser.parse_args()
 
 #==============================================================================
 # Script functionality
 #==============================================================================
 if __name__ == '__main__':
+
     args = parse_input_arguments()
-    namespace = main( **args )
+    namespace = main( **vars( args ) )
+
+    import __main__
+    if hasattr( __main__, '__file__' ):
+        try:
+           __IPYTHON__
+        except NameError:
+            import matplotlib.pyplot as plt
+            plt.show()
