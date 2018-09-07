d5940f245cfc2ede5281cbfe003364ee64ccd2db
Yaman Güçlü <yaman.guclu@gmail.com>
2018-09-07 15:09:42 +0200

    [mapping] Add constructors to 'SplineMapping' object:
    - Simplify '__init__' signature and use different class methods as constructors;
    - Option 1 (old): create mapping by interpolation of pre-existing mapping;
    - Option 2 (NEW): create mapping from multi-dimensional array of control points
      (this can be a Numpy array or an H5Py dataset).

--- spl/mapping/discrete.py
+++ spl/mapping/discrete.py
@@ -3,6 +3,10 @@
 # Copyright 2018 Yaman Güçlü
 
 from itertools import product
+import numpy as np
+import string
+import random
+import h5py
 
 from spl.mapping.basic import Mapping
 from spl.fem.tensor    import TensorFemSpace
@@ -11,21 +15,42 @@ from spl.fem.basic     import FemField
 __all__ = ['SplineMapping']
 
 #==============================================================================
+def random_string( n ):
+    chars    = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    selector = random.SystemRandom()
+    return ''.join( selector.choice( chars ) for _ in range( n ) )
 
+#==============================================================================
 class SplineMapping( Mapping ):
 
+    def __init__( self, *components ):
 
-    def __init__( self, *args, **kwargs ):
-        self._init_from_mapping( **kwargs )
+        # Sanity checks
+        assert len( components ) >= 1
+        assert all( isinstance( c, FemField ) for c in components )
+        assert all( isinstance( c.space, TensorFemSpace ) for c in components )
+        assert all( c.space is components[0].space for c in components )
 
+        # Store spline space and one field for each coordinate X_i
+        self._space  = components[0].space
+        self._fields = components
 
-    def _init_from_mapping( self, tensor_space, mapping ):
+        # Store number of logical and physical dimensions
+        self._ldim = components[0].space.ldim
+        self._pdim = len( components )
+
+    #--------------------------------------------------------------------------
+    # Option [1]: initialize from TensorFemSpace and pre-existing mapping
+    #--------------------------------------------------------------------------
+    @classmethod
+    def from_mapping( cls, tensor_space, mapping ):
 
         assert isinstance( tensor_space, TensorFemSpace )
         assert isinstance( mapping, Mapping )
         assert tensor_space.ldim == mapping.ldim
 
-        fields = [FemField( tensor_space, 'x{}'.format(d) )
+        name   = random_string( 8 )
+        fields = [FemField( tensor_space, 'mapping_{name}_x{d}'.format( name=name, d=d ) )
                   for d in range( mapping.pdim )]
 
         V = tensor_space.vector_space
@@ -47,13 +72,39 @@ class SplineMapping( Mapping ):
         for pvals, field in zip( values, fields ):
             tensor_space.compute_interpolant( pvals, field )
 
-        # Store spline space and one field for each coordinate X_i
-        self._space  = tensor_space
-        self._fields = fields
+        # Create SplineMapping object
+        return cls( *fields )
 
-        # Store number of logical and physical dimensions
-        self._ldim = mapping.ldim
-        self._pdim = mapping.pdim
+    #--------------------------------------------------------------------------
+    # Option [2]: initialize from TensorFemSpace and spline control points
+    #--------------------------------------------------------------------------
+    @classmethod
+    def from_control_points( cls, tensor_space, control_points ):
+
+        assert isinstance( tensor_space, TensorFemSpace )
+        assert isinstance( control_points, (np.ndarray, h5py.Dataset) )
+
+        assert control_points.ndim       == tensor_space.ldim + 1
+        assert control_points.shape[:-1] == tuple( V.nbasis for V in tensor_space.spaces )
+        assert control_points.shape[ -1] >= tensor_space.ldim
+
+        # Create one separate scalar field for each physical dimension
+        # TODO: use one unique field belonging to VectorFemSpace
+        name   = random_string( 8 )
+        fields = [FemField( tensor_space, 'mapping_{name}_x{d}'.format( name=name, d=d ) )
+                  for d in range( control_points.shape[2] )]
+
+        # Get spline coefficients for each coordinate X_i
+        starts = tensor_space.vector_space.starts
+        ends   = tensor_space.vector_space.ends
+        idx_to = tuple( slice( s, e+1 ) for s,e in zip( starts, ends ) )
+        for i,field in enumerate( fields ):
+            idx_from = tuple(list(idx_to)+[i])
+            field.coeffs[idx_to] = control_points[idx_from]
+            field.coeffs.update_ghost_regions()
+
+        # Create SplineMapping object
+        return cls( *fields )
 
     #--------------------------------------------------------------------------
     # Abstract interface
