70aa18bc782ecaac6842e5bf7c5cd82d52988251
ratnania <ratnaniahmed@gmail.com>
2018-09-16 10:25:35 +0200

    spl-mesh export

--- spl/cmd/mesh.py
+++ spl/cmd/mesh.py
@@ -4,6 +4,7 @@
 import sys
 import os
 import argparse
+import os.path
 
 # TODO add version
 #  --version  show program's version number and exit
@@ -20,7 +21,7 @@ class MyParser(argparse.ArgumentParser):
         sys.exit(2)
 
 # ... TODO add more geoemtries from caid
-def export_caid_geometry(name, ncells, degree):
+def export_caid_geometry(name, ncells, degree, filename):
     # ...
     from caid.cad_geometry import cad_geometry
     from caid.cad_geometry import line
@@ -32,18 +33,33 @@ def export_caid_geometry(name, ncells, degree):
     geo = constructor(n=[i+1 for i in ncells], p=degree)
     # ...
 
-    # TODO export to file
+    extension = os.path.splitext(filename)[1]
+    if not extension == '.h5':
+        raise ValueError('> Only h5 extension is allowed for filename')
+
+    geo.save(filename)
 
 # ...
 
+
+# example of usage:
+#  spl-mesh -n='16,16' -d='3,3' square mesh.h5
 def main():
     """
     pyccel console command.
     """
-    parser = MyParser(description='spl-mesh command line')
+    parser = MyParser(
+        usage='%(prog)s [OPTIONS] <GEOMETRY> <FILENAME>',
+        epilog="For more information, visit <http://spl.readthedocs.io/>.",
+        description="""spl mesh generation command line.""")
 
-    parser.add_argument('name', metavar='N', type=str, nargs='+',
+    # ...
+    parser.add_argument('geometry', metavar='GEOMETRY',
                         help='a geometry name from gallery/caid')
+
+    parser.add_argument('filename', metavar='FILENAME',
+                        help='output filename')
+    # ...
 
     # ...
     group = parser.add_argument_group('Discretization')
@@ -65,8 +81,13 @@ def main():
     # ...
 
     # ...
-    name = args.name[0] # why is it a list here?
-    print('> name = ', name)
+    geo_name = args.geometry
+#    print('> name = ', geo_name)
+    # ...
+
+    # ...
+    filename = args.filename
+#    print('> filename = ', filename)
     # ...
 
     # ...
@@ -74,7 +95,7 @@ def main():
     if not ncells: raise ValueError('> ncells must be given')
 
     ncells = [int(i) for i in  ncells.split(',')]
-    print('> ncells = ', ncells)
+#    print('> ncells = ', ncells)
     # ...
 
     # ...
@@ -82,7 +103,7 @@ def main():
     if not degree: raise ValueError('> degree must be given')
 
     degree = [int(i) for i in  degree.split(',')]
-    print('> degree = ', degree)
+#    print('> degree = ', degree)
     # ...
 
-    export_caid_geometry(name, ncells, degree)
+    export_caid_geometry(geo_name, ncells, degree, filename)
