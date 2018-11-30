8ebf1da58f22e5053e7f0eb5975661d817f3973d
ratnania <ratnaniahmed@gmail.com>
2018-11-30 19:16:35 +0100

    [cmd] analytical map with spl-mesh

--- spl/cmd/mesh.py
+++ spl/cmd/mesh.py
@@ -38,27 +38,114 @@ def export_caid_geometry(name, ncells, degree, filename):
         raise ValueError('> Only h5 extension is allowed for filename')
 
     geo.save(filename)
+# ...
 
+# ... TODO use export h5 of mapping when available 
+def export_analytical_mapping(mapping, ncells, degree, filename, **kwargs):
+    import numpy as np
+    from mpi4py import MPI
+
+    from spl.fem.splines                import SplineSpace
+    from spl.fem.tensor                 import TensorFemSpace
+    from spl.mapping.discrete           import SplineMapping
+    from spl.mapping.analytical         import IdentityMapping
+    from spl.mapping.analytical_gallery import Annulus, Target, Czarny, Collela
+
+    dim = len(ncells)
+    if not( dim == 2 ):
+        raise NotImplementedError('only 2d is available')
+
+    mapping = mapping.lower()
+
+    # ...
+    if dim == 2:
+        # Input parameters
+        if mapping == 'identity':
+            map_analytic = IdentityMapping( ndim=2 )
+            lims1   = (0, 1)
+            lims2   = (0, 1)
+            period1 = False
+            period2 = False
+
+        elif mapping == 'collela':
+            map_analytic = Collela( **kwargs )
+            lims1   = (0, 1)
+            lims2   = (0, 1)
+            period1 = False
+            period2 = False
+
+        else:
+            mapping = mapping.capitalize()
+            map_analytic = locals()[mapping]( **kwargs )
+            lims1 = (0, 1)
+            lims2 = (0, 2*np.pi)
+            period1 = False
+            period2 = True
+
+        p1 , p2  = degree
+        nc1, nc2 = ncells
+
+        # Create tensor spline space, distributed
+        V1 = SplineSpace( grid=np.linspace( *lims1, num=nc1+1 ), degree=p1, periodic=period1 )
+        V2 = SplineSpace( grid=np.linspace( *lims2, num=nc2+1 ), degree=p2, periodic=period2 )
+        space = TensorFemSpace( V1, V2, comm=MPI.COMM_WORLD )
+
+        # Create spline mapping by interpolating analytical one
+        map_discrete = SplineMapping.from_mapping( space, map_analytic )
+    # ...
+
+    # ... TODO remove this later, once we have export h5
+    knots = [V.knots for V in space.spaces]
+    coeffs = [f.coeffs.toarray() for f in map_discrete._fields]
+    shape  = [V.nbasis for V in space.spaces] + [3]
+    points = np.zeros(shape)
+    for i,c in enumerate(coeffs):
+        points[:,:,i] = c.reshape([V.nbasis for V in space.spaces])
+
+    from caid.cad_geometry import cad_geometry
+    from caid.cad_geometry import cad_nurbs
+
+    nrb = cad_nurbs(knots, points)
+    nrb.orientation = [-1,1,1,-1]
+    geo = cad_geometry()
+    geo.append(nrb)
+    # TODO must be done automaticaly
+    geo._internal_faces = []
+    geo._external_faces = [[0,0],[0,1],[0,2],[0,3]]
+    geo._connectivity   = []
+
+    extension = os.path.splitext(filename)[1]
+    if not extension == '.h5':
+        raise ValueError('> Only h5 extension is allowed for filename')
+
+    geo.save(filename)
+    # ...
 # ...
 
 
 # example of usage:
-#  spl-mesh -n='16,16' -d='3,3' square mesh.h5
+#  spl-mesh -n='16,16' -d='3,3' --geometry=square mesh.h5
 def main():
     """
     pyccel console command.
     """
     parser = MyParser(
-        usage='%(prog)s [OPTIONS] <GEOMETRY> <FILENAME>',
+        usage='%(prog)s [OPTIONS] <FILENAME>',
         epilog="For more information, visit <http://spl.readthedocs.io/>.",
         description="""spl mesh generation command line.""")
 
     # ...
-    parser.add_argument('geometry', metavar='GEOMETRY',
-                        help='a geometry name from gallery/caid')
-
     parser.add_argument('filename', metavar='FILENAME',
                         help='output filename')
+    # ...
+
+    # ...
+    group = parser.add_argument_group('Geometry')
+    group.add_argument('--caid', default = '',
+                        help='a geometry name from gallery/caid')
+
+    group.add_argument('--analytical', default = '',
+                        help='analytical mapping provided by SPL')
     # ...
 
     # ...
@@ -81,13 +168,9 @@ def main():
     # ...
 
     # ...
-    geo_name = args.geometry
-#    print('> name = ', geo_name)
-    # ...
-
-    # ...
     filename = args.filename
-#    print('> filename = ', filename)
+    geo_name = args.caid
+    analytical = args.analytical
     # ...
 
     # ...
@@ -95,7 +178,6 @@ def main():
     if not ncells: raise ValueError('> ncells must be given')
 
     ncells = [int(i) for i in  ncells.split(',')]
-#    print('> ncells = ', ncells)
     # ...
 
     # ...
@@ -103,7 +185,10 @@ def main():
     if not degree: raise ValueError('> degree must be given')
 
     degree = [int(i) for i in  degree.split(',')]
-#    print('> degree = ', degree)
     # ...
 
-    export_caid_geometry(geo_name, ncells, degree, filename)
+    if geo_name:
+        export_caid_geometry(geo_name, ncells, degree, filename)
+
+    elif analytical:
+        export_analytical_mapping(analytical, ncells, degree, filename)
