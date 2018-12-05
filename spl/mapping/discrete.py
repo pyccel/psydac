3ccfb2090aea69a86b2d85e2f0a6d9e127d91e1a
Yaman Güçlü <yaman.guclu@gmail.com>
2018-12-05 17:32:41 +0100

    [cad] SplineMapping class: add 'export' method for writing to HDF5 file:
    - Geometry metadata;
    - Degrees and knot sequences of tensor-product spline finite element space;
    - Control points of spline mapping.

--- spl/mapping/discrete.py
+++ spl/mapping/discrete.py
@@ -3,10 +3,13 @@
 # Copyright 2018 Yaman Güçlü
 
 from itertools import product
+from collections import OrderedDict
 import numpy as np
 import string
 import random
 import h5py
+import yaml
+import yamlloader
 
 from spl.mapping.basic import Mapping
 from spl.fem.tensor    import TensorFemSpace
@@ -151,6 +154,69 @@ class SplineMapping( Mapping ):
     @property
     def control_points( self ):
         return self._control_points
+
+    # TODO: move to 'Geometry' class in 'spl.cad.geometry' module
+    def export( self, filename ):
+        """
+        Export tensor-product spline space and mapping to geometry file in HDF5
+        format (single-patch only).
+
+        Parameters
+        ----------
+        filename : str
+          Name of HDF5 output file.
+
+        """
+        space = self.space
+        comm  = space.vector_space.cart.comm
+
+        # Create dictionary with geometry metadata
+        yml = OrderedDict()
+        yml['ldim'] = self.ldim
+        yml['pdim'] = self.pdim
+        yml['patches'] = [OrderedDict( [('name' , 'patch_{}'.format( 0 ) ),
+                                        ('type' , 'cad_nurbs'            ),
+                                        ('color', 'None'                 )] )]
+        yml['internal_faces'] = []
+        yml['external_faces'] = [[0,i] for i in range( 2*self.ldim )]
+        yml['connectivity'  ] = []
+
+        # Dump geometry metadata to string in YAML file format
+        geo = yaml.dump(
+            data   = yml,
+            Dumper = yamlloader.ordereddict.Dumper,
+        )
+
+        # Create HDF5 file (in parallel mode if MPI communicator size > 1)
+        kwargs = dict( driver='mpio', comm=comm ) if comm.size > 1 else {}
+        h5 = h5py.File( filename, mode='w', **kwargs )
+
+        # Write geometry metadata as fixed-length array of ASCII characters
+        h5['geometry.yml'] = np.array( geo, dtype='S' )
+
+        # Create group for patch 0
+        group = h5.create_group( yml['patches'][0]['name'] )
+        group.attrs['shape'      ] = space.vector_space.npts
+        group.attrs['degree'     ] = space.degree
+        group.attrs['rational'   ] = False
+        group.attrs['periodic'   ] = space.periodic
+        for d in range( self.pdim ):
+            group['knots_{}'.format( d )] = space.spaces[d].knots
+
+        # Collective: create dataset for control points
+        shape = [n for n in space.vector_space.npts] + [self.pdim]
+        dtype = space.vector_space.dtype
+        dset  = group.create_dataset( 'points', shape=shape, dtype=dtype )
+
+        # Independent: write control points to dataset
+        starts = space.vector_space.starts
+        ends   = space.vector_space.ends
+        index  = [slice(s, e+1) for s, e in zip(starts, ends)] + [slice(None)]
+        index  = tuple( index )
+        dset[index] = self.control_points[index]
+
+        # Close HDF5 file
+        h5.close()
 
     #==========================================================================
     class ControlPoints:
