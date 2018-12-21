107690ad0e7b3df3e8279120353f066450f5de32
ratnania <ratnaniahmed@gmail.com>
2018-12-21 16:26:41 +0100

    [api] update run_tests script

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-python3 -m pytest --pyargs spl/api/tests/ -m "not parallel"
-mpirun -np 2 python3 -m pytest --pyargs spl/api/tests/ -m "parallel"
+#python3 -m pytest --pyargs spl/api/tests/ -m "not parallel"
+#mpirun -np 2 python3 -m pytest --pyargs spl/api/tests/ -m "parallel"
+
+
+python3 -m pytest spl/api/tests/test_api_2d_scalar.py
+python3 -m pytest spl/api/tests/test_api_2d_scalar_mapping.py
+python3 -m pytest spl/api/tests/test_api_2d_vector.py
+python3 -m pytest spl/api/tests/test_api_2d_vector_mapping.py
+python3 -m pytest spl/api/tests/test_api_3d_scalar.py
+python3 -m pytest spl/api/tests/test_api_3d_scalar_mapping.py
+python3 -m pytest spl/api/tests/test_api_3d_vector.py
