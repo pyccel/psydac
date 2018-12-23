e251e364b854ac2af3fc331e86a1ca755b715a27
ratnania <ratnaniahmed@gmail.com>
2018-12-23 19:09:39 +0100

    run tests script

--- run_tests.sh
+++ run_tests.sh
@@ -13,4 +13,8 @@ python3 -m pytest spl/api/tests/test_api_3d_scalar_mapping.py
 python3 -m pytest spl/api/tests/test_api_3d_vector.py
 
 mpirun -np 2 python3 -m pytest spl/api/tests/test_api_2d_scalar_parallel.py
+mpirun -np 2 python3 -m pytest spl/api/tests/test_api_2d_scalar_mapping_parallel.py
+
+mpirun -np 2 python3 -m pytest spl/api/tests/test_api_3d_scalar_parallel.py
+mpirun -np 2 python3 -m pytest spl/api/tests/test_api_3d_scalar_mapping_parallel.py
 
