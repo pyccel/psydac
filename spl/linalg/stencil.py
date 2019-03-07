3697ebd33a3c365dd57c1b40568f5d573a23af58
saidctb <said-ctb@live.fr>
2019-03-07 18:32:58 +0100

    fix bug in StencilMatrix._tocoo_parallel_with_pads

--- spl/linalg/stencil.py
+++ spl/linalg/stencil.py
@@ -921,7 +921,7 @@ class StencilMatrix( Matrix ):
             for ll in np.ndindex( *ll_dims ):
 
                 # Compute column multi-index (k = j - i)
-                jj = [(i+l-p) % n for (i,l,n,p) in zip(ii,ll,nd,pp)]
+                jj = [(i+l-p) % n for (i,l,n,p) in zip(ii,ll,nc,pp)]
 
                 # Compute column flat index
                 J = ravel_multi_index( jj, dims=nd, order='C' )
