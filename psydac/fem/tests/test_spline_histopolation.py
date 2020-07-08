17a6335152680b2ad2d1c501053c6dc976cc8b31
Yaman Güçlü <yaman.guclu@gmail.com>
2020-07-08 17:08:45 +0200

    Update histopolation unit test

--- psydac/fem/tests/test_spline_histopolation.py
+++ psydac/fem/tests/test_spline_histopolation.py
@@ -1,61 +1,90 @@
+import pytest
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.integrate import quad
 
-from psydac.core.bsplines import make_knots
-from psydac.fem.basic     import FemField
-from psydac.fem.splines   import SplineSpace
-from psydac.fem.tensor    import TensorFemSpace
+from psydac.fem.basic   import FemField
+from psydac.fem.splines import SplineSpace
+#from psydac.fem.tensor  import TensorFemSpace
 
 from psydac.fem.tests.utilities              import horner, random_grid
-from psydac.fem.tests.splines_error_bounds   import spline_1d_error_bound
-from psydac.fem.tests.analytical_profiles_1d import (AnalyticalProfile1D_Cos,
-                                                  AnalyticalProfile1D_Poly)
+#from psydac.fem.tests.splines_error_bounds   import spline_1d_error_bound
+#from psydac.fem.tests.analytical_profiles_1d import (AnalyticalProfile1D_Cos,
+#                                                  AnalyticalProfile1D_Poly)
 
 #==============================================================================
+def histopolate_polynomial(basis, ncells, degree):
 
-ncells = 10
-degree = 7
+    domain   = (-1.0, 1.0)
+    periodic = False
+
+    # Polynomial to be approximated
+    poly_coeffs = np.random.random_sample( degree+1 ) # 0 <= c < 1
+    poly_coeffs = 1.0 - poly_coeffs                   # 0 < c <= 1
+    f = lambda x : horner( x, *poly_coeffs )
+
+    # Define spline space and field
+    grid = random_grid( domain, ncells, 0.5 )
+    Vh = SplineSpace( degree=degree, grid=grid, periodic=periodic, basis=basis )
+    fh = FemField( Vh )
+
+    # Compute histopolant
+    xg = Vh.ext_greville
+    Ig = np.array([quad(f, xg[i], xg[i+1])[0] for i in range(len(xg)-1)])
+    Vh.compute_histopolant(Ig, fh)
+
+    return domain, f, fh
 
 #==============================================================================
+@pytest.mark.parametrize('basis', ['B', 'M'])
+@pytest.mark.parametrize('ncells', [10, 20, 33])
+@pytest.mark.parametrize('degree', [2, 5, 7])
+def test_histopolation_exact(basis, ncells, degree, num_pts=100, tol=1e-12):
 
-domain   = (-1.0, 1.0)
-periodic = False
+    domain, f, fh = histopolate_polynomial(basis, ncells, degree)
 
-poly_coeffs = np.random.random_sample( degree+1 ) # 0 <= c < 1
-poly_coeffs = 1.0 - poly_coeffs                   # 0 < c <= 1
-f1 = lambda x : horner( x, *poly_coeffs )
+    # Compare to exact solution
+    x  = np.linspace(*domain, num=num_pts)
+    y  = f(x)
+    yh = np.array([fh(xi) for xi in x])
 
-
-grid = random_grid( domain, ncells, 0.5 )
-V1_h = SplineSpace( degree=degree, grid=grid, periodic=periodic, basis='M' )
-f1_h = FemField( V1_h )
-
-xg = V1_h.ext_greville
-c1 = np.array([quad(f1, xg[i], xg[i+1])[0] for i in range(len(xg)-1)])
-V1_h.compute_interpolant( c1, f1_h )
+    assert np.allclose(yh, y, rtol=tol, atol=tol)
 
 #==============================================================================
 # Diagnostics
 #==============================================================================
-xt  = np.linspace( *domain, num=100 )
-err = np.array( [f1_h( x ) - f1( x ) for x in xt] )
+def compare_and_plot(domain, f, fh, num_pts=100):
 
-max_norm_err = np.max( abs( err ) )
-print("Maximum error on evaluation grid: {}".format(max_norm_err))
+    x  = np.linspace(*domain, num=num_pts)
+    y  = f(x)
+    yh = np.array([fh(xi) for xi in x])
 
-fig, ax = plt.subplots(1, 1)
-ax.plot(xt, f1(xt), label='f1(x)')
-ax.plot(xt, [f1_h(x) for x in xt], '.', label='f1_h(x)')
-ax.grid(True)
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-fig.legend()
-fig.show()
+    max_norm_err = np.max(abs(yh - y))
+    print("Maximum error on evaluation grid: {}".format(max_norm_err))
 
-import __main__ as main
-if hasattr( main, '__file__' ):
-    try:
-       __IPYTHON__
-    except NameError:
-        plt.show()
+    fig, ax = plt.subplots(1, 1)
+    ax.plot(x, y, label='f(x)')
+    ax.plot(x, yh, '.', label='f1_h(x)')
+    ax.grid(True)
+    ax.set_xlabel('x')
+    ax.set_ylabel('y')
+    fig.legend()
+    fig.show()
+
+#==============================================================================
+# Diagnostics
+#==============================================================================
+if __name__ == '__main__':
+
+    domain, f, fh = histopolate_polynomial(basis='B', ncells=10, degree=7)
+    compare_and_plot(domain, f, fh)
+
+    domain, f, fh = histopolate_polynomial(basis='M', ncells=10, degree=7)
+    compare_and_plot(domain, f, fh)
+
+    import __main__ as main
+    if hasattr( main, '__file__' ):
+        try:
+           __IPYTHON__
+        except NameError:
+            plt.show()
