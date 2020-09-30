adff325187cb5ff204d49e693378969bbf62a752
saidctb <said-ctb@live.fr>
2020-09-30 02:44:08 +0200

    return fields instead of coeffs for the differential operators and the projectors

--- psydac/api/discretization.py
+++ psydac/api/discretization.py
@@ -47,7 +47,7 @@ from psydac.fem.vector               import ProductFemSpace
 from psydac.cad.geometry             import Geometry
 from psydac.mapping.discrete         import SplineMapping, NurbsMapping
 from psydac.feec.derivatives         import Grad, Curl, Div, Rot
-from psydac.feec.utilities           import interpolation_matrices
+from psydac.feec.global_projectors   import Projector_H1, Projector_Hcurl, Projector_Hdiv, Projector_L2
 
 import inspect
 import sys
@@ -319,12 +319,31 @@ class DiscreteDerham(BasicDiscrete):
 
     @property
     def derivatives_as_matrices(self):
-        return tuple(V.diff._matrix for V in self.spaces)
+        return tuple(V.diff.matrix for V in self.spaces)
 
     @property
     def derivatives_as_operators(self):
         return tuple(V.diff for V in self.spaces)
 
+    def projectors(kind='global', nquads=None):
+
+        if not kind == 'global':
+            raise NotImplementedError('only global projectors are available')
+
+        if self.dim == 1:
+            P0 = Projector_H1(self.V0)
+            P1 = Projector_L2(self.V1, nquads)
+            return P0, P1
+
+        elif self.dim == 2:
+            raise NotImplementedError('TODO')
+
+        elif self.dim == 3:
+            P0 = Projector_H1(self.V0)
+            P1 = Projector_Hcurl(self.V1, nquads)
+            P2 = Projector_Hdiv(self.V2, nquads)
+            P3 = Projector_L2(self.V3, nquads)
+            return P0, P1, P2, P3
 
 #==============================================================================           
 def discretize_derham(Complex, domain_h, *args, **kwargs):
