020eaf772caf261c23550ca54b497547e6243a66
Yaman Güçlü <yaman.guclu@gmail.com>
2020-12-03 17:22:38 +0100

    Use sympde version 0.10.2
    Moreover, let sympde specify requirements on sympy version.

--- setup.py
+++ setup.py
@@ -48,14 +48,14 @@ install_requires = [
     # Third-party packages from PyPi
     'numpy>=1.16',
     'scipy>=0.18',
-    'sympy>=1.2,<1.7',
+    'sympy',
     'matplotlib',
     'pytest>=4.5',
     'pyyaml',
     'yamlloader',
 
     # Our packages from PyPi
-    'sympde==0.10.1',
+    'sympde==0.10.2',
     'pyccel==0.10.1',
     'gelato',
 
