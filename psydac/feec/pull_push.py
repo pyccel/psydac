b906c32efaa98ac0b96a73bf58b893a9fb89e68f
saidctb <said-ctb@live.fr>
2020-12-08 07:18:42 +0100

    use callable mapping

--- psydac/feec/pull_push.py
+++ psydac/feec/pull_push.py
@@ -2,10 +2,9 @@ from sympy import lambdify
 # =======================================================================
 def pull_v(funcs_ini, mapping):
 
-    coords   = mapping.logical_coordinates
-    exprs    = mapping.expressions
-    f1,f2,f3 = lambdify(coords, exprs[0]), lambdify(coords, exprs[1]), lambdify(coords, exprs[2])
-    J_inv    = lambdify(coords, mapping.jacobian_inv_expr)
+    mapping  = mapping.get_callable_mapping()
+    f1,f2,f3 = mapping._func_eval
+    J_inv    = mapping._jacobian_inv
 
     def fun(xi1, xi2, xi3):
         x = f1(xi1, xi2, xi3)
@@ -27,9 +26,8 @@ def pull_v(funcs_ini, mapping):
 #==============================================================================
 def pull_0(func_ini, mapping):
 
-    coords   = mapping.logical_coordinates
-    exprs    = mapping.expressions
-    f1,f2,f3 = lambdify(coords, exprs[0]), lambdify(coords, exprs[1]), lambdify(coords, exprs[2])
+    mapping  = mapping.get_callable_mapping()
+    f1,f2,f3 = mapping._func_eval
 
     def fun(xi1, xi2, xi3):
         x = f1(xi1, xi2, xi3)
@@ -44,11 +42,9 @@ def pull_0(func_ini, mapping):
 #==============================================================================
 def pull_1(funcs_ini, mapping):
 
-    coords   = mapping.logical_coordinates
-    exprs    = mapping.expressions
-    f1,f2,f3 = lambdify(coords, exprs[0]), lambdify(coords, exprs[1]), lambdify(coords, exprs[2])
-    J_T      = lambdify(coords, mapping.jacobian_expr.T)
-
+    mapping  = mapping.get_callable_mapping()
+    f1,f2,f3 = mapping._func_eval
+    jacobian = mapping._jacobian
 
     def fun1(xi1, xi2, xi3):
         x = f1(xi1, xi2, xi3)
@@ -59,7 +55,7 @@ def pull_1(funcs_ini, mapping):
         a2_phys = funcs_ini[1](x, y, z)
         a3_phys = funcs_ini[2](x, y, z)
 
-        J_T_value = J_T(xi1, xi2, xi3)
+        J_T_value = jacobian(xi1, xi2, xi3).T
         value_1 = J_T_value[0,0]*a1_phys + J_T_value[0,1]*a2_phys + J_T_value[0,2]*a3_phys
         return value_1
 
@@ -72,7 +68,7 @@ def pull_1(funcs_ini, mapping):
         a2_phys = funcs_ini[1](x, y, z)
         a3_phys = funcs_ini[2](x, y, z)
 
-        J_T_value = J_T(xi1, xi2, xi3)
+        J_T_value = jacobian(xi1, xi2, xi3).T
 
         value_2 = J_T_value[1,0]*a1_phys + J_T_value[1,1]*a2_phys + J_T_value[1,2]*a3_phys
 
@@ -87,7 +83,7 @@ def pull_1(funcs_ini, mapping):
         a2_phys = funcs_ini[1](x, y, z)
         a3_phys = funcs_ini[2](x, y, z)
 
-        J_T_value = J_T(xi1, xi2, xi3)
+        J_T_value = jacobian(xi1, xi2, xi3).T
 
         value_3 = J_T_value[2,0]*a1_phys + J_T_value[2,1]*a2_phys + J_T_value[2,2]*a3_phys
         return value_3
@@ -97,11 +93,10 @@ def pull_1(funcs_ini, mapping):
 #==============================================================================
 def pull_2(funcs_ini, mapping):
 
-    coords   = mapping.logical_coordinates
-    exprs    = mapping.expressions
-    f1,f2,f3 = lambdify(coords, exprs[0]), lambdify(coords, exprs[1]), lambdify(coords, exprs[2])
-    J_inv    = lambdify(coords, mapping.jacobian_inv_expr)
-    det      = lambdify(coords, mapping.metric_det_expr**0.5)
+    mapping    = mapping.get_callable_mapping()
+    f1,f2,f3   = mapping._func_eval
+    J_inv      = mapping._jacobian_inv
+    metric_det = mapping._metric_det
 
     def fun1(xi1, xi2, xi3):
         x = f1(xi1, xi2, xi3)
@@ -113,7 +108,7 @@ def pull_2(funcs_ini, mapping):
         a3_phys = funcs_ini[2](x, y, z)
 
         J_inv_value = J_inv(xi1, xi2, xi3)
-        det_value   = det(xi1, xi2, xi3)
+        det_value   = metric_det(xi1, xi2, xi3)**0.5
 
         value_1 = J_inv_value[0,0]*a1_phys + J_inv_value[0,1]*a2_phys + J_inv_value[0,2]*a3_phys
 
@@ -129,7 +124,7 @@ def pull_2(funcs_ini, mapping):
         a3_phys = funcs_ini[2](x, y, z)
 
         J_inv_value = J_inv(xi1, xi2, xi3)
-        det_value   = det(xi1, xi2, xi3)
+        det_value   = metric_det(xi1, xi2, xi3)**0.5
 
         value_2 = J_inv_value[1,0]*a1_phys + J_inv_value[1,1]*a2_phys + J_inv_value[1,2]*a3_phys
 
@@ -145,7 +140,7 @@ def pull_2(funcs_ini, mapping):
         a3_phys = funcs_ini[2](x, y, z)
 
         J_inv_value = J_inv(xi1, xi2, xi3)
-        det_value   = det(xi1, xi2, xi3)
+        det_value   = metric_det(xi1, xi2, xi3)**0.5
 
         value_3 = J_inv_value[2,0]*a1_phys + J_inv_value[2,1]*a2_phys + J_inv_value[2,2]*a3_phys
 
@@ -156,17 +151,16 @@ def pull_2(funcs_ini, mapping):
 #==============================================================================
 def pull_3(func_ini, mapping):
 
-    coords   = mapping.logical_coordinates
-    exprs    = mapping.expressions
-    f1,f2,f3 = lambdify(coords, exprs[0]), lambdify(coords, exprs[1]), lambdify(coords, exprs[2])
-    det      = lambdify(coords, mapping.metric_det_expr**0.5)
+    mapping    = mapping.get_callable_mapping()
+    f1,f2,f3   = mapping._func_eval
+    metric_det = mapping._metric_det
 
     def fun(xi1, xi2, xi3):
         x = f1(xi1, xi2, xi3)
         y = f2(xi1, xi2, xi3)
         z = f3(xi1, xi2, xi3)
 
-        det_value = det(xi1, xi2, xi3)
+        det_value = metric_det(xi1, xi2, xi3)**0.5
         value     = func_ini(x, y, z)
         return det_value*value
 
@@ -175,12 +169,10 @@ def pull_3(func_ini, mapping):
 #==============================================================================
 
 def push_1(a1, a2, a3, xi1, xi2, xi3, mapping):
-    
 
-    coords      = mapping.logical_coordinates
-    exprs       = mapping.expressions
-    f1,f2,f3    = lambdify(coords, exprs[0]), lambdify(coords, exprs[1]), lambdify(coords, exprs[2])
-    J_inv       = lambdify(coords, mapping.jacobian_inv_expr)
+    mapping    = mapping.get_callable_mapping()
+    J_inv      = mapping._jacobian_inv
+
     J_inv_value = J_inv(xi1, xi2, xi3)
 
     value1 = (J_inv_value[0,0]*a1(xi1, xi2, xi3) +
@@ -201,13 +193,12 @@ def push_1(a1, a2, a3, xi1, xi2, xi3, mapping):
 #==============================================================================
 def push_2(a1, a2, a3, xi1, xi2, xi3, mapping):
 
-    coords    = mapping.logical_coordinates
-    exprs     = mapping.expressions
-    f1,f2,f3  = lambdify(coords, exprs[0]), lambdify(coords, exprs[1]), lambdify(coords, exprs[2])
-    J         = lambdify(coords, mapping.jacobian_expr)
-    J_value   = J(xi1, xi2, xi3)
-    det       = lambdify(coords, mapping.metric_det_expr**0.5)
-    det_value = det(xi1, xi2, xi3)
+    mapping    = mapping.get_callable_mapping()
+    J          = mapping._jacobian
+    metric_det = mapping._metric_det
+
+    J_value    = J(xi1, xi2, xi3)
+    det_value  = metric_det(xi1, xi2, xi3)**0.5
 
     value1 = ( J_value[0,0]*a1(xi1, xi2, xi3) +
                J_value[0,1]*a2(xi1, xi2, xi3) +
