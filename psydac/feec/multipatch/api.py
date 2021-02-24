795c80579999a670eb3f8a5a9dd15c43cb2096d3
Martin Campos Pinto <martin.campos-pinto@ipp.mpg.de>
2021-02-24 13:26:38 +0100

    [feec-multipatch] add Scalar Curl + L2 Projector for 2D multipatch

--- psydac/feec/multipatch/api.py
+++ psydac/feec/multipatch/api.py
@@ -4,8 +4,10 @@ from psydac.api.discretization        import discretize as discretize_single_pat
 from psydac.api.discretization        import discretize_space
 from psydac.api.discretization        import DiscreteDerham
 from psydac.feec.multipatch.operators import BrokenGradient_2D
+from psydac.feec.multipatch.operators import BrokenScalarCurl_2D
 from psydac.feec.multipatch.operators import Multipatch_Projector_H1
 from psydac.feec.multipatch.operators import Multipatch_Projector_Hcurl
+from psydac.feec.multipatch.operators import Multipatch_Projector_L2
 
 __all__ = ('DiscreteDerhamMultipatch', 'discretize')
 
@@ -36,7 +38,7 @@ class DiscreteDerhamMultipatch(DiscreteDerham):
                 self._sequence = tuple(sequence)
                 self._broken_diff_ops = (
                     BrokenGradient_2D(self.V0, self.V1),
-                    None,
+                    BrokenScalarCurl_2D(self.V1, self.V2),  # None,
                 )
 
             elif tuple(sequence) == ('h1', 'hdiv', 'l2'):
@@ -89,7 +91,7 @@ class DiscreteDerhamMultipatch(DiscreteDerham):
                 P1 = None # TODO: Multipatch_Projector_Hdiv(self.V1, nquads=nquads)
                 raise NotImplementedError('2D sequence with H-div not available yet')
 
-            P2 = None # TODO: Multipatch_Projector_Hcurl(self.V1, nquads=nquads)
+            P2 = Multipatch_Projector_L2(self.V2, nquads=nquads)
             return P0, P1, P2
 
         elif self.dim == 3:
