38054a10ea8458f261e7c82727bbeaa21720cd5a
saidctb <said-ctb@live.fr>
2021-07-28 06:02:42 +0200

    add kwargs in the abstract class

--- psydac/linalg/basic.py
+++ psydac/linalg/basic.py
@@ -144,11 +144,11 @@ class Matrix( LinearOperator ):
     # Deferred methods
     #-------------------------------------
     @abstractmethod
-    def toarray( self, *, order='C', with_pads=False ):
+    def toarray( self, *, **kwargs ):
         """ Convert to Numpy 2D array. """
 
     @abstractmethod
-    def tosparse( self, *, order='C', with_pads=False ):
+    def tosparse( self, *, **kwargs ):
         """ Convert to any Scipy sparse matrix format. """
 
     @abstractmethod
