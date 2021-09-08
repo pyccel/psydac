2c2c186b48ae20e253477acc9f407600ae988760
saidctb <said-ctb@live.fr>
2021-09-08 18:19:30 +0200

    clean code

--- psydac/feec/multipatch/conga_maxwell_eigenproblem_example.py
+++ psydac/feec/multipatch/conga_maxwell_eigenproblem_example.py
@@ -28,6 +28,7 @@ from scipy.sparse.linalg import spsolve
 from scipy.sparse.linalg import LinearOperator, eigsh, minres, gmres
 
 from scipy.sparse.linalg import inv
+from scipy.linalg        import eig
 from scipy.sparse import save_npz, load_npz
 
 # from scikits.umfpack import splu    # import error
@@ -292,235 +293,9 @@ def run_maxwell_2d_eigenproblem_nitsche(nb_eigs, ncells, degree, gamma_jump,
 
     L_option = 2
 
-    if test_harmonic_field:
-        print("testing harmonic field (for debugging purposes)...")
-
-        t_stamp = time_count(t_stamp)
-        print("assembling projection operators...")
-        P0, P1, P2 = derham_h.projectors(nquads=nquads)
-
-        # testing fields in kernel for circular annulus...
-        harmonic_field = 2
-
-        if harmonic_field == 1:
-            # 'diverging' harmonic field: hf = ((cos theta)/r , (sin theta)/r) = (-y/r**2, x/r**2)
-            hf_x = x/(x**2 + y**2)
-            hf_y = y/(x**2 + y**2)
-        else:
-            # 'rotating' harmonic field: hf = (-(sin theta)/r , (cos theta)/r) = (-y/r**2, x/r**2)
-            hf_x = -y/(x**2 + y**2)
-            hf_y =  x/(x**2 + y**2)
-
-        from sympy import lambdify
-        hf_x   = lambdify(domain.coordinates, hf_x)
-        hf_y   = lambdify(domain.coordinates, hf_y)
-        hf_log = [pull_2d_hcurl([hf_x,hf_y], f) for f in mappings_list]
-
-        hf = P1(hf_log)
-        chf = D1(hf)
-
-        grid_vals_h1 = lambda v: get_grid_vals_scalar(v, etas, mappings_list, space_kind='h1')
-        grid_vals_hcurl = lambda v: get_grid_vals_vector(v, etas, mappings_list, space_kind='hcurl')
-
-        hf_x_vals, hf_y_vals = grid_vals_hcurl(hf)
-        chf_vals  = grid_vals_h1(chf)
-
-        my_small_plot(
-            title=r'diverging harmonic field and Conga curl',
-            vals=[hf_x_vals, hf_y_vals, chf_vals],
-            titles=[r'$v_x$', r'$v_y$' , r'$curl Pv$' ],
-            surface_plot=True,
-            xx=xx, yy=yy,
-        )
-
-    print('Finding eigenmodes and eigenvalues ... ')
-
-    if sigma == 0:
-        # computing kernel
-        mode = 'normal'
-        which = 'LM'
-        # note: using scipy.linalg.null_space with A_m.todense() is way too slow
-    else:
-        mode = 'normal'
-        # mode='cayley'
-        # mode='buckling'
-        which = 'LM'
-
-    # from eigsh docstring:
-    #   ncv = number of Lanczos vectors generated ncv must be greater than k and smaller than n;
-    #   it is recommended that ncv > 2*k. Default: min(n, max(2*k + 1, 20))
-    ncv = 4*nb_eigs
-    # search mode: normal and buckling give a lot of zero eigenmodes. Cayley seems best for Maxwell.
-    # mode='normal'
-
-    t_stamp = time_count(t_stamp)
-    print('A_m.shape = ', A_m.shape)
-
-    print('computing eigenvalues and eigenvectors with scipy.sparse.eigsh...' )
-    if A_m.shape[0] < 17000:   # max value for super_lu is >= 13200
-        print('(with super_lu decomposition)')
-        eigenvalues, eigenvectors = eigsh(A_m, k=nb_eigs, M=M1_m, sigma=ref_sigmas[len(ref_sigmas)//2], mode=mode, which=which, ncv=ncv)
-        print(eigenvalues)
-    else:
-        # from https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.eigsh.html:
-        # the user can supply the matrix or operator OPinv, which gives x = OPinv @ b = [A - sigma * M]^-1 @ b.
-        # > here, minres: MINimum RESidual iteration to solve Ax=b
-        # suggested in https://github.com/scipy/scipy/issues/4170
-        OP = A_m - sigma*M1_m
-        print('(with minres iterative solver for A_m - sigma*M1_m)')
-        OPinv = LinearOperator(matvec=lambda v: minres(OP, v, tol=1e-10)[0], shape=M1_m.shape, dtype=M1_m.dtype)
-        # print('(with gmres iterative solver for A_m - sigma*M1_m)')
-        # OPinv = LinearOperator(matvec=lambda v: gmres(OP, v, tol=1e-7)[0], shape=M1_m.shape, dtype=M1_m.dtype)
-        # print('(with spsolve solver for A_m - sigma*M1_m)')
-        # OPinv = LinearOperator(matvec=lambda v: spsolve(OP, v, use_umfpack=True), shape=M1_m.shape, dtype=M1_m.dtype)
-
-        # lu = splu(OP)
-        # OPinv = LinearOperator(matvec=lambda v: lu.solve(v), shape=M1_m.shape, dtype=M1_m.dtype)
-        eigenvalues, eigenvectors = eigsh(A_m, k=nb_eigs, M=M1_m, sigma=sigma, mode=mode, which=which, ncv=ncv, tol=1e-10, OPinv=OPinv)
-
-    t_stamp = time_count(t_stamp)
-    print("done. eigenvalues found: " + repr(eigenvalues))
-
-    grid_vals_h1 = lambda v: get_grid_vals_scalar(v, etas, mappings_list, space_kind='h1')
-    grid_vals_hcurl = lambda v: get_grid_vals_vector(v, etas, mappings_list, space_kind='hcurl')
-
-    curl_Pemodes_vals = []
-    curl_Pemodes_titles = []
-    curl_evalues = []
-    other_evalues = []
-
-    k_eig = 0
-    nb_eigs_found = 0   # we only look for curl-curl eigenmodes
-    while k_eig < nb_eigs:
-        t_stamp = time_count(t_stamp)
-        print('looking at emode k = ', repr(k_eig), '... ')
-        evalue  = eigenvalues[k_eig]
-        emode_sp = np.real(eigenvectors[:,k_eig])
-        aux_div_emode = div_aux_m.dot(emode_sp)
-        ampl_aux_div_emode = np.dot(aux_div_emode, aux_div_emode)/np.dot(emode_sp,emode_sp)
-        print('rel amplitude of aux_div_emode: ', repr(ampl_aux_div_emode))
-
-        # normalize mode in L2
-        Me = M1_m.dot(emode_sp)
-        norm_emode = np.dot(emode_sp,Me)
-        print('norm of computed eigenmode: ', norm_emode)
-
-        emode = FemField(V1h, coeffs=array_to_stencil(emode_sp/norm_emode, V1h.vector_space))
-        cP_emode = FemField(V1h, coeffs=array_to_stencil(cP1_m.dot(emode_sp), V1h.vector_space))
-        curl_emode = FemField(V2h, coeffs=array_to_stencil(D1_m.dot(emode_sp), V2h.vector_space))
-        # psydac version (ok if operators are there):
-        # cP_emode_c = cP1(emode)
-        # curl_emode = D1(emode)
-
-        eh_x_vals, eh_y_vals = grid_vals_hcurl(emode)
-        cPeh_x_vals, cPeh_y_vals = grid_vals_hcurl(cP_emode)
-        Peh_abs_vals = [np.sqrt(abs(Pex)**2 + abs(Pey)**2) for Pex, Pey in zip(cPeh_x_vals, cPeh_y_vals)]
-        jumps_eh_vals = [np.sqrt(abs(ex-Pex)**2 + abs(ey-Pey)**2)
-                         for ex, Pex, ey, Pey in zip (eh_x_vals, cPeh_x_vals, eh_y_vals, cPeh_y_vals)]
-        curl_eh_vals = grid_vals_h1(curl_emode)
-
-        if ampl_aux_div_emode < 1e-5:
-            print('seems to be a curl-curl eigenmode.')
-
-            if nb_eigs_found < 8:
-                curl_Pemodes_vals.append(Peh_abs_vals)
-                curl_Pemodes_titles.append(r'$\sigma=$'+'{0:0.2f}'.format(np.real(evalue)))
-                curl_evalues.append(np.real(evalue))
-            else:
-                print('warning: not plotting eigenmode for nb_eigs_found = ' + repr(nb_eigs_found))
-
-            nb_eigs_found += 1
-            is_curl_curl = 'Yes'
-        else:
-            print('does not seem to be a curl-curl eigenmode.')
-            other_evalues.append(np.real(evalue))
-            is_curl_curl = 'No'
-
-        if show_all:
-            if fem_name:
-                fig_name=plot_dir+'HL_emode_k='+repr(k_eig)+'.png'  # +'_'+fem_name+'.png'
-                fig_name_vf=plot_dir+'HL_emode_k='+repr(k_eig)+'_vf.png'   # +'_vf_'+fem_name+'.png'
-            else:
-                fig_name=None
-                fig_name_vf=None
-
-            print('len(Peh_abs_vals) = ',len(Peh_abs_vals))
-            if ext_plots:
-                title=('mode k:'+repr(k_eig)+' -- eigenvalue: '+repr(evalue)+' -- is curl_curl: '+is_curl_curl)
-                vals=[eh_x_vals, eh_y_vals, Peh_abs_vals, jumps_eh_vals, curl_eh_vals]
-                titles=[r'$e^h_{k,x}$', r'$e^h_{k,y}$', r'$|P^1_c e^h_k|$', r'$|(I-P^1_c) e^h_k|$', r'curl$(e^h_k)$']
-            else:
-                # lambda is std notation for eigenvalue
-                title=('eigenmode for $\lambda_{k,h}$ = '+repr(evalue))
-                vals=[Peh_abs_vals,]
-                titles=[r'$|P^1_c e_{k,h}|$',]
-
-            my_small_plot(
-                title=title,
-                vals=vals,
-                titles=titles,
-                xx=xx,
-                yy=yy,
-                cmap='hsv',
-                save_fig=fig_name,
-                dpi=dpi,
-                show_xylabel=False,
-            )
-
-            my_small_streamplot(
-                title=title,
-                vals_x=eh_x_vals,
-                vals_y=eh_y_vals,
-                xx=xx,
-                yy=yy,
-                save_fig=fig_name_vf,
-                dpi=dpi_vf
-            )
-
-        k_eig += 1
-
-    if fem_name:
-        fig_name=plot_dir+'HL_emodes.png'  # _'+fem_name+'.png'
-    else:
-        fig_name=None
-    my_small_plot(
-        title=r'Amplitude $|P^1_c e^h_k|$ of some curl eigenmodes for ncells = {nc} and degree = {deg}'.format(nc=ncells[0],deg=degree[0]),
-        vals=curl_Pemodes_vals,
-        titles=curl_Pemodes_titles,
-        xx=xx,
-        yy=yy,
-        cmap='magma',
-        save_fig=fig_name,
-        show_xylabel=False,
-    )
-
-
-    t_stamp = time_count(t_stamp)
-    print('done -- summary: ')
-
-    print("using jump penalization factor gamma_jump = ", gamma_jump )
-    print('nb of spline cells per patch: ' + repr(ncells))
-    h = 1/ncells[0]
-    print('-- corresponding to h: '+ repr(h))
-    print('degree: ' + repr(degree))
-
-    nb_dofs = len(emode_sp)
-    print(' -- nb of DOFS: ' + repr(nb_dofs))
-
-    print('computed eigenvalues for curl curl operator: ')
-    print(curl_evalues)
-
-    print('other eigenvalues (for grad div) operator: ')
-    print(other_evalues)
-
-    if ref_sigmas is not None:
-        errors = []
-        n_errs = min(len(ref_sigmas), len(curl_evalues))
-        for k in range(n_errs):
-            errors.append(abs(curl_evalues[k]-ref_sigmas[k]))
-
-        print('errors from reference eigenvalues: ')
-        print(errors)
+    compute_eigenvalues(t_stamp, domain, mappings_list, gamma_jump, sigma, ref_sigmas, 
+                        A_m, M1_m, div_aux_m, cP1_m, D1_m, nb_eigs, derham_h, 
+                        ncells, degree, etas, xx, yy, ext_plots, fem_name, test_harmonic_field, nitsche=True)
 
 #----------------------------------------------------------------------------------------------------------------------------------
 def run_maxwell_2d_eigenproblem_conga(nb_eigs, ncells, degree, gamma_jump,
@@ -715,6 +490,17 @@ def run_maxwell_2d_eigenproblem_conga(nb_eigs, ncells, degree, gamma_jump,
             + gamma_jump * jump_penal_m.transpose() * M1_m * jump_penal_m
     )
 
+    compute_eigenvalues(t_stamp, domain, mappings_list, gamma_jump, sigma, ref_sigmas, 
+                        A_m, M1_m, div_aux_m, cP1_m, D1_m, nb_eigs, derham_h, 
+                        ncells, degree, etas, xx, yy, ext_plots, fem_name, test_harmonic_field, nitsche=False)
+
+def compute_eigenvalues(t_stamp, domain, mappings_list, gamma_jump, sigma, ref_sigmas, 
+                        A_m, M1_m, div_aux_m, cP1_m, D1_m, nb_eigs, derham_h, 
+                        ncells, degree, etas, xx, yy, ext_plots, fem_name, test_harmonic_field, nitsche):
+
+    V0h = derham_h.V0
+    V1h = derham_h.V1
+    V2h = derham_h.V2
 
     if test_harmonic_field:
         print("testing harmonic field (for debugging purposes)...")
@@ -781,10 +567,11 @@ def run_maxwell_2d_eigenproblem_conga(nb_eigs, ncells, degree, gamma_jump,
     print('A_m.shape = ', A_m.shape)
 
     print('computing eigenvalues and eigenvectors with scipy.sparse.eigsh...' )
+    sigma_ref = ref_sigmas[len(ref_sigmas)//2] if nitsche else 0
     if A_m.shape[0] < 17000:   # max value for super_lu is >= 13200
         print('(with super_lu decomposition)')
-        eigenvalues, eigenvectors = eigsh(A_m, k=nb_eigs, M=M1_m, sigma=ref_sigmas[len(ref_sigmas)//2], mode=mode, which=which, ncv=ncv)
-        print(eigenvalues)
+        
+        eigenvalues, eigenvectors = eigsh(A_m, k=nb_eigs, M=M1_m, sigma=sigma_ref, mode=mode, which=which, ncv=ncv)
     else:
         # from https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.eigsh.html:
         # the user can supply the matrix or operator OPinv, which gives x = OPinv @ b = [A - sigma * M]^-1 @ b.
@@ -829,8 +616,8 @@ def run_maxwell_2d_eigenproblem_conga(nb_eigs, ncells, degree, gamma_jump,
         norm_emode = np.dot(emode_sp,Me)
         print('norm of computed eigenmode: ', norm_emode)
 
-        emode = FemField(V1h, coeffs=array_to_stencil(emode_sp/norm_emode, V1h.vector_space))
-        cP_emode = FemField(V1h, coeffs=array_to_stencil(cP1_m.dot(emode_sp), V1h.vector_space))
+        emode      = FemField(V1h, coeffs=array_to_stencil(emode_sp/norm_emode, V1h.vector_space))
+        cP_emode   = FemField(V1h, coeffs=array_to_stencil(cP1_m.dot(emode_sp), V1h.vector_space))
         curl_emode = FemField(V2h, coeffs=array_to_stencil(D1_m.dot(emode_sp), V2h.vector_space))
         # psydac version (ok if operators are there):
         # cP_emode_c = cP1(emode)
@@ -1031,7 +818,7 @@ if __name__ == '__main__':
     # nc = 20; deg = 8  # OK --
     # nc=20
     # nc=8
-    nc=10
+    #nc=10
 
     # (nc, deg = 30, 2 is too large for super_lu)
 
