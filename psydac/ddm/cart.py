e7545383deef0c55c9f22031b280cbd51c8c2680
saidctb <said-ctb@live.fr>
2021-10-20 05:05:29 +0200

    add parallel to blockvector

--- psydac/ddm/cart.py
+++ psydac/ddm/cart.py
@@ -575,7 +575,7 @@ class CartDataExchanger:
 
         # Shortcuts
         cart = self._cart
-        comm = cart._comm_cart
+        comm = self._comm
 
         # Choose non-negative invertible function tag(disp) >= 0
         # NOTES:
--- psydac/linalg/block.py
+++ psydac/linalg/block.py
@@ -87,6 +87,10 @@ class BlockVectorSpace( VectorSpace ):
         return self._spaces
 
     @property
+    def parallel( self ):
+        return self._spaces[0].parallel
+
+    @property
     def starts( self ):
         return [s.starts for s in self._spaces]
 
