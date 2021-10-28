20a41bce1e0b5029e352a6bb4f414a27f83c97d5
saidctb <said-ctb@live.fr>
2021-10-28 07:54:09 +0200

    Revert "use comm_cart"
    This reverts commit 7275541bfa1db5cb0b6e0dcd61b500c3b21487e5.

--- psydac/ddm/cart.py
+++ psydac/ddm/cart.py
@@ -587,7 +587,7 @@ class CartDataExchanger:
 
         # Shortcuts
         cart = self._cart
-        comm = cart._comm_cart
+        comm = self._comm_cart
 
         # Choose non-negative invertible function tag(disp) >= 0
         # NOTES:
