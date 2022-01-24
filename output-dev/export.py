78c28e699c53450c24ef81f9e678ab2714a02010
Antoine Lavandier <antoine.lavandier@ecl19.ec-lyon.fr>
2022-01-24 15:43:06 +0100

    OutputManager class in api/postprocessing + example and results in output-dev

--- output-dev/export.py
+++ output-dev/export.py
@@ -1,10 +1,8 @@
+
+
 import os
 import pytest
 import numpy as np
-import h5py as h5
-import yaml
-import re
-
 from sympy import pi, cos, sin, sqrt, exp, ImmutableDenseMatrix as Matrix, Tuple, lambdify
 from scipy.sparse.linalg import spsolve
 from scipy.sparse.linalg import gmres as sp_gmres
@@ -27,6 +25,7 @@ from sympde.core     import Constant
 from sympde.expr     import TerminalExpr
 
 from psydac.api.essential_bc   import apply_essential_bc
+from psydac.api.postprocessing import OutputManager
 from psydac.fem.basic          import FemField
 from psydac.fem.vector         import ProductFemSpace
 from psydac.core.bsplines      import make_knots
@@ -38,232 +37,197 @@ from psydac.api.settings       import PSYDAC_BACKEND_GPYCCEL
 from psydac.utilities.utils    import refine_array_1d, animate_field, split_space, split_field
 from psydac.linalg.iterative_solvers import cg, pcg, bicg, lsmr
 
+def get_boundaries(*args):
 
-from mpi4py import MPI
-# comm = MPI.COMM_WORLD
-
-
-import re
-def export(self,**fields):
-
-    assert all(field.space is self for field in fields.values())
-
-    V = self.vector_space
-    comm = V.cart.comm if V.parallel else None
-
-    # Get space info
-    # To do, use geometry to get the patch number
-    patch = 'patch_1'
-    #Space information
-
-    ldim = self.ldim
-
-    symbolic_space = self.symbolic_space
-    name_space = symbolic_space.name
-    pdim = symbolic_space.domain.dim
-    kind = symbolic_space.kind
-    periodic = self.periodic
-    degree = [self.spaces[i].degree for i in range(ldim)]
-    basis = [self.spaces[i].basis for i in range(ldim)]
-    knots = [self.spaces[i].knots.tolist() for i in range(ldim)]
-
-    new_space = {'name': name_space, 'kind': str(kind), 'dtype': 'float', 'rational': False, 'periodic': periodic,
-                 'degree': degree, 'basis': basis, 'knots': knots
-                 }
-
-    # YAML
-    try:
-        f = open("spaces.yml",'r+')
-        current = yaml.safe_load(f)
-        f.close()
-    except:
-        current = None
-
-    if current is not None: #Make sure we aren't overwriting data
-        #assert current['ldim'] == ldim
-        assert current['ndim'] == pdim
-        try:
-            patch_index = [current['patches'][i]['name'] for i in range(len(current['patches']))].index(patch,-1)
-        except ValueError:
-            patch_index = -1
-        if patch_index != -1:
-            assert all(current['patches'][patch_index]['scalar_spaces'][i]['name'] != name_space
-                       for i in range(len(current['patches'][patch_index]['scalar_spaces'])))
-            current['patches'][patch_index]['scalar_spaces'].append(new_space)
-        else:
-            current['patches'].append({'name' : patch, 'scalar_spaces': [new_space]})
-
+    if not args:
+        return ()
     else:
-        current = {'ndim' : pdim, 'patches':[{'name' : patch, 'scalar_spaces': [new_space]}]}
+        assert all(1 <= a <= 4 for a in args)
+        assert len(set(args)) == len(args)
+
+    boundaries = {1: {'axis': 0, 'ext': -1},
+                  2: {'axis': 0, 'ext':  1},
+                  3: {'axis': 1, 'ext': -1},
+                  4: {'axis': 1, 'ext':  1}}
+
+    return tuple(boundaries[i] for i in args)
+
+def scipy_solver(M, b):
+    x  = spsolve(M.tosparse().tocsr(), b.toarray())
+    x  = array_to_stencil(x, b.space)
+    return x,0
+
+def psydac_solver(M, b):
+    return lsmr(M, M.T, b, maxiter=10000, tol=1e-10)
 
 
-    with open('spaces.yml','w') as fout:
-        yaml.dump(current,fout,default_flow_style=None, sort_keys=False)
+def run_time_dependent_navier_stokes_2d(filename, dt_h, nt, newton_tol=1e-4, max_newton_iter=50, scipy=True):
+    """
+        Time dependent Navier Stokes solver in a 2d domain.
+        this example was taken from the pyiga library
+        https://github.com/c-f-h/pyiga/blob/master/notebooks/solve-navier-stokes.ipynb
+    """
+    domain  = Domain.from_file(filename)
+
+    # ... abstract model
+    V1 = VectorFunctionSpace('V1', domain, kind='H1')
+    V2 = ScalarFunctionSpace('V2', domain, kind='L2')
+    X  = ProductSpace(V1, V2)
+
+    u, v = elements_of(V1, names='u, v')
+    p, q = elements_of(V2, names='p, q')
+
+    x, y  = domain.coordinates
+    int_0 = lambda expr: integral(domain , expr)
+
+    u0 = element_of(V1, name='u0')
+    du = element_of(V1, name='du')
+    dp = element_of(V2, name='dp')
+
+    # time step
+    dt = Constant(name='dt')
+
+    # boundaries
+    boundary_h = Union(*[domain.get_boundary(**kw) for kw in get_boundaries(3,4)])
+    boundary   = Union(*[domain.get_boundary(**kw) for kw in get_boundaries(1)])
+    ue         = Tuple(40*y*(0.5-y)*exp(-100*(y-0.25)**2), 0)
+    bc         = [EssentialBC(du, ue, boundary), EssentialBC(du, 0, boundary_h)]
+
+    # Reynolds number
+    Re = 1e4
+
+    # define the linearized navier stokes
+    a = BilinearForm(((du,dp),(v, q)), integral(domain, dot(du,v) + dt*dot(Transpose(grad(du))*u, v) + dt*dot(Transpose(grad(u))*du, v)
+                                                      + dt*Re**-1*inner(grad(du), grad(v)) - dt*div(du)*q - dt*dp*div(v) + dt*1e-10*dp*q) )
+    l = LinearForm((v, q),             integral(domain, dot(u,v)-dot(u0,v) + dt*dot(Transpose(grad(u))*u, v)
+                                                      + dt*Re**-1*inner(grad(u), grad(v)) - dt*div(u)*q - dt*p*div(v) + dt*1e-10*p*q) )
+
+    # use the stokes equation to compute the initial solution
+    a_stokes = BilinearForm(((du,dp),(v, q)), integral(domain, Re**-1*inner(grad(du), grad(v)) - div(du)*q - dp*div(v) + 1e-10*dp*q) )
+    l_stokes = LinearForm((v, q), integral(domain, dot(v,Tuple(0,0)) ))
+
+    equation        = find((du, dp), forall=(v, q), lhs=a((du, dp), (v, q)), rhs=l(v, q), bc=bc)
+    equation_stokes = find((du, dp), forall=(v, q), lhs=a_stokes((du, dp), (v, q)), rhs=l_stokes(v, q), bc=bc)
+
+    # Define (abstract) norms
+    l2norm_du  = Norm(Matrix([du[0],du[1]]), domain, kind='l2')
+    l2norm_dp  = Norm(dp     , domain, kind='l2')
+
+    # ... create the computational domain from a topological domain
+    domain_h = discretize(domain, filename=filename)
+
+    # ... discrete spaces
+    V1h = discretize(V1, domain_h)
+    V2h = discretize(V2, domain_h)
+    Xh  = V1h*V2h
+
+    # ... discretize the equations
+    equation_h        = discretize(equation,        domain_h, [Xh, Xh], backend=PSYDAC_BACKEND_GPYCCEL)
+    equation_stokes_h = discretize(equation_stokes, domain_h, [Xh, Xh], backend=PSYDAC_BACKEND_GPYCCEL)
+
+    a_h        = equation_h.lhs
+    l_h        = equation_h.rhs
+
+    # Discretize the norms
+    l2norm_du_h = discretize(l2norm_du, domain_h, V1h, backend=PSYDAC_BACKEND_GPYCCEL)
+    l2norm_dp_h = discretize(l2norm_dp, domain_h, V2h, backend=PSYDAC_BACKEND_GPYCCEL)
+
+    # compute the initial solution
+    equation_stokes_h.set_solver('bicg', tol=1e-15)
+    x0 = equation_stokes_h.solve()
 
 
+    u0_h = FemField(V1h)
+    p0_h = FemField(V2h)
 
-    #HDF5
-    # Multi-dimensional index range local to process
-    index = tuple(slice(s, e + 1) for s, e in zip(V.starts, V.ends))
+    u_h  = FemField(V1h)
+    p_h  = FemField(V2h)
 
-    # Create HDF5 file (in parallel mode if MPI communicator size > 1)
-    kwargs = {}
-    if comm is not None:
-        if comm.size > 1:
-            kwargs.update(driver='mpio', comm=comm)
-    fh5 = h5.File("fields.h5", mode='a', **kwargs)
+    du_h = FemField(V1h)
+    dp_h = FemField(V2h)
 
-    regexp = re.compile('snapshot_(?P<id>\d+)')
-    try:
-        i = max([int(regexp.search(k).group('id')) for k in fh5.keys() if regexp.search(k) is not None]) + 1
-    except:
-        i = 0
-    snapshot = fh5.create_group(f'snapshot_{i:0>4}')
-    snapshot.create_group(f'{patch}/{name_space}')
-    snapshot.attrs.create('t', data=0., dtype=float)
-    snapshot.attrs.create('ts', data=0, dtype=int)
+    # First guess
+    u_h[0].coeffs[:,:] = x0[0].coeffs[:,:]
+    u_h[1].coeffs[:,:] = x0[1].coeffs[:,:]
+    p_h.coeffs[:,:]    = x0[2].coeffs[:,:]
 
+    # store the solutions
+    solutions                    = [FemField(V1h)]
+    solutions[-1][0].coeffs[:,:] = u_h[0].coeffs[:,:]
+    solutions[-1][1].coeffs[:,:] = u_h[1].coeffs[:,:]
+    Tf = dt_h*(nt+1)
+    t  = 0
 
-    # Add field coefficients as named datasets
-    for name_field, field in fields.items():
-        dset = snapshot.create_dataset(f'{name_field}', shape=V.npts, dtype=V.dtype)
-        dset[index] = field.coeffs[index]
+    solver = scipy_solver if scipy else psydac_solver
 
+    #Output Management
+    O = OutputManager('spaces_test.yml','fields_test.h5')
+    O.add_spaces(V1h, V2h) #Xh)
+    O.export_fields(0., 0, u0 = u0_h, p0 = p0_h, u = u_h, p = p_h, du = du_h, dp =dp_h)
+    ts = 0
 
-    # Close HDF5 file
-    fh5.close()
+    while t<Tf:
+        ts += 1
+        t  += dt_h
+        print()
+        print('======= time {}/{} ======='.format(t,Tf),flush = True)
 
+        u0_h[0].coeffs[:,:] = u_h[0].coeffs[:,:]
+        u0_h[1].coeffs[:,:] = u_h[1].coeffs[:,:]
+        p0_h.coeffs[:,:]    = p_h.coeffs[:,:]
 
+        # Newton iteration
+        for n in range(max_newton_iter):
+            print()
+            print('==== iteration {} ===='.format(n), flush = True)
 
-class Output_manager():
-    def __init__(self,*spaces):
+            M = a_h.assemble(u=u_h, p=p_h, dt=dt_h)
+            b = l_h.assemble(u=u_h, p=p_h, u0=u0_h, dt=dt_h)
 
-        self._spaces_info = {}
-        self._spaces      = []
+            apply_essential_bc(M, *equation_h.bc, identity=True)
+            apply_essential_bc(b, *equation_h.bc)
 
-        self._next_snapshot_number = 0
+            x,info = solver(M, b)
 
+            du_h[0].coeffs[:] = x[0][:]
+            du_h[1].coeffs[:] = x[1][:]
+            dp_h.coeffs[:]    = x[2][:]
 
-        self.add_space(*spaces)
+            # Compute L2 norm of increment
+            l2_error_du = l2norm_du_h.assemble(du=du_h)
+            l2_error_dp = l2norm_dp_h.assemble(dp=dp_h)
 
+            print('L2_error_norm(du) = {}'.format(l2_error_du))
+            print('L2_error_norm(dp) = {}'.format(l2_error_dp))
 
+            if abs(l2_error_du) <= newton_tol and abs(l2_error_dp) <= newton_tol:
+                print()
+                print('CONVERGED')
+                break
+            elif n == max_newton_iter-1 or abs(l2_error_du)>1/newton_tol or abs(l2_error_dp) > 1/newton_tol:
+                print()
+                print('NOT CONVERGED')
+                t = Tf
+                return solutions, p_h, domain, domain_h
 
-    def add_space(self, *femspaces):
-        spaces_info = self._spaces_info
-        for femspace in femspaces:
+            # update field
+            u_h -= du_h
+            p_h -= dp_h
 
-            # To do, use geometry to get the patch number
-            patch = 'patch_0'
+        solutions.append(FemField(V1h))
+        solutions[-1][0].coeffs[:,:] = u_h[0].coeffs[:,:]
+        solutions[-1][1].coeffs[:,:] = u_h[1].coeffs[:,:]
 
-            # Space information
-            symbolic_space = femspace.symbolic_space
-            vector_space   = femspace.vector_space
+        O.export_fields(t, ts, u0=u0_h, p0=p0_h, u=u_h, p=p_h, du=du_h, dp=dp_h)
 
-            femspace_name = symbolic_space.name
-            pdim          = symbolic_space.domain.dim
-            ldim          = femspace.ldim
-            kind          = symbolic_space.kind
-            dtype         = str(vector_space.dtype)
-            periodic      = femspace.periodic
-            degree        = [femspace.spaces[i].degree for i in range(ldim)]
-            basis         = [femspace.spaces[i].basis for i in range(ldim)]
-            knots         = [femspace.spaces[i].knots.tolist() for i in range(ldim)]
+    O.export_space_info()
+    return solutions, p_h, domain, domain_h
 
-            new_space = {'name': femspace_name, 'ldim':  ldim,
-                         'kind': str(kind), 'dtype': dtype, 'rational': False,
-                         'periodic': periodic,
-                         'degree': degree, 'basis': basis, 'knots': knots
-                         }
-            if spaces_info == {}:
-                spaces_info = {'ndim' : pdim,
-                               'patches':[{'name' : patch,
-                                           'scalar_spaces': [new_space]}]}
-            else:
-                assert spaces_info['ndim'] == pdim
-                try:
-                    patch_index = [spaces_info['patches'][i]['name']
-                                   for i in range(len(spaces_info['patches']))].index(patch)
-                except ValueError:
-                    patch_index = -1
+if __name__ == '__main__':
 
-                if patch_index != -1:
-                    assert all(spaces_info['patches'][patch_index]['scalar_spaces'][i]['name'] != femspace_name
-                               for i in range(len(spaces_info['patches'][patch_index]['scalar_spaces'])))
+    filename = '../mesh/bent_pipe.h5'
 
-                    spaces_info['patches'][patch_index]['scalar_spaces'].append(new_space)
-                else:
-                    spaces_info['patches'].append({'name': patch, 'scalar_spaces': [new_space]})
-            self._spaces.append(femspace)
-        self._spaces_info = spaces_info
-
-    def add_fields(self, filename,t , ts, **fields):
-        assert isinstance(filename, str)
-        assert all(f.space in self._spaces for f in fields.values())
-        # should we restrict to only fields belonging to the same space ?
-
-        # For now, I assume that if something is mpi parallel everything is
-        space_test = list(fields.values())[0].space.vector_space
-        comm = space_test.cart.comm if space_test.parallel else None
-
-        # Create HDF5 file (in parallel mode if MPI communicator size > 1)
-        kwargs = {}
-        if comm is not None:
-            if comm.size > 1:
-                kwargs.update(driver='mpio', comm=comm)
-        fh5 = h5.File(filename, mode='a', **kwargs)
-
-        # Unsure about this
-        #regexp = re.compile('snapshot_(?P<id>\d+)')
-        #try:
-        #    i = max([int(regexp.search(k).group('id')) for k in fh5.keys() if regexp.search(k) is not None]) + 1
-        #except:
-        #     i = 0
-
-        i = self._next_snapshot_number
-
-        snapshot = fh5.create_group(f'snapshot_{i:0>4}')
-        snapshot.attrs.create('t', data=t, dtype=float)
-        snapshot.attrs.create('ts', data=ts, dtype=int)
-
-        self._next_snapshot_number += 1
-
-        # Add field coefficients as named datasets
-        for name_field, field in fields.items():
-            name_space = field.space.symbolic_space.name
-            patch = 'patch_0'
-            V = field.space.vector_space
-            index = tuple(slice(s, e + 1) for s, e in zip(V.starts, V.ends))
-            dset = snapshot.create_dataset(f'{patch}/{name_space}/{name_field}', shape=V.npts, dtype=V.dtype)
-            dset[index] = field.coeffs[index]
-
-        # Close HDF5 file
-        fh5.close()
-
-
-    def export_space_info(self, filename):
-        with open(filename, 'w') as f:
-            yaml.dump(self._spaces_info, f, default_flow_style = None, sort_keys = None)
-
-
-# Test
-A = Square('A',bounds1=(0.5, 1.), bounds2=(-1., 0.))
-
-V = ScalarFunctionSpace('V0', A, kind=None)
-ne = [2 ** 2, 2 ** 2]
-degree = [2, 2]
-Ah = discretize(A, ncells=ne, comm=None)
-Vh = discretize(V, Ah, degree=degree)
-uh = FemField(Vh)
-
-O = Output_manager(Vh)
-O.add_fields('test2.h5', 0., 0, u0 = uh)
-
-V1 = ScalarFunctionSpace('V1', A, kind='L2')
-V1h = discretize(V1, Ah, degree = degree)
-
-O.add_space(V1h)
-
-O.export_space_info('test2.yml')
-
-O.add_fields('test2.h5', 1., 1, u0 = uh)
-
+    Tf   = 0.30
+    dt_h = 0.05
+    nt   = Tf//dt_h
+    solutions, p_h, domain, domain_h = run_time_dependent_navier_stokes_2d(filename, dt_h=dt_h, nt=nt, scipy=False)
--- psydac/api/postprocessing.py
+++ psydac/api/postprocessing.py
@@ -2,8 +2,13 @@
 #
 # Copyright 2019 Yaman Güçlü
 
+import yaml
+import re
+import h5py as h5
+
 from psydac.cad.geometry    import Geometry
 from psydac.utilities.utils import refine_array_1d
+from psydac.fem.basic import FemSpace
 
 #==============================================================================
 def get_grid_lines_2d(domain_h, V_h, *, refine=1):
@@ -79,3 +84,180 @@ def get_grid_lines_2d(domain_h, V_h, *, refine=1):
         isolines_2.append( dict(eta2=e2, x=x, y=y) )
 
     return isolines_1, isolines_2
+
+class OutputManager():
+    def __init__(self,filename_space,filename_fields,*spaces):
+
+        self._spaces_info          = {}
+        self._spaces               = []
+        self.filename_space        = filename_space
+        self.filename_fields       = filename_fields
+        self._next_snapshot_number = 0
+        self.add_spaces(*spaces)
+
+    def add_spaces(self, *femspaces):
+        for femspace in femspaces:
+            if femspace.is_product:
+                self._add_vector_space(femspace)
+            else:
+                self._add_scalar_space(femspace)
+
+    def _add_scalar_space(self,scalar_space,name = None, dim = None, patch_name = None, kind = None):
+        assert isinstance(scalar_space, FemSpace)
+        spaces_info = self._spaces_info
+
+        if name is None and dim is None:
+            symbolic_space = scalar_space.symbolic_space
+            scalar_space_name = symbolic_space.name
+            pdim = symbolic_space.domain.dim
+            patch = symbolic_space.domain.name
+            kind = symbolic_space.kind
+        else:
+            scalar_space_name = name
+            pdim = dim
+            patch = patch_name
+            kind = kind
+
+        ldim = scalar_space.ldim
+        vector_space = scalar_space.vector_space
+        dtype = str(vector_space.dtype)
+        periodic = scalar_space.periodic
+        degree = [scalar_space.spaces[i].degree for i in range(ldim)]
+        basis = [scalar_space.spaces[i].basis for i in range(ldim)]
+        knots = [scalar_space.spaces[i].knots.tolist() for i in range(ldim)]
+
+        new_space = {'name': scalar_space_name, 'ldim': ldim,
+                     'kind': str(kind), 'dtype': dtype, 'rational': False,
+                     'periodic': periodic,
+                     'degree': degree, 'basis': basis, 'knots': knots
+                     }
+        if spaces_info == {}:
+            spaces_info = {'ndim': pdim,
+                           'patches': [{'name': patch,
+                                        'scalar_spaces': [new_space]
+                                        }]
+                           }
+        else:
+            assert spaces_info['ndim'] == pdim
+            try:
+                patch_index = [spaces_info['patches'][i]['name']
+                               for i in range(len(spaces_info['patches']))].index(patch)
+            except ValueError:
+                patch_index = -1
+
+            if patch_index != -1:
+                assert all(spaces_info['patches'][patch_index]['scalar_spaces'][i]['name'] != scalar_space_name
+                           for i in range(len(spaces_info['patches'][patch_index]['scalar_spaces'])))
+
+                spaces_info['patches'][patch_index]['scalar_spaces'].append(new_space)
+            else:
+                spaces_info['patches'].append({'name': patch, 'scalar_spaces': [new_space]})
+        self._spaces.append(scalar_space)
+
+        self._spaces_info = spaces_info
+
+        return new_space
+
+    def _add_vector_space(self,vector_space):
+
+        assert isinstance(vector_space, FemSpace)
+
+
+
+        symbolic_space = vector_space.symbolic_space
+        name = symbolic_space.name
+        dim = symbolic_space.domain.dim
+        patch_name = symbolic_space.domain.name
+        kind = symbolic_space.kind
+
+        scalar_spaces_info = []
+        for i,scalar_space in enumerate(vector_space.spaces):
+            sc_space_info = self._add_scalar_space(scalar_space,
+                                                   name = name+f'[{i}]',
+                                                   dim = dim,
+                                                   patch_name = patch_name,
+                                                   kind = kind)
+            scalar_spaces_info.append(sc_space_info)
+
+        spaces_info = self._spaces_info
+
+        new_vector_space = {'name': name, 'components': scalar_spaces_info}
+
+        patch_index = [spaces_info['patches'][i]['name']
+                       for i in range(len(spaces_info['patches']))].index(patch_name)
+
+        try:
+            spaces_info['patches'][patch_index]['vector_spaces'].append(new_vector_space)
+        except:
+            spaces_info['patches'][patch_index].update({'vector_spaces': [new_vector_space]})
+
+        self._spaces_info = spaces_info
+        self._spaces.append(vector_space)
+
+
+    def export_fields(self,t , ts, **fields):
+        '''
+        A function that exports the fields' coefficients to an HDF5 file. They are
+        saved under a snapshot/patch/space/field scheme. One snapshot corresponds to one call
+        to this method.
+
+        Parameters
+        ----------
+          t: `float`
+            Time of the export
+          ts: `int`
+            Timestep corresponding to the export.
+          fields: `dict`
+            List of named fields
+
+        '''
+        assert all(f.space in self._spaces for f in fields.values())
+        # should we restrict to only fields belonging to the same space ?
+
+        # For now, I assume that if something is mpi parallel everything is
+        space_test = list(fields.values())[0].space.vector_space
+        comm = space_test.cart.comm if space_test.parallel else None
+
+        # Create HDF5 file (in parallel mode if MPI communicator size > 1)
+        kwargs = {}
+        if comm is not None:
+            if comm.size > 1:
+                kwargs.update(driver='mpio', comm=comm)
+        fh5 = h5.File(self.filename_fields, mode='a', **kwargs)
+
+        i = self._next_snapshot_number
+        try:
+            snapshot = fh5.create_group(f'snapshot_{i:0>4}')
+        except:
+            regexp = re.compile('snapshot_(?P<id>\d+)')
+            i = max([int(regexp.search(k).group('id')) for k in fh5.keys() if regexp.search(k) is not None]) + 1
+            snapshot = fh5.create_group(f'snapshot_{i:0>4}')
+        snapshot.attrs.create('t', data=t, dtype=float)
+        snapshot.attrs.create('ts', data=ts, dtype=int)
+
+        self._next_snapshot_number = i + 1
+
+        # Add field coefficients as named datasets
+        for name_field, field in fields.items():
+            name_space = field.space.symbolic_space.name
+            patch = field.space.symbolic_space.domain.name
+            if field.space.is_product:
+                for i,field_coeff in enumerate(field.coeffs):
+                    name_field_i = name_field+f'[{i}]'
+                    name_space_i = name_space+f'[{i}]'
+                    Vi = field.space.vector_space.spaces[i]
+                    index = tuple(slice(s, e + 1) for s, e in zip(Vi.starts, Vi.ends))
+                    dset = snapshot.create_dataset(f'{patch}/{name_space_i}/{name_field_i}', shape=Vi.npts, dtype=Vi.dtype)
+                    dset[index] = field_coeff[index]
+            else:
+                V = field.space.vector_space
+                index = tuple(slice(s, e + 1) for s, e in zip(V.starts, V.ends))
+                dset = snapshot.create_dataset(f'{patch}/{name_space}/{name_field}', shape=V.npts, dtype=V.dtype)
+                dset[index] = field.coeffs[index]
+
+        # Close HDF5 file
+        fh5.close()
+
+    def export_space_info(self):
+        with open(self.filename_space, 'w') as f:
+            yaml.dump(self._spaces_info, f, default_flow_style = None, sort_keys = None)
