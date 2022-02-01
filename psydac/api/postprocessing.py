21146be218cc7d1b39953384543bffc2ae98a121
Antoine Lavandier <antoine.lavandier@ecl19.ec-lyon.fr>
2022-02-01 17:23:52 +0100

    OuputManager class now ask for explicit saving scheme from the user. Small test included

--- psydac/api/postprocessing.py
+++ psydac/api/postprocessing.py
@@ -10,7 +10,7 @@ from psydac.cad.geometry    import Geometry
 from psydac.utilities.utils import refine_array_1d
 from psydac.fem.basic import FemSpace
 
-#==============================================================================
+# ==============================================================================
 def get_grid_lines_2d(domain_h, V_h, *, refine=1):
     """
     Get the grid lines (i.e. element boundaries) of a 2D computational domain,
@@ -29,7 +29,8 @@ def get_grid_lines_2d(domain_h, V_h, *, refine=1):
         Number of segments used to describe a grid curve in each element
         (minimum value is 1, which yields quadrilateral elements).
 
-    Results
+
+    Returns
     -------
     isolines_1 : list of dict
         Lines having constant value of 'eta1' parameter;
@@ -85,25 +86,143 @@ def get_grid_lines_2d(domain_h, V_h, *, refine=1):
 
     return isolines_1, isolines_2
 
-class OutputManager():
-    def __init__(self,filename_space,filename_fields,*spaces):
 
-        self._spaces_info          = {}
-        self._spaces               = []
-        self.filename_space        = filename_space
-        self.filename_fields       = filename_fields
+# ===========================================================================
+class OutputManager:
+    """A class meant to streamline the exportation of
+    output data.
+
+    Parameters
+    ----------
+    filename_space : str
+        Name of the file in which to save the space information.
+    filename_fields : str
+        Name of the file in which to save the fields.
+    static : boolean or None, optional
+        Whether to initialize the OuputManager in static mode. If None, nothing will be assumed
+        and the user will have to specify whether to use the static mode when calling ``export_fields``.
+
+    Attributes
+    ----------
+
+
+    """
+    def __init__(self, filename_space, filename_fields):
+
+        self._spaces_info = {}
+        self._spaces = []
+
+        if filename_space[-4:] != ".yml" and filename_space[-4:] != ".yaml":
+            filename_space += ".yml"
+        self.filename_space = filename_space
+
+        if filename_fields[-3:] != ".h5":
+            filename_fields += ".h5"
+        self.filename_fields = filename_fields
         self._next_snapshot_number = 0
-        self.add_spaces(*spaces)
+        self.is_static = None
+        self._current_hdf5_group = None
+
+    def set_static(self):
+        """Set the export to static mode
+
+        Returns
+        -------
+        self
+        """
+        if self.is_static:
+            return self
+
+        self.is_static = True
+
+        file_fields = h5.File(self.filename_fields, 'a')
+
+        if 'static' not in file_fields.keys():
+            static_group = file_fields.create_group('static')
+            self._current_hdf5_group = static_group
+        else:
+            self._current_hdf5_group = file_fields['static']
+
+        return self
+
+    def add_snapshot(self, t, ts):
+        """Add a snapshot to the fields' HDF5 file
+        and set the export mode to time dependent.
+
+        Parameters
+        ----------
+        t : float
+            Floating point time of the snapshot
+        ts : int
+            Time step of the snapshot
+
+        Returns
+        -------
+        self
+        """
+        self.is_static = False
+
+        file_fields = h5.File(self.filename_fields, 'a')
+
+        i = self._next_snapshot_number
+        try:
+            snapshot = file_fields.create_group(f'snapshot_{i:0>4}')
+        except ValueError:
+            regexp = re.compile(r'snapshot_(?P<id>\d+)')
+            i = max([int(regexp.search(k).group('id')) for k in file_fields.keys() if regexp.search(k) is not None]) + 1
+            snapshot = file_fields.create_group(f'snapshot_{i:0>4}')
+        snapshot.attrs.create('t', data=t, dtype=float)
+        snapshot.attrs.create('ts', data=ts, dtype=int)
+
+        self._next_snapshot_number = i + 1
+        self._current_hdf5_group = snapshot
+
+        return self
 
     def add_spaces(self, *femspaces):
+        """Add femspaces to the scope of this instance of OutputManager
+
+        Parameters
+        ----------
+        femspaces: Tuple of psydac.fem.basic.FemsSpace
+            Femspaces to add in the scope of this OutputManager instance.
+
+        """
+        assert all(isinstance(femspace, FemSpace) for femspace in femspaces)
         for femspace in femspaces:
+
             if femspace.is_product:
                 self._add_vector_space(femspace)
             else:
                 self._add_scalar_space(femspace)
 
-    def _add_scalar_space(self,scalar_space,name = None, dim = None, patch_name = None, kind = None):
-        assert isinstance(scalar_space, FemSpace)
+    def _add_scalar_space(self, scalar_space, name=None, dim=None, patch_name=None, kind=None):
+        """Add a scalar space to the scope of this instance of OutputManager
+
+        Parameters
+        ----------
+        scalar_space : psydac.fem.tensor.TensorFemSpace
+            Scalar space to add to the scope.
+
+        name : str or None, optional
+            Name under which to save the space. Will be generated
+            by looking at the related symbolic space if not given
+
+        dim : int or None, optional
+            Physical dimension of the related symbolic space.
+            Is read directly if not given.
+
+        patch_name : str or None, optional
+            Name of the patch in which the symbolic domain belongs.
+
+        kind : str or None, optional
+            Kind of the space.
+
+        Returns
+        -------
+        new_space : dict
+            Formatted space info.
+        """
         spaces_info = self._spaces_info
 
         if name is None and dim is None:
@@ -133,6 +252,7 @@ class OutputManager():
                      }
         if spaces_info == {}:
             spaces_info = {'ndim': pdim,
+                           'fields': self.filename_fields,
                            'patches': [{'name': patch,
                                         'scalar_spaces': [new_space]
                                         }]
@@ -158,11 +278,14 @@ class OutputManager():
 
         return new_space
 
-    def _add_vector_space(self,vector_space):
+    def _add_vector_space(self, vector_space):
+        """Add a vector space to the scope of this instance of OutputManager.
 
-        assert isinstance(vector_space, FemSpace)
-
-
+        Parameters
+        ----------
+        vector_space: psydac.fem.vector.VectorFemSpace
+            Vector FemSpace to add to the scope.
+        """
 
         symbolic_space = vector_space.symbolic_space
         name = symbolic_space.name
@@ -171,12 +294,12 @@ class OutputManager():
         kind = symbolic_space.kind
 
         scalar_spaces_info = []
-        for i,scalar_space in enumerate(vector_space.spaces):
+        for i, scalar_space in enumerate(vector_space.spaces):
             sc_space_info = self._add_scalar_space(scalar_space,
-                                                   name = name+f'[{i}]',
-                                                   dim = dim,
-                                                   patch_name = patch_name,
-                                                   kind = kind)
+                                                   name=name+f'[{i}]',
+                                                   dim=dim,
+                                                   patch_name=patch_name,
+                                                   kind=kind)
             scalar_spaces_info.append(sc_space_info)
 
         spaces_info = self._spaces_info
@@ -188,76 +311,83 @@ class OutputManager():
 
         try:
             spaces_info['patches'][patch_index]['vector_spaces'].append(new_vector_space)
-        except:
+        except KeyError:
             spaces_info['patches'][patch_index].update({'vector_spaces': [new_vector_space]})
 
         self._spaces_info = spaces_info
         self._spaces.append(vector_space)
 
-
-    def export_fields(self,t , ts, **fields):
-        '''
+    def export_fields(self, **fields):
+        """
         A function that exports the fields' coefficients to an HDF5 file. They are
-        saved under a snapshot/patch/space/field scheme. One snapshot corresponds to one call
-        to this method.
+        saved under a snapshot/patch/space/field scheme or static/patch/space/field.
+
+        If the saving scheme is time dependent, unsets it to avoid writing twice to
+        the same snapshot.
 
         Parameters
         ----------
-          t: `float`
-            Time of the export
-          ts: `int`
-            Timestep corresponding to the export.
-          fields: `dict`
+        fields : dict
             List of named fields
 
-        '''
+        Raises
+        ------
+        ValueError
+            When self.is_static is None (no saving scheme specified)
+        """
+        if self.is_static is None:
+            raise ValueError('Saving scheme not specified')
+
         assert all(f.space in self._spaces for f in fields.values())
         # should we restrict to only fields belonging to the same space ?
 
         # For now, I assume that if something is mpi parallel everything is
-        space_test = list(fields.values())[0].space.vector_space
-        comm = space_test.cart.comm if space_test.parallel else None
+        space_test = list(fields.values())[0].space
+        if space_test.is_product:
+            comm = space_test.spaces[0].vector_space.cart.comm if space_test.vector_space.parallel else None
+        else:
+            comm = space_test.vector_space.cart.comm if space_test.vector_space.parallel else None
 
-        # Create HDF5 file (in parallel mode if MPI communicator size > 1)
+        # Open HDF5 file (in parallel mode if MPI communicator size > 1)
         kwargs = {}
         if comm is not None:
             if comm.size > 1:
                 kwargs.update(driver='mpio', comm=comm)
         fh5 = h5.File(self.filename_fields, mode='a', **kwargs)
 
-        i = self._next_snapshot_number
-        try:
-            snapshot = fh5.create_group(f'snapshot_{i:0>4}')
-        except:
-            regexp = re.compile('snapshot_(?P<id>\d+)')
-            i = max([int(regexp.search(k).group('id')) for k in fh5.keys() if regexp.search(k) is not None]) + 1
-            snapshot = fh5.create_group(f'snapshot_{i:0>4}')
-        snapshot.attrs.create('t', data=t, dtype=float)
-        snapshot.attrs.create('ts', data=ts, dtype=int)
+        if not 'spaces' in fh5.attrs.keys():
+            fh5.attrs.create('spaces', self.filename_space)
 
-        self._next_snapshot_number = i + 1
+        saving_group = self._current_hdf5_group
 
         # Add field coefficients as named datasets
         for name_field, field in fields.items():
+
             name_space = field.space.symbolic_space.name
             patch = field.space.symbolic_space.domain.name
-            if field.space.is_product:
-                for i,field_coeff in enumerate(field.coeffs):
+
+            if field.space.is_product:  # Vector field case
+                for i, field_coeff in enumerate(field.coeffs):
                     name_field_i = name_field+f'[{i}]'
                     name_space_i = name_space+f'[{i}]'
                     Vi = field.space.vector_space.spaces[i]
                     index = tuple(slice(s, e + 1) for s, e in zip(Vi.starts, Vi.ends))
-                    dset = snapshot.create_dataset(f'{patch}/{name_space_i}/{name_field_i}', shape=Vi.npts, dtype=Vi.dtype)
+
+                    dset = saving_group.create_dataset(f'{patch}/{name_space_i}/{name_field_i}',
+                                                       shape=Vi.npts, dtype=Vi.dtype)
                     dset[index] = field_coeff[index]
             else:
                 V = field.space.vector_space
                 index = tuple(slice(s, e + 1) for s, e in zip(V.starts, V.ends))
-                dset = snapshot.create_dataset(f'{patch}/{name_space}/{name_field}', shape=V.npts, dtype=V.dtype)
+                dset = saving_group.create_dataset(f'{patch}/{name_space}/{name_field}', shape=V.npts, dtype=V.dtype)
                 dset[index] = field.coeffs[index]
 
         # Close HDF5 file
         fh5.close()
 
+        if self.is_static:
+            self.is_static = None
+
     def export_space_info(self):
         with open(self.filename_space, 'w') as f:
-            yaml.dump(self._spaces_info, f, default_flow_style = None, sort_keys = None)
+            yaml.dump(data=self._spaces_info, stream=f, default_flow_style=None, sort_keys=False)
---
+++ psydac/api/tests/test_post_processing.py
@@ -0,0 +1,121 @@
+import pytest
+
+from sympde.topology import Square, ScalarFunctionSpace, VectorFunctionSpace
+from psydac.api.discretization import discretize
+from psydac.fem.basic import FemField
+from psydac.api.postprocessing import OutputManager
+
+def test_OuputManager():
+
+    domain = Square('D')
+    A = ScalarFunctionSpace('A', domain, kind='H1')
+    B = VectorFunctionSpace('B', domain, kind=None)
+
+    domain_h = discretize(domain, ncells=[5,5])
+
+    Ah = discretize(A, domain_h, degree = [3,3])
+    Bh = discretize(B, domain_h, degree = [2,2])
+
+    uh = FemField(Ah)
+    vh = FemField(Bh)
+
+    Om = OutputManager('file.yml','file.h5')
+    Om.add_spaces(Ah, Bh)
+
+    Om.set_static().export_fields(uh=uh, vh=vh)
+
+    Om.add_snapshot(t=0., ts=0).export_fields(uh=uh ,vh=vh)
+    Om.add_snapshot(t=1., ts=1).export_fields(uh=uh, vh=vh)
+
+
+
+    expected_spaces_info = {'ndim': 2,
+                            'fields': 'file.h5',
+                            'patches': [{'name': 'D',
+                                         'scalar_spaces': [{'name': 'A',
+                                                            'ldim': 2,
+                                                            'kind': 'H1SpaceType()',
+                                                            'dtype': "<class 'float'>",
+                                                            'rational': False,
+                                                            'periodic': [False, False],
+                                                            'degree': [3, 3],
+                                                            'basis': ['B', 'B'],
+                                                            'knots': [
+                                                                [0.0, 0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                 0.6000000000000001, 0.8, 1.0, 1.0, 1.0, 1.0],
+                                                                [0.0, 0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                 0.6000000000000001, 0.8, 1.0, 1.0, 1.0, 1.0]
+                                                            ]
+                                                            },
+                                                           {'name': 'B[0]',
+                                                            'ldim': 2,
+                                                            'kind': 'UndefinedSpaceType()',
+                                                            'dtype': "<class 'float'>",
+                                                            'rational': False,
+                                                            'periodic': [False, False],
+                                                            'degree': [2, 2],
+                                                            'basis': ['B', 'B'],
+                                                            'knots': [
+                                                                [0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                 0.6000000000000001, 0.8, 1.0, 1.0, 1.0],
+                                                                [0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                 0.6000000000000001, 0.8, 1.0, 1.0, 1.0]
+                                                            ]
+                                                            },
+                                                           {'name': 'B[1]',
+                                                            'ldim': 2,
+                                                            'kind': 'UndefinedSpaceType()',
+                                                            'dtype': "<class 'float'>",
+                                                            'rational': False,
+                                                            'periodic': [False, False],
+                                                            'degree': [2, 2],
+                                                            'basis': ['B', 'B'],
+                                                            'knots': [
+                                                                [0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                 0.6000000000000001, 0.8, 1.0, 1.0, 1.0],
+                                                                [0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                 0.6000000000000001, 0.8, 1.0, 1.0, 1.0]
+                                                            ]
+                                                            }
+                                                           ],
+                                         'vector_spaces': [{'name': 'B',
+                                                            'components': [
+                                                                {'name': 'B[0]',
+                                                                 'ldim': 2,
+                                                                 'kind': 'UndefinedSpaceType()',
+                                                                 'dtype': "<class 'float'>",
+                                                                 'rational': False,
+                                                                 'periodic': [False, False],
+                                                                 'degree': [2, 2],
+                                                                 'basis': ['B', 'B'],
+                                                                 'knots': [
+                                                                     [0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                      0.6000000000000001, 0.8, 1.0, 1.0, 1.0],
+                                                                     [0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                      0.6000000000000001, 0.8, 1.0, 1.0, 1.0]
+                                                                 ]
+                                                                 },
+                                                                {'name': 'B[1]',
+                                                                 'ldim': 2,
+                                                                 'kind': 'UndefinedSpaceType()',
+                                                                 'dtype': "<class 'float'>",
+                                                                 'rational': False,
+                                                                 'periodic': [False, False],
+                                                                 'degree': [2, 2],
+                                                                 'basis': ['B', 'B'],
+                                                                 'knots': [
+                                                                     [0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                      0.6000000000000001, 0.8, 1.0, 1.0, 1.0],
+                                                                     [0.0, 0.0, 0.0, 0.2, 0.4,
+                                                                      0.6000000000000001, 0.8, 1.0, 1.0, 1.0]
+                                                                 ]
+                                                                 }
+                                                            ]
+                                                            }]
+                                         }]
+                            }
+
+    assert(Om._spaces_info == expected_spaces_info)
+
+if __name__ == '__main__':
+    test_OuputManager()
