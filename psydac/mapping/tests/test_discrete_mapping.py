d5cf7bc333afbd62fe06bbcc892c720cfc463524
Antoine Lavandier <antoine.lavandier@ecl19.ec-lyon.fr>
2022-02-16 19:17:27 +0100

    test

--- psydac/mapping/tests/test_discrete_mapping.py
+++ psydac/mapping/tests/test_discrete_mapping.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pyevtk.hl
 import pytest
 import os
 
@@ -15,7 +16,7 @@ except KeyError:
     mesh_dir = os.path.join(base_dir, 'mesh')
 
 
-@pytest.mark.parametrize('geometry_file', ['collela_2d.h5', 'collela_3d.h5', 'bent_pipe.h5'])
+@pytest.mark.parametrize('geometry_file', ['collela_3d.h5', 'collela_2d.h5', 'bent_pipe.h5'])
 @pytest.mark.parametrize('k', [1, 2, 3])
 def test_build_mesh(geometry_file, k):
     filename = os.path.join(mesh_dir, geometry_file)
@@ -43,9 +44,33 @@ def test_build_mesh(geometry_file, k):
 
             glob_points.append(glob_points_i)
 
-        mesh_fast = mapping.build_mesh(refine_factor=k)
+        x_mesh, y_mesh, z_mesh = mapping.build_mesh(refine_factor=k)
 
-        mesh_slow = np.empty_like(mesh_fast)
+        if mapping.ldim == 2:
+
+            eta1 = [glob_points[0][i // (k + 1)][i % (k + 1)] for i in range(x_mesh.shape[0])]
+            eta2 = [glob_points[1][i // (k + 1)][i % (k + 1)] for i in range(x_mesh.shape[1])]
+
+            pcoords = np.array([[mapping([e1, e2]) for e2 in eta2] for e1 in eta1])
+
+            x_mesh_l = pcoords[..., 0:1]
+            y_mesh_l = pcoords[..., 1:2]
+            z_mesh_l = np.zeros_like(x_mesh_l)
+
+        if mapping.ldim == 3:
+
+            eta1 = [glob_points[0][i // (k + 1)][i % (k + 1)] for i in range(x_mesh.shape[0])]
+            eta2 = [glob_points[1][i // (k + 1)][i % (k + 1)] for i in range(x_mesh.shape[1])]
+            eta3 = [glob_points[2][i // (k + 1)][i % (k + 1)] for i in range(x_mesh.shape[2])]
+
+            pcoords = np.array([[[mapping([e1, e2, e3]) for e3 in eta3] for e2 in eta2] for e1 in eta1])
+
+            x_mesh_l = pcoords[..., 0]
+            y_mesh_l = pcoords[..., 1]
+            z_mesh_l = pcoords[..., 2]
 
 
 
+        assert np.allclose(x_mesh, x_mesh_l)
+        assert np.allclose(y_mesh, y_mesh_l)
+        assert np.allclose(z_mesh, z_mesh_l)
