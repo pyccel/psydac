cb88e44656bb8bba7ed9b8078f04c66405485986
Antoine Lavandier <antoine.lavandier@ecl19.ec-lyon.fr>
2022-03-01 15:22:24 +0100

    Fixes

--- psydac/core/tests/test_kernels.py
+++ psydac/core/tests/test_kernels.py
@@ -5,7 +5,7 @@ import itertools as it
 
 from sympde.topology import Domain
 from psydac.api.discretization import discretize
-
+from psydac.utilities.utils import refine_array_1d
 from psydac.core.kernels import (eval_jacobians_2d, eval_jacobians_3d, eval_jacobians_2d_weights,
                                  eval_jacobians_3d_weights, eval_jacobians_inv_2d_weights,
                                  eval_jacobians_inv_3d_weights, eval_jacobians_inv_2d, eval_jacobians_inv_3d)
@@ -35,15 +35,22 @@ def test_jacobians(geometry_file, ldim, is_nurbs):
 
     mapping = list(domainh.mappings.values())[0]
 
-    ncells, \
+    grid = []
+    ncells = []
+    for i in range(ldim):
+        grid_i_initial = mapping.space.breaks[i]
+        ncells.append(len(grid_i_initial) - 1)
+        grid.append(np.reshape(np.asarray(refine_array_1d(grid_i_initial, 1, remove_duplicates=False)),
+                               (ncells[-1], 2)))
+    n_evals_points = [2] * ldim
+
     pads, \
     degree, \
-    quad_order, \
     global_basis, \
-    global_spans = mapping.space.preprocess(der=1, refine_factor=1)
+    global_spans = mapping.space.preprocess_regular_tensor_grid(grid, der=1)
 
-    jac_mats = np.zeros((*(tuple(ncells[i] * (quad_order[i] + 1) for i in range(ldim))), ldim, ldim))
-    inv_jac_mats = np.zeros((*(tuple(ncells[i] * (quad_order[i] + 1) for i in range(ldim))), ldim, ldim))
+    jac_mats = np.zeros((*(tuple(ncells[i] * n_evals_points[i] for i in range(ldim))), ldim, ldim))
+    inv_jac_mats = np.zeros((*(tuple(ncells[i] * n_evals_points[i] for i in range(ldim))), ldim, ldim))
 
     if is_nurbs:
         global_arr_weights = mapping._weights_field.coeffs._data
@@ -53,14 +60,15 @@ def test_jacobians(geometry_file, ldim, is_nurbs):
             global_arr_y = mapping._fields[1].coeffs._data
 
             # Compute the jacobians
-            eval_jacobians_2d_weights(ncells[0], ncells[1], pads[0], pads[1], degree[0], degree[1], quad_order[0],
-                                      quad_order[1], global_basis[0], global_basis[1], global_spans[0], global_spans[1],
-                                      global_arr_x, global_arr_y, global_arr_weights, jac_mats)
+            eval_jacobians_2d_weights(ncells[0], ncells[1], pads[0], pads[1], degree[0], degree[1], n_evals_points[0],
+                                      n_evals_points[1], global_basis[0], global_basis[1], global_spans[0],
+                                      global_spans[1], global_arr_x, global_arr_y, global_arr_weights, jac_mats)
 
             # Compute the inverses of the jacobians
-            eval_jacobians_inv_2d_weights(ncells[0], ncells[1], pads[0], pads[1], degree[0], degree[1], quad_order[0],
-                                          quad_order[1], global_basis[0], global_basis[1], global_spans[0],
-                                          global_spans[1], global_arr_x, global_arr_y, global_arr_weights, inv_jac_mats)
+            eval_jacobians_inv_2d_weights(ncells[0], ncells[1], pads[0], pads[1], degree[0], degree[1],
+                                          n_evals_points[0], n_evals_points[1], global_basis[0], global_basis[1],
+                                          global_spans[0], global_spans[1], global_arr_x, global_arr_y,
+                                          global_arr_weights, inv_jac_mats)
 
         if ldim == 3:
             global_arr_x = mapping._fields[0].coeffs._data
@@ -69,14 +77,14 @@ def test_jacobians(geometry_file, ldim, is_nurbs):
 
             # Compute the jacobians
             eval_jacobians_3d_weights(ncells[0], ncells[1], ncells[2], pads[0], pads[1], pads[2], degree[0], degree[1],
-                                      degree[2], quad_order[0], quad_order[1], quad_order[2], global_basis[0],
-                                      global_basis[1], global_basis[2], global_spans[0], global_spans[1],
-                                      global_spans[2], global_arr_x, global_arr_y, global_arr_z, global_arr_weights,
-                                      jac_mats)
+                                      degree[2], n_evals_points[0], n_evals_points[1], n_evals_points[2],
+                                      global_basis[0], global_basis[1], global_basis[2], global_spans[0],
+                                      global_spans[1], global_spans[2], global_arr_x, global_arr_y, global_arr_z,
+                                      global_arr_weights, jac_mats)
 
             # Compute the inverses of the jacobians
             eval_jacobians_inv_3d_weights(ncells[0], ncells[1], ncells[2], pads[0], pads[1], pads[2], degree[0],
-                                          degree[1], degree[2], quad_order[0], quad_order[1], quad_order[2],
+                                          degree[1], degree[2], n_evals_points[0], n_evals_points[1], n_evals_points[2],
                                           global_basis[0], global_basis[1], global_basis[2], global_spans[0],
                                           global_spans[1], global_spans[2], global_arr_x, global_arr_y, global_arr_z,
                                           global_arr_weights, inv_jac_mats)
@@ -88,13 +96,13 @@ def test_jacobians(geometry_file, ldim, is_nurbs):
             global_arr_y = mapping._fields[1].coeffs._data
 
             # Compute the jacobians
-            eval_jacobians_2d(ncells[0], ncells[1], pads[0], pads[1], degree[0], degree[1], quad_order[0],
-                              quad_order[1], global_basis[0], global_basis[1], global_spans[0], global_spans[1],
+            eval_jacobians_2d(ncells[0], ncells[1], pads[0], pads[1], degree[0], degree[1], n_evals_points[0],
+                              n_evals_points[1], global_basis[0], global_basis[1], global_spans[0], global_spans[1],
                               global_arr_x, global_arr_y, jac_mats)
 
             # Compute the inverses of the jacobians
-            eval_jacobians_inv_2d(ncells[0], ncells[1], pads[0], pads[1], degree[0], degree[1], quad_order[0],
-                                  quad_order[1], global_basis[0], global_basis[1], global_spans[0], global_spans[1],
+            eval_jacobians_inv_2d(ncells[0], ncells[1], pads[0], pads[1], degree[0], degree[1], n_evals_points[0],
+                                  n_evals_points[1], global_basis[0], global_basis[1], global_spans[0], global_spans[1],
                                   global_arr_x, global_arr_y, inv_jac_mats)
 
         if ldim == 3:
@@ -105,13 +113,13 @@ def test_jacobians(geometry_file, ldim, is_nurbs):
 
             # Compute the jacobians
             eval_jacobians_3d(ncells[0], ncells[1], ncells[2], pads[0], pads[1], pads[2], degree[0], degree[1],
-                              degree[2], quad_order[0], quad_order[1], quad_order[2], global_basis[0], global_basis[1],
-                              global_basis[2], global_spans[0], global_spans[1], global_spans[2], global_arr_x,
-                              global_arr_y, global_arr_z, jac_mats)
+                              degree[2], n_evals_points[0], n_evals_points[1], n_evals_points[2], global_basis[0],
+                              global_basis[1], global_basis[2], global_spans[0], global_spans[1], global_spans[2],
+                              global_arr_x, global_arr_y, global_arr_z, jac_mats)
 
             # Compute the inverses of the jacobians
             eval_jacobians_inv_3d(ncells[0], ncells[1], ncells[2], pads[0], pads[1], pads[2], degree[0], degree[1],
-                                  degree[2], quad_order[0], quad_order[1], quad_order[2], global_basis[0],
+                                  degree[2], n_evals_points[0], n_evals_points[1], n_evals_points[2], global_basis[0],
                                   global_basis[1], global_basis[2], global_spans[0], global_spans[1], global_spans[2],
                                   global_arr_x, global_arr_y, global_arr_z, inv_jac_mats)
 
