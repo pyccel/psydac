480bc226648b653dc984cb80fff2d0cfd3348614
saidctb <said-ctb@live.fr>
2022-04-28 09:20:56 +0200

    increase npts

--- psydac/linalg/tests/test_kron_direct_solver.py
+++ psydac/linalg/tests/test_kron_direct_solver.py
@@ -297,10 +297,8 @@ def test_kron_solver_nd_ser(seed, dim):
 @pytest.mark.parallel
 def test_kron_solver_nd_par(seed, dim):
     # for now, avoid too high dim's, since we solve the matrix completely on each rank as well...
-    if dim < 5:
-        npts_base = 4
-    else:
-        npts_base = 2
+
+    npts_base = 8
     compare_solve(seed, MPI.COMM_WORLD, [npts_base]*dim, [1]*dim, [False]*dim, matrix_to_sparse, transposed=False, verbose=False)
 
 if __name__ == '__main__':
