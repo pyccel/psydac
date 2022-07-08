55fc3f5f946678b53a298df97f5241488448f057
Martin Campos Pinto <martin.campos-pinto@ipp.mpg.de>
2022-07-08 17:15:17 +0200

    P1 test of non-matching pretzel

--- psydac/feec/multipatch/multipatch_non_conf_scipy.py
+++ psydac/feec/multipatch/multipatch_non_conf_scipy.py
@@ -48,6 +48,12 @@ from psydac.feec.multipatch.api       import discretize
 from psydac.api.settings              import PSYDAC_BACKENDS
 from psydac.fem.splines               import SplineSpace
 
+from psydac.fem.basic                                   import FemField
+from psydac.feec.multipatch.plotting_utilities          import plot_field
+
+from sympde.topology      import IdentityMapping, PolarMapping
+from psydac.feec.multipatch.multipatch_domain_utilities import build_multipatch_domain
+
 def get_patch_index_from_face(domain, face):
     """ Return the patch index of subdomain/boundary
 
@@ -174,41 +180,53 @@ def construct_V1_conforming_projection(V1h, domain_h, hom_bc=False, storage_fn=N
         axis      = I.axis
         direction = I.direction
 
-        k_fine    = get_patch_index_from_face(domain, I.minus)
-        k_coarse  = get_patch_index_from_face(domain, I.plus )
+        k_minus = get_patch_index_from_face(domain, I.minus)
+        k_plus  = get_patch_index_from_face(domain, I.plus )
+        minus_axis, plus_axis = I.minus.axis, I.plus.axis  # logical directions normal to interface
+        d_minus, d_plus       = 1-minus_axis, 1-plus_axis  # logical directions along the interface
+        I_minus_ncells = V1h.spaces[k_minus].spaces[d_minus].ncells[d_minus]
+        I_plus_ncells  = V1h.spaces[k_plus] .spaces[d_plus] .ncells[d_plus]
 
-        # This is the component normal to the interface
-        fine_axis, coarse_axis   = I.minus.axis, I.plus.axis
-        fine_ext, coarse_ext   = I.minus.ext, I.plus.ext
+        matching_interfaces = (I_minus_ncells == I_plus_ncells)
 
-        d_coarse = 1-coarse_axis   # direction of the components on interface
+        if I_minus_ncells <= I_plus_ncells:
+            k_fine, k_coarse        = k_plus, k_minus
+            fine_axis, coarse_axis  = I.plus.axis, I.minus.axis
+            fine_ext,  coarse_ext   = I.plus.ext,  I.minus.ext
+        
+        else:
+            k_fine, k_coarse        = k_minus, k_plus
+            fine_axis, coarse_axis  = I.minus.axis, I.plus.axis
+            fine_ext,  coarse_ext   = I.minus.ext, I.plus.ext
+
         d_fine   = 1-fine_axis
-
-        if V1h.spaces[k_fine].spaces[d_fine].ncells[d_fine] < V1h.spaces[k_coarse].spaces[d_coarse].ncells[d_coarse]:
-            k_fine, k_coarse       = k_coarse, k_fine
-            fine_axis, coarse_axis = I.plus.axis, I.minus.axis
-            fine_ext, coarse_ext   = I.plus.ext, I.minus.ext
-
-            d_coarse = 1-coarse_axis   # direction of the components on interface
-            d_fine   = 1-fine_axis
+        d_coarse = 1-coarse_axis
 
         space_fine   = V1h.spaces[k_fine]
         space_coarse = V1h.spaces[k_coarse]
+        
+        print("coarse = \n", space_coarse.spaces[d_coarse])
+        print("coarse 2 = \n", space_coarse.spaces[d_coarse].spaces[d_coarse])
+        coarse_space_1d = space_coarse.spaces[d_coarse].spaces[d_coarse] # todo: merge with first test above
+        
+        print("fine = \n", space_fine.spaces[d_fine])
+        print("fine 2 = \n", space_fine.spaces[d_fine].spaces[d_fine])
 
-        coarse_space_1d = space_coarse.spaces[d_coarse].spaces[d_coarse]
         fine_space_1d   = space_fine.spaces[d_fine].spaces[d_fine]
         grid = np.linspace(fine_space_1d.breaks[0], fine_space_1d.breaks[-1], coarse_space_1d.ncells+1)
         coarse_space_1d_k_plus  = SplineSpace(degree=fine_space_1d.degree, grid=grid, basis=fine_space_1d.basis)
 
-        E_1D    = construct_extension_operator_1D(domain=coarse_space_1d_k_plus, codomain=fine_space_1d)
-
-        product = (E_1D.T) @ E_1D
-        R_1D    = inv(product.tocsc()) @ E_1D.T
-        ER_1D   = E_1D @ R_1D
+        if not matching_interfaces:
+            E_1D    = construct_extension_operator_1D(domain=coarse_space_1d_k_plus, codomain=fine_space_1d)
+            product = (E_1D.T) @ E_1D
+            R_1D    = inv(product.tocsc()) @ E_1D.T
+            ER_1D   = E_1D @ R_1D
+        else:
+            ER_1D = R_1D = E_1D = sparse_eye(fine_space_1d.nbasis,format="lil")
 
         # P_k_minus_k_minus
         multi_index = [None]*ndim
-        multi_index[coarse_axis] = 0 if coarse_ext == -1 else space_coarse.spaces.spaces[d_coarse][coarse_axis].nbasis-1
+        multi_index[coarse_axis] = 0 if coarse_ext == -1 else space_coarse.spaces[d_coarse].spaces[coarse_axis].nbasis-1
         for i in range(coarse_space_1d.nbasis):
             multi_index[d_coarse]  = i
             ig = l2g.get_index(k_coarse, d_coarse, multi_index)
@@ -273,32 +291,59 @@ if __name__ == '__main__':
     print(' .. multi-patch domain...')
     # domain = build_multipatch_domain(domain_name='two_patch_nc')
 
-    A  = Square('A',bounds1=(0, 0.5), bounds2=(0, 1))
-    B  = Square('B',bounds1=(0.5, 1.), bounds2=(0, 1))
-    M1 = IdentityMapping('M1', dim=2)
-    M2 = IdentityMapping('M2', dim=2)
-    A  = M1(A)
-    B  = M2(B)
+    domain_name = 'pretzel_f'    
+    # domain_name = '2patch_nc_mapped'
+    # domain_name = '2patch_nc'
 
-    domain = A.join(B, name = 'domain',
-                bnd_minus = A.get_boundary(axis=0, ext=1),
-                bnd_plus  = B.get_boundary(axis=0, ext=-1),
-                direction=1)
+    if domain_name == '2patch_nc_mapped':
+
+        A = Square('A',bounds1=(0.5, 1), bounds2=(0,       np.pi/2))
+        B = Square('B',bounds1=(0.5, 1), bounds2=(np.pi/2, np.pi)  )
+        M1 = PolarMapping('M1',2, c1= 0, c2= 0, rmin = 0., rmax=1.)
+        M2 = PolarMapping('M2',2, c1= 0, c2= 0, rmin = 0., rmax=1.)
+        A = M1(A)
+        B = M2(B)
+
+        domain = A.join(B, name = 'domain',
+                    bnd_minus = A.get_boundary(axis=1, ext=1),
+                    bnd_plus  = B.get_boundary(axis=1, ext=-1),
+                    direction=1)
+
+    elif domain_name == '2patch_nc':
+
+        A  = Square('A',bounds1=(0, 0.5), bounds2=(0, 1))
+        B  = Square('B',bounds1=(0.5, 1.), bounds2=(0, 1))
+        M1 = IdentityMapping('M1', dim=2)
+        M2 = IdentityMapping('M2', dim=2)
+        A  = M1(A)
+        B  = M2(B)
+
+        domain = A.join(B, name = 'domain',
+                    bnd_minus = A.get_boundary(axis=0, ext=1),
+                    bnd_plus  = B.get_boundary(axis=0, ext=-1),
+                    direction=1)
     
-    # nc = 2
-    ncells_c = {
-        'M1(A)':[nc, nc],
-        'M2(B)':[nc, nc],
-    }
-    ncells_f = {
-        'M1(A)':[2*nc, 2*nc],
-        'M2(B)':[2*nc, 2*nc],
-    }
-    ncells_h = {
-        'M1(A)':[2*nc, 2*nc],
-        'M2(B)':[nc, nc],
-    }
+    else:
+        domain = build_multipatch_domain(domain_name=domain_name)
+    
+    n_patches = len(domain)
+    
+    def levelof(k):
+        # some random refinement level (1 or 2 here)
+        return 1+((2*k)%3)%2
 
+    if len(domain) == 2:
+        ncells_h = {
+            'M1(A)':[nc, nc],
+            'M2(B)':[2*nc, 2*nc],
+        }
+
+    else:
+        ncells_h = {}
+        for k, D in enumerate(domain.interior):
+            ncells_h[D.name] = [levelof(k)*nc,levelof(k)*nc]
+
+    print( 'ncells_h = ', ncells_h )
     backend_language = 'python'
 
     t_stamp = time_count()
@@ -313,7 +358,32 @@ if __name__ == '__main__':
 
     V1h = derham_h.V1
 
-    CP1 = construct_V1_conforming_projection(V1h, domain_h)
+    cP1_m = construct_V1_conforming_projection(V1h, domain_h)
 
     np.set_printoptions(linewidth=100000, precision=2, threshold=100000, suppress=True)
-    print(CP1.toarray())
+    # print(cP1_m.toarray())
+
+    ### apply cP1 on some discontinuous G
+
+    # G_sol_log = [[lambda xi1, xi2, ii=i : ii+xi1+xi2**2 for d in [0,1]] for i in range(len(domain))]   
+    # G_sol_log = [[lambda xi1, xi2, kk=k : levelof(kk)-1  for d in [0,1]] for k in range(len(domain))]   
+    G_sol_log = [[lambda xi1, xi2, kk=k : kk  for d in [0,1]] for k in range(len(domain))]   
+
+    P0, P1, P2 = derham_h.projectors()
+    
+    G1h   = P1(G_sol_log)
+    G1h_coeffs = G1h.coeffs.toarray()
+    
+    plot_field(numpy_coeffs=G1h_coeffs, Vh=V1h, space_kind='hcurl',             
+            plot_type='components',
+            domain=domain, title='G1h', cmap='viridis',
+            filename=plot_dir+'/G.png')
+
+    G1h_conf_coeffs = cP1_m @ G1h_coeffs
+
+    plot_field(numpy_coeffs=G1h_conf_coeffs, Vh=V1h, space_kind='hcurl',             
+            plot_type='components',
+            domain=domain, title='PG', cmap='viridis',
+            filename=plot_dir+'/PG.png')
+
+
