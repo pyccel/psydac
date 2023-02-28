802905b1e0d5eeedb208b2e21398db3fb0fc9d44
tomcaruso <tomarthur.caruso@gmail.com>
2023-02-28 09:34:14 +0100

    (StencilMatrix) Correct some test code

--- psydac/linalg/tests/test_stencil_vector_space.py
+++ psydac/linalg/tests/test_stencil_vector_space.py
@@ -276,7 +276,7 @@ def test_stencil_vector_space_2D_serial_set_interface(dtype, n1, n2, p1, p2, s1,
 # ===============================================================================
 
 @pytest.mark.parametrize('dtype', [float, complex])
-@pytest.mark.parametrize('n1', [1, 7])
+@pytest.mark.parametrize('n1', [15, 30])
 @pytest.mark.parametrize('p1', [1, 2])
 @pytest.mark.parametrize('s1', [1, 2])
 @pytest.mark.parametrize('P1', [True, False])
@@ -303,12 +303,12 @@ def test_stencil_vector_space_1d_parallel_init(dtype, n1, p1, s1, P1):
     assert V.dimension == n1
     assert V.dtype == dtype
     assert V.mpi_type == find_mpi_type(dtype)
-    assert V.shape == (n1 + 2 * p1*s1,)
+    assert V.shape == (V.ends[0]+1-V.starts[0] + 2 * p1*s1,)
     assert V.parallel
     assert V.cart == C
     assert V.npts == (n1,)
-    assert V.starts == (0,)
-    assert V.ends == (n1 - 1,)
+    assert V.starts == C.starts
+    assert V.ends == C.ends
     assert V.parent_starts == (None,)
     assert V.parent_ends == (None,)
     assert V.pads == (p1,)
@@ -319,8 +319,8 @@ def test_stencil_vector_space_1d_parallel_init(dtype, n1, p1, s1, P1):
 # ===============================================================================
 
 @pytest.mark.parametrize('dtype', [float, complex])
-@pytest.mark.parametrize('n1', [1, 7])
-@pytest.mark.parametrize('n2', [1, 5])
+@pytest.mark.parametrize('n1', [15, 30])
+@pytest.mark.parametrize('n2', [20, 40])
 @pytest.mark.parametrize('p1', [1, 2])
 @pytest.mark.parametrize('p2', [1, 2])
 @pytest.mark.parametrize('s1', [1, 2])
@@ -349,12 +349,12 @@ def test_stencil_vector_space_2d_parallel_init(dtype, n1, n2, p1, p2, s1, s2, P1
     assert V.dimension == n1 * n2
     assert V.dtype == dtype
     assert V.mpi_type == find_mpi_type(dtype)
-    assert V.shape == ((n1 + 2 * p1*s1), (n2 + 2 * p2*s2))
+    assert V.shape == ((V.ends[0]+1-V.starts[0] + 2 * p1*s1), (V.ends[1]+1-V.starts[1] + 2 * p2*s2))
     assert V.parallel
     assert V.cart == C
     assert V.npts == (n1, n2)
-    assert V.starts == (0, 0)
-    assert V.ends == (n1 - 1, n2 - 1)
+    assert V.starts == C.starts
+    assert V.ends == C.ends
     assert V.parent_starts == (None, None)
     assert V.parent_ends == (None, None)
     assert V.pads == (p1, p2)
@@ -365,9 +365,9 @@ def test_stencil_vector_space_2d_parallel_init(dtype, n1, n2, p1, p2, s1, s2, P1
 # ===============================================================================
 
 @pytest.mark.parametrize('dtype', [float, complex])
-@pytest.mark.parametrize('n1', [1, 9])
-@pytest.mark.parametrize('n2', [1, 7])
-@pytest.mark.parametrize('n3', [1, 5])
+@pytest.mark.parametrize('n1', [15, 30])
+@pytest.mark.parametrize('n2', [20, 40])
+@pytest.mark.parametrize('n3', [10, 25])
 @pytest.mark.parametrize('p1', [1, 2])
 @pytest.mark.parametrize('p2', [1, 2])
 @pytest.mark.parametrize('p3', [1, 2])
@@ -399,12 +399,12 @@ def test_stencil_vector_space_3d_parallel_init(dtype, n1, n2, n3, p1, p2, p3, s1
     assert V.dimension == n1*n2*n3
     assert V.dtype == dtype
     assert V.mpi_type == find_mpi_type(dtype)
-    assert V.shape == (n1 + 2 * p1*s1, n2 + 2 * p2*s2, n3 + 2 * p3*s3)
+    assert V.shape == (V.ends[0]+1-V.starts[0] + 2 * p1*s1, V.ends[1]+1-V.starts[1] + 2 * p2*s2, V.ends[2]+1-V.starts[2] + 2 * p3*s3)
     assert V.parallel
     assert V.cart == C
     assert V.npts == (n1, n2, n3)
-    assert V.starts == (0, 0, 0)
-    assert V.ends == (n1 - 1, n2 - 1, n3 - 1)
+    assert V.starts == C.starts
+    assert V.ends == C.ends
     assert V.parent_starts == (None, None, None)
     assert V.parent_ends == (None, None, None)
     assert V.pads == (p1, p2, p3)
@@ -415,9 +415,9 @@ def test_stencil_vector_space_3d_parallel_init(dtype, n1, n2, n3, p1, p2, p3, s1
 # ===============================================================================
 
 @pytest.mark.parametrize('dtype', [float, complex])
-@pytest.mark.parametrize('n1', [2, 9])
-@pytest.mark.parametrize('n2', [2, 7])
-@pytest.mark.parametrize('n3', [2, 5])
+@pytest.mark.parametrize('n1', [15, 30])
+@pytest.mark.parametrize('n2', [20, 40])
+@pytest.mark.parametrize('n3', [10, 25])
 @pytest.mark.parallel
 
 def test_stencil_vector_space_3D_parallel_parent(dtype, n1, n2, n3, P1=True, P2=False, P3=True):
@@ -443,16 +443,16 @@ def test_stencil_vector_space_3D_parallel_parent(dtype, n1, n2, n3, P1=True, P2=
     # Test properties of the vector space
     assert V.dimension == 1
     assert V.dtype == dtype
-    assert V.starts == (0, 0, 0)
-    assert V.ends == (n1-1, n2-1, n3-1)
+    assert V.starts == C.starts
+    assert V.ends == C.ends
     assert V.parent_starts == (0, 0, 0)
-    assert V.parent_ends == (n1-1, n2-1, n3-1)
+    assert V.parent_ends == (V.ends[0]+1-V.starts[0]-1, V.ends[1]+1-V.starts[1]-1, V.ends[2]+1-V.starts[2]-1)
 # ===============================================================================
 
 @pytest.mark.parametrize('dtype', [float, complex])
-@pytest.mark.parametrize('n1', [2, 9])
-@pytest.mark.parametrize('n2', [2, 7])
-@pytest.mark.parametrize('p1', [1, 2])
+@pytest.mark.parametrize('n1', [15, 30])
+@pytest.mark.parametrize('n2', [20, 40])
+@pytest.mark.parametrize('p1', [10, 25])
 @pytest.mark.parametrize('p2', [1, 2])
 @pytest.mark.parametrize('s1', [1, 2])
 @pytest.mark.parametrize('s2', [1, 2])
@@ -481,80 +481,13 @@ def test_stencil_vector_space_2D_parallel_zeros(dtype, n1, n2, p1, p2, s1, s2, P
     assert isinstance(x, StencilVector )
     assert x.space is V
     assert x.dtype == dtype
-    assert x.starts == (0, 0)
-    assert x.ends   == (n1-1, n2-1)
-    assert x._data.shape == (n1+2*p1*s1, n2+2*p2*s2)
+    assert x.starts == V.starts
+    assert x.ends == V.ends
+    assert x._data.shape == (V.ends[0]+1-V.starts[0]+2*p1*s1, V.ends[1]+1-V.starts[1]+2*p2*s2)
     assert x.pads == (p1, p2)
     assert x._data.dtype == dtype
-    assert np.array_equal(x._data, np.zeros((n1+2*p1*s1, n2+2*p2*s2), dtype=dtype))
+    assert np.array_equal(x._data, np.zeros((V.ends[0]+1-V.starts[0]+2*p1*s1, V.ends[1]+1-V.starts[1]+2*p2*s2), dtype=dtype))
 # ===============================================================================
-
-@pytest.mark.parametrize('dtype', [float, complex])
-@pytest.mark.parametrize('n1', [1, 9])
-@pytest.mark.parametrize('n2', [1, 7])
-@pytest.mark.parametrize('p1', [1, 2])
-@pytest.mark.parametrize('p2', [1, 2])
-@pytest.mark.parametrize('s1', [1, 2])
-@pytest.mark.parametrize('s2', [1, 2])
-@pytest.mark.parametrize('axis', [0, 1])
-@pytest.mark.parametrize('ext', [-1, 1])
-@pytest.mark.parallel
-
-def test_stencil_vector_space_2D_parrallel_set_interface(dtype, n1, n2, p1, p2, s1, s2, axis, ext, P1=True, P2=False):
-    from mpi4py import MPI
-
-    comm = MPI.COMM_WORLD
-    # Create domain decomposition
-    D = DomainDecomposition([n1, n2], periods=[P1, P2], comm=comm)
-
-    # Partition the points
-    npts = [n1, n2]
-    global_starts, global_ends = compute_global_starts_ends(D, npts)
-
-    # Create cart and  vector space
-    C = CartDecomposition(D, npts, global_starts, global_ends, pads=[p1, p2], shifts=[s1, s2])
-    V = StencilVectorSpace(C, dtype=dtype)
-
-    # Create an interface on our vector space
-    V.set_interface(axis, ext, C)
-    V_inter=V.interfaces[axis, ext]
-
-    # Test properties of the interface
-    assert isinstance(V_inter, StencilVectorSpace)
-    assert V_inter.dimension == n1 * n2
-    assert V_inter.dtype == dtype
-    assert V_inter.mpi_type == find_mpi_type(dtype)
-    assert V_inter.parallel
-    assert isinstance(V_inter.cart, CartDecomposition)
-    assert V_inter.npts == (n1, n2)
-    assert V_inter.parent_starts == (None, None)
-    assert V_inter.parent_ends == (None, None)
-    assert V_inter.pads == (p1, p2)
-    assert V_inter.periods == (P1, P2)
-    assert V_inter.shifts == (s1, s2)
-    assert V_inter.ndim == 2
-    assert V_inter.interfaces == type(type.__dict__)({})
-
-    if axis == 0:
-        assert V_inter.shape == ((p1+1 + 2 * p1 * s1), (n2 + 2 * p2 * s2))
-        if ext == 1:
-            assert V_inter.starts == (n1-1-p1, 0)
-            assert V_inter.ends == (n1-1, n2-1)
-        else:
-            assert V_inter.starts == (0, 0)
-            assert V_inter.ends == (p1, n2-1)
-    else:
-        assert V_inter.shape == ((n1 + 2 * p1 * s1), p2+1+2*p2*s2)
-        if ext == 1:
-            assert V_inter.starts == (0, n2-1-p2)
-            assert V_inter.ends == (n1-1, n2-1)
-        else:
-            assert V_inter.starts == (0, 0)
-            assert V_inter.ends == (n1-1, p2)
-
-    assert V_inter.parent_starts == (None, None)
-    assert V_inter.parent_ends == (None, None)
-#===============================================================================
 # SCRIPT FUNCTIONALITY
 #===============================================================================
 if __name__ == "__main__":
