1b6c20eef8811d5cfe7ae9b156ba3ad828f885dd
Yaman Güçlü <yaman.guclu@gmail.com>
2023-04-20 15:00:20 +0200

    Apply cosmetic changes to psydac/fem/projectors.py

--- psydac/fem/projectors.py
+++ psydac/fem/projectors.py
@@ -5,6 +5,7 @@ from psydac.core.bsplines   import hrefinement_matrix
 from psydac.linalg.identity import IdentityStencilMatrix
 from psydac.linalg.stencil  import StencilVectorSpace
 
+
 def knots_to_insert(coarse_grid, fine_grid, tol=1e-14):
     """ Compute the point difference between the fine grid and coarse grid."""
 #    assert len(coarse_grid)*2-2 == len(fine_grid)-1
@@ -16,6 +17,7 @@ def knots_to_insert(coarse_grid, fine_grid, tol=1e-14):
 
     assert abs(intersection-coarse_grid).max()<tol
     return T
+
 
 def knot_insertion_projection_operator(domain, codomain):
     """
@@ -72,25 +74,28 @@ def knot_insertion_projection_operator(domain, codomain):
 
     """
     ops = []
-    for d,c in zip(domain.spaces, codomain.spaces):
-        if d.ncells>c.ncells:
+    for d, c in zip(domain.spaces, codomain.spaces):
+
+        if d.ncells > c.ncells:
             Ts = knots_to_insert(c.breaks, d.breaks)
             P  = hrefinement_matrix(Ts, c.degree, c.knots)
 
             if d.basis == 'M':
                 assert c.basis == 'M'
-                P = np.diag(1/d._scaling_array) @ P @ np.diag(c._scaling_array)
+                P = np.diag(1 / d._scaling_array) @ P @ np.diag(c._scaling_array)
 
             ops.append(P.T)
-        elif d.ncells<c.ncells:
+
+        elif d.ncells < c.ncells:
             Ts = knots_to_insert(d.breaks, c.breaks)
-            P  = hrefinement_matrix(Ts , d.degree, d.knots)
+            P  = hrefinement_matrix(Ts, d.degree, d.knots)
 
             if d.basis == 'M':
                 assert c.basis == 'M'
-                P = np.diag(1/c._scaling_array) @ P @ np.diag(d._scaling_array)
+                P = np.diag(1 / c._scaling_array) @ P @ np.diag(d._scaling_array)
 
             ops.append(P)
+
         else:
             ops.append(np.eye(d.nbasis))
 
