a72dc7604f7b6058afc15ec98fed4f77585512c7
Julian Owezarek <julian.owezarek@tum.de>
2023-05-24 09:19:53 +0200

    Add documentation workflow (#299)
    Add the GitHub Action `documentation.yml` to `.github/workflows`. It
    will be responsible for building and deploying PSYDAC's documentation in
    the future. Additionally add an empty documentation in order for the
    tests to pass.
    
    ---------
    
    Co-authored-by: Yaman Güçlü <yaman.guclu@gmail.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,5 @@
+# pylint: disable=redefined-builtin
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -9,13 +11,18 @@
 # add these directories to sys.path here.
 import pathlib
 import sys
+import tomli
+
 autodoc_mock_imports = ['sympy', 'sympde', 'numpy', 'scipy', 'mpi4py', 'pyccel', 'h5py', 'yaml', 'gelato', 'pyevtk', 'matplotlib']
 sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
-project = 'PSYDAC'
-copyright = '2023, Numerical Methods in Plasma Physics division, Max Planck Institute for Plasma Physics Garching'
-author = 'Numerical Methods in Plasma Physics division, Max Planck Institute for Plasma Physics Garching'
-release = 'v0.1'
+with open('../../pyproject.toml', mode='rb') as pyproject:
+    pkg_meta = tomli.load(pyproject)['project']
+
+project   = str(pkg_meta['name'])
+copyright = '2018-2023, Psydac Developers'
+author    = str(pkg_meta['authors'][0]['name'])
+release   = str(pkg_meta['version'])
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -38,9 +45,8 @@ add_module_names = False
 #inheritance_graph_attrs = dict(rankdir="LR", size='"6.0, 4.0"',
 #                               fontsize=14, ratio='compress')
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme' # used to be 'piccolo_theme'
+html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
