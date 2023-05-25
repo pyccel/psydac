31b8cbafdd27eb2d3d82eb3f8edc7172058374f9
tomcaruso <tomarthur,caruso@gmail,com>
2023-05-25 16:05:12 +0200

    create psydac accelerate command

---
+++ psydac/cmd/accelerate.py
@@ -0,0 +1,10 @@
+def main( language='fortran'):
+    import os
+    psydac_path=os.path.dirname(os.path.abspath(__file__))
+    print(psydac_path)
+    os.system('pyccel ' + psydac_path + '/../linalg/kernels.py --language '+ language)
+    os.system('pyccel ' + psydac_path + '/../api/ast/linalg_kernels.py --language '+ language)
+    os.system('pyccel ' + psydac_path + '/../core/kernels.py --language '+ language)
+    os.system('pyccel ' + psydac_path + '/../core/bsplines_pyccel.py --language '+ language)
+    return
+
