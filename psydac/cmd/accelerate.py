ecf4cb8bf92f1370a7ff0aedb4719d1b9ac25b99
tomcaruso <tomarthur,caruso@gmail,com>
2023-06-06 16:38:30 +0200

    correct PYCCEL_LANGUAGE initialisation in github tests

--- psydac/cmd/accelerate.py
+++ psydac/cmd/accelerate.py
@@ -9,7 +9,7 @@ def main():
 
     os.system('pyccel ' + psydac_path + '/../linalg/kernels.py --language '         + language)
     os.system('pyccel ' + psydac_path + '/../api/ast/linalg_kernels.py --language ' + language)
-    os.system('pyccel ' + psydac_path + '/../core/kernels.py --language '           + language)
+    os.system('pyccel ' + psydac_path + '/../core/kernels.py --language '           + 'fortran')
     os.system('pyccel ' + psydac_path + '/../core/bsplines_pyccel.py --language '   + language)
     return
 
