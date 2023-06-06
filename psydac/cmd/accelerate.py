d879d9123787e5c8392b6366dc2e63d7d539bf30
tomcaruso <tomarthur,caruso@gmail,com>
2023-06-06 17:19:21 +0200

    correct PYCCEL_LANGUAGE initialisation in github tests to only fortran

--- psydac/cmd/accelerate.py
+++ psydac/cmd/accelerate.py
@@ -4,12 +4,12 @@ def main():
     psydac_path = os.path.dirname(os.path.abspath(__file__))
     language    = os.getenv('PYCCEL_LANGUAGE')
 
-    if language not in ['fortran', 'C']:
+    if language not in ['fortran', 'c']:
         language = 'fortran'
 
     os.system('pyccel ' + psydac_path + '/../linalg/kernels.py --language '         + language)
     os.system('pyccel ' + psydac_path + '/../api/ast/linalg_kernels.py --language ' + language)
-    os.system('pyccel ' + psydac_path + '/../core/kernels.py --language '           + 'fortran')
+    os.system('pyccel ' + psydac_path + '/../core/kernels.py --language '           + language)
     os.system('pyccel ' + psydac_path + '/../core/bsplines_pyccel.py --language '   + language)
     return
 
