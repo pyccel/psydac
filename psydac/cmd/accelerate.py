2f3223d5fbe03d5bb35d879aed9b52e5f4458d7a
tomcaruso <tomarthur,caruso@gmail,com>
2023-06-06 15:56:42 +0200

    Remove the variable in accelerate script. And add an environnement variable to pass the language of pyccelisation

--- psydac/cmd/accelerate.py
+++ psydac/cmd/accelerate.py
@@ -1,10 +1,15 @@
-def main( language='fortran'):
+def main():
+
     import os
-    psydac_path=os.path.dirname(os.path.abspath(__file__))
-    print(psydac_path)
-    os.system('pyccel ' + psydac_path + '/../linalg/kernels.py --language '+ language)
-    os.system('pyccel ' + psydac_path + '/../api/ast/linalg_kernels.py --language '+ language)
-    os.system('pyccel ' + psydac_path + '/../core/kernels.py --language '+ language)
-    os.system('pyccel ' + psydac_path + '/../core/bsplines_pyccel.py --language '+ language)
+    psydac_path = os.path.dirname(os.path.abspath(__file__))
+    language    = os.getenv('PYCCEL_LANGUAGE')
+
+    if language not in ['fortran', 'C']:
+        language = 'fortran'
+
+    os.system('pyccel ' + psydac_path + '/../linalg/kernels.py --language '         + language)
+    os.system('pyccel ' + psydac_path + '/../api/ast/linalg_kernels.py --language ' + language)
+    os.system('pyccel ' + psydac_path + '/../core/kernels.py --language '           + language)
+    os.system('pyccel ' + psydac_path + '/../core/bsplines_pyccel.py --language '   + language)
     return
 
