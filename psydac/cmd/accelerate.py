af11d0151a429776229b49fb082ed0afe85e4690
tomcaruso <tomarthur,caruso@gmail,com>
2023-06-13 19:07:49 +0200

    Change the psydac-accelerate command to pyccelise all the files ending with _kernels.py

--- psydac/cmd/accelerate.py
+++ psydac/cmd/accelerate.py
@@ -1,6 +1,7 @@
 def main():
     import argparse
     import os
+    from glob import glob
 
     parser = argparse.ArgumentParser(
         formatter_class=argparse.ArgumentDefaultsHelpFormatter,
@@ -18,17 +19,29 @@ def main():
     args = parser.parse_args()
 
 
-    psydac_path = os.path.dirname(os.path.abspath(__file__))
+    psydac_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
     language    = args.language
 
     if language not in ['fortran', 'c']:
         print("The language given is not used by pyccel. It must be 'fortran' or 'c'.")
         language = 'fortran'
 
-    os.system('pyccel ' + psydac_path + '/../linalg/stencil2coo_kernels.py --language '    + language)
-    os.system('pyccel ' + psydac_path + '/../api/ast/transpose_kernels.py --language '     + language)
-    os.system('pyccel ' + psydac_path + '/../core/field_evaluation_kernels.py --language ' + language)
-    os.system('pyccel ' + psydac_path + '/../core/bsplines_kernels.py --language '         + language)
+
+    # for file in glob(psydac_path+"/*/*_kernels.py"):
+    #     print('Pyccelise file :' + file)
+    #     os.system('pyccel '+ file +' --language ' + language)
+
+    for path, subdirs, files in os.walk(psydac_path):
+        for name in files:
+            if name.endswith('_kernels.py'):
+                print('Pyccelise file :' + os.path.join(path, name))
+                os.system('pyccel '+ os.path.join(path, name) +' --language ' + language)
+
+    # os.system('pyccel ' + psydac_path + '/linalg/stencil2coo_kernels.py --language '    + language)
+    # os.system('pyccel ' + psydac_path + '/api/ast/transpose_kernels.py --language '     + language)
+    # os.system('pyccel ' + psydac_path + '/core/field_evaluation_kernels.py --language ' + language)
+    # os.system('pyccel ' + psydac_path + '/core/bsplines_kernels.py --language '         + language)
 
 
     return
