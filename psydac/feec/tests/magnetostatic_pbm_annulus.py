2912160c36c77d01c5a109eae3ab39722b6109f3
alex-m-h <alexanderhoffmann0997@gmail.com>
2023-07-07 10:09:09 +0200

    Added solution function for distorted annulus + test

--- psydac/feec/tests/magnetostatic_pbm_annulus.py
+++ psydac/feec/tests/magnetostatic_pbm_annulus.py
@@ -237,6 +237,54 @@ def assemble_mass_matrices(derham: Derham, derham_h: DiscreteDerham, annulus_h :
     M2 = l2_inner_product_V2h.assemble().tosparse()
     return M0,M1,M2
 
+def assemble_mass_matrices_general(derham: Derham, derham_h: DiscreteDerham, domain_h : Geometry
+                           ) -> Tuple[coo_matrix]:
+    """
+    Assemble the mass matrices of the spaces a discrete de Rham sequence
+    of length 2 on an annulus
+
+    Parameters
+    ----------
+    derham : Derham
+        Symbolic de Rham sequence of length 2
+    derham_h : DiscreteDerham
+        Discrete de Rham sequence of length 2
+    ???
+    Returns
+    -------
+    Tuple[coo_matrix]
+        Mass matrices in Scipy COO-format
+    """
+    # Define symbolic L2 inner products
+    u, v = top.elements_of(derham.V1, names='u, v')
+    rho, nu = top.elements_of(derham.V2, names='rho nu')
+    domain = domain_h.domain
+    l2_inner_product_V2 = BilinearForm(arguments=(rho,nu), 
+        expr=integral(domain, rho*nu)
+    )
+    l2_inner_product_V1 = BilinearForm(arguments=(u,v), 
+        expr=integral(domain, dot(u,v))
+    )
+    sigma, tau = top.elements_of(derham.V0, names='sigma, tau')
+    l2_inner_product_V0 = BilinearForm(arguments=(sigma,tau), 
+        expr=integral(domain, sigma*tau)
+    )
+
+    # Discretize the inner products and convert to scipy COO-matrix
+    l2_inner_product_V2h = discretize(l2_inner_product_V2, domain_h, 
+                                      spaces=[derham_h.V2, derham_h.V2])
+    l2_inner_product_V1h = discretize(l2_inner_product_V1, domain_h, 
+                                      spaces=[derham_h.V1, derham_h.V1])
+    l2_inner_product_V0h = discretize(l2_inner_product_V0, domain_h, 
+                                      spaces=[derham_h.V0, derham_h.V0])
+    assert isinstance(l2_inner_product_V2h, DiscreteBilinearForm)
+    assert isinstance(l2_inner_product_V1h, DiscreteBilinearForm)
+    assert isinstance(l2_inner_product_V0h, DiscreteBilinearForm)
+    M0 = l2_inner_product_V0h.assemble().tosparse()
+    M1 = l2_inner_product_V1h.assemble().tosparse()
+    M2 = l2_inner_product_V2h.assemble().tosparse()
+    return M0,M1,M2
+
 
 def solve_magnetostatic_pbm_J_direct_annulus(J : sympy.Expr, 
                                     psi_h : FemField, 
@@ -392,6 +440,118 @@ def solve_magnetostatic_pbm_J_direct_with_bc(J : sympy.Expr,
     B = B_0 + B_boundary_coeffs
     return B
 
+def solve_magnetostatic_pbm_distorted_annulus(J : sympy.Expr, 
+                                    psi_h : FemField, 
+                                    rhs_curve_integral: float,
+                                    boundary_data: FemField,
+                                    derham_h: DiscreteDerham,
+                                    derham: Derham,
+                                    domain_h: Geometry):
+    """
+    """
+    logger_magnetostatic = logging.getLogger(name='solve_magnetostatic_pbm_J_direct_with_bc')
+
+    # Compute the discrete differential operators, mass matrices and projection 
+    # matrices
+    matrices = _Matrices()
+    D0_block_operator, D1_block_operator = derham_h.derivatives_as_matrices
+    matrices.D0 = D0_block_operator.tosparse()
+    matrices.D1 = D1_block_operator.tosparse()
+    assert isinstance(matrices.D0, coo_matrix)
+    assert isinstance(matrices.D1, coo_matrix)
+    matrices.M0, matrices.M1, matrices.M2 = assemble_mass_matrices_general(derham, derham_h, domain_h)
+    assert isinstance(matrices.M0, coo_matrix)
+    assert isinstance(matrices.M1, coo_matrix)
+    assert isinstance(matrices.M2 , coo_matrix)
+    matrices.P0h = projection_matrix_H1_homogeneous_bc(derham_h.V0)
+    matrices.P1h = projection_matrix_Hdiv_homogeneous_bc(derham_h.V1)
+    assert isinstance(matrices.P0h, lil_matrix)
+    assert isinstance(matrices.P1h, lil_matrix)
+
+    # Compute penalty matrix
+    alpha = 10
+    I0 = scipy.sparse.eye(derham_h.V0.nbasis, format='lil')
+    I1 = scipy.sparse.eye(derham_h.V1.nbasis, format='lil')
+    matrices.S0_h_tilde = (I0 - matrices.P0h).transpose() @ matrices.M0 @ (I0 - matrices.P0h)
+    matrices.S1_h_tilde = (I1 - matrices.P1h).transpose() @ matrices.M1 @ (I1 - matrices.P1h)
+    matrices.D0_h = matrices.D0 @ matrices.P0h
+    matrices.D1_h = matrices.D1 @ matrices.P1h
+    assert isinstance(matrices.D0_h, csr_matrix), f"type(D0_h_mat):{type(matrices.D0_h)}"
+    assert isinstance(matrices.D1_h, csr_matrix)
+    assert isinstance(matrices.S0_h_tilde, csc_matrix), f"type(S0_h_tilde_mat):{type(matrices.S0_h_tilde)}"
+    assert isinstance(matrices.S1_h_tilde, csc_matrix), f"type(S1_h_tilde_mat):{type(matrices.S1_h_tilde)}"
+
+    harmonic_block = _assemble_harmonic_block(matrices=matrices, alpha=alpha)
+
+    # Compute part of right hand side
+    domain = domain_h.domain
+    sigma, tau = top.elements_of(derham.V0, names='sigma, tau')
+    l_J = LinearForm(tau, integral(domain, J*tau))
+    l_J_h = discretize(l_J, domain_h, space=derham_h.V0)
+    assert isinstance(l_J_h, DiscreteLinearForm)
+    J_tilde_h_array = l_J_h.assemble().toarray()
+    assert isinstance(J_tilde_h_array, np.ndarray)
+    J_h_array = matrices.P0h.transpose() @ J_tilde_h_array
+    assert isinstance(J_h_array, np.ndarray)
+
+    # Get the boundary data for rhs
+    boundary_data_coeffs = boundary_data.coeffs.toarray()
+    assert isinstance(boundary_data_coeffs, np.ndarray)
+    B_boundary_coeffs = (I1 - matrices.P1h) @ boundary_data_coeffs
+    boundary_rhs = (-matrices.D1_h.transpose() @ matrices.M2 @ matrices.D1 @ B_boundary_coeffs)
+
+    # Compute curl of the integration function psi
+    psi_h_coeffs = psi_h.coeffs.toarray()
+    assert isinstance(psi_h_coeffs, np.ndarray)
+    curl_psi_h_coeffs = matrices.D0 @ psi_h_coeffs
+    curl_psi_h_mat = csr_matrix(curl_psi_h_coeffs)
+    curl_psi_h_mat = curl_psi_h_mat.transpose()
+    logger_magnetostatic.debug('curl_psi_h_mat.shape:%s\n', curl_psi_h_mat.shape)
+
+    # Assemble the final system and solve
+    curve_integral_rhs_arr = np.array([rhs_curve_integral])
+    DD_tilde_mat = matrices.D1_h.transpose() @ matrices.M2  @ matrices.D1_h
+    A_mat = bmat([[matrices.P0h.transpose() @ matrices.M0 @ matrices.P0h +  alpha * matrices.S0_h_tilde, -matrices.D0_h.transpose() @ matrices.M1 @ matrices.P1h, None],
+                  [matrices.P1h.transpose() @ matrices.M1 @ matrices.D0_h,  DD_tilde_mat  + alpha * matrices.S1_h_tilde, matrices.P1h.transpose() @ harmonic_block],
+                  [None, curl_psi_h_mat.transpose() @ matrices.M1 @ matrices.P1h, None]])
+    rhs = np.concatenate((-J_h_array + matrices.D0_h.transpose() @ matrices.M1 @ B_boundary_coeffs, 
+                          boundary_rhs, 
+                          curve_integral_rhs_arr - curl_psi_h_mat.transpose() @ matrices.M1 @ B_boundary_coeffs))
+    A_mat = csr_matrix(A_mat) # Why is this not transforming into a CSR matrix?
+    sol = spsolve(A_mat, rhs)
+    logger_magnetostatic.debug("sol[:derham_h.V0.nbasis]:%s\n", sol[:derham_h.V0.nbasis])
+    B_0 = sol[derham_h.V0.nbasis:-1]
+    B = B_0 + B_boundary_coeffs
+
+    ###DEBUG###
+    div_B_coeffs = matrices.D1 @ B
+    curl_B_tilde_coeffs = matrices.D0_h.transpose() @ matrices.M1 @ B
+    curve_integral = curl_psi_h_coeffs @ matrices.M1 @ B
+    logger_magnetostatic.debug("np.linalg.norm(div_B_coeffs):%s", np.linalg.norm(div_B_coeffs))
+    logger_magnetostatic.debug("np.linalg.norm(curl_B_tilde_coeffs):%s", np.linalg.norm(curl_B_tilde_coeffs))
+    logger_magnetostatic.debug("curve_integral:%s\n",curve_integral)
+    ###########
+
+    ###DEBUG###
+
+    B_h_0_coeffs = array_to_psydac(B_0, derham_h.V1.vector_space)
+    B_h_boundary_coeffs = array_to_psydac(B_boundary_coeffs, derham_h.V1.vector_space)
+    B_h_0 = FemField(derham_h.V1, coeffs=B_h_0_coeffs)
+    B_h_boundary = FemField(derham_h.V1, coeffs=B_h_boundary_coeffs)
+    output_manager = OutputManager('spaces_magnetostatic_lifted.yml', 
+                                    'fields_magnetostatic_lifted.h5')
+    output_manager.add_spaces(V1=derham_h.V1)
+    output_manager.export_space_info()
+    output_manager.set_static()
+    output_manager.export_fields(B_h_0=B_h_0, B_h_boundary=B_h_boundary)
+    post_processor = PostProcessManager(domain=domain, 
+                                        space_file='spaces_magnetostatic_lifted.yml',
+                                        fields_file='fields_magnetostatic_lifted.h5')
+    post_processor.export_to_vtk('magnetostatic_pbm_lifted_vtk', npts_per_cell=3,
+                                    fields=("B_h_0", "B_h_boundary"))
+    ###########
+
+    return B
 
 if __name__ == "__main__":
     logging.basicConfig(filename='mydebug.log', level=logging.DEBUG, filemode='w')
--- psydac/feec/tests/test_magnetostatic_pbm_annulus.py
+++ psydac/feec/tests/test_magnetostatic_pbm_annulus.py
@@ -11,6 +11,7 @@ from psydac.feec.global_projectors import Projector_H1, Projector_Hdiv
 from psydac.feec.global_projectors import projection_matrix_H1_homogeneous_bc, projection_matrix_Hdiv_homogeneous_bc 
 from psydac.feec.tests.magnetostatic_pbm_annulus import solve_magnetostatic_pbm_annulus, solve_magnetostatic_pbm_J_direct_annulus
 from psydac.feec.tests.magnetostatic_pbm_annulus import solve_magnetostatic_pbm_J_direct_with_bc
+from psydac.feec.tests.magnetostatic_pbm_annulus import solve_magnetostatic_pbm_distorted_annulus
 from psydac.feec.pull_push         import pull_2d_hdiv
 from psydac.ddm.cart               import DomainDecomposition
 
@@ -21,6 +22,7 @@ from typing import Tuple
 
 from sympde.topology  import Derham, Square, IdentityMapping, PolarMapping
 from sympde.topology.domain import Domain, Union, Connectivity
+from sympde.topology.mapping import Mapping
 
 from psydac.feec.global_projectors import projection_matrix_Hdiv_homogeneous_bc, projection_matrix_H1_homogeneous_bc
 
@@ -507,7 +509,7 @@ def test_biot_savart():
 
 def test_constant_one():
     annulus, derham = _create_domain_and_derham()
-    ncells = [64,64]
+    ncells = [16,16]
     annulus_h = discretize(annulus, ncells=ncells, periodic=[False, True])
     derham_h = discretize(derham, annulus_h, degree=[2,2])
     assert isinstance(derham_h, DiscreteDerham)
@@ -530,7 +532,7 @@ def test_constant_one():
                                     rmin=1.0, rmax=2.0)
     omega_gamma = polar_mapping(logical_domain_gamma)
     derham_gamma = Derham(domain=omega_gamma, sequence=['H1', 'Hdiv', 'L2'])
-    omega_gamma_h = discretize(omega_gamma, ncells=[32,64], periodic=[False, True])
+    omega_gamma_h = discretize(omega_gamma, ncells=[8,16], periodic=[False, True])
     derham_gamma_h = discretize(derham_gamma, omega_gamma_h, degree=[2,2])
     h1_proj_gamma = Projector_H1(derham_gamma_h.V0)
     assert isinstance(derham_h, DiscreteDerham)
@@ -586,13 +588,141 @@ def test_constant_one():
     assert isinstance(V1h, VectorFemSpace)
     B_h_eval = V1h.eval_fields(eval_grid, B_h)
     print(B_h_eval)
-    assert abs( B_h_eval[0][0][0,1] - (0.25+1) * (np.sin(np.pi)+np.cos(np.pi))) < 0.01, f"B_h_eval[0][0][0,1] - (0.25+1) * (np.sin(np.pi)+np.cos(np.pi)):{B_h_eval[0][0][0,1] - (0.25+1) * (np.sin(np.pi)+np.cos(np.pi))}"
-    assert abs( B_h_eval[0][1][0,1] - (np.cos(np.pi) -  np.sin(np.pi))) < 0.01
-    assert abs( B_h_eval[0][0][2,1] - (0.75+1) * (np.sin(np.pi)+np.cos(np.pi))) < 0.01
+    assert abs( B_h_eval[0][0][0,1] - (0.25+1) * (np.sin(np.pi)+np.cos(np.pi))) < 0.02, f"B_h_eval[0][0][0,1] - (0.25+1) * (np.sin(np.pi)+np.cos(np.pi)):{B_h_eval[0][0][0,1] - (0.25+1) * (np.sin(np.pi)+np.cos(np.pi))}"
+    assert abs( B_h_eval[0][1][0,1] - (np.cos(np.pi) -  np.sin(np.pi))) < 0.02
+    assert abs( B_h_eval[0][0][2,1] - (0.75+1) * (np.sin(np.pi)+np.cos(np.pi))) < 0.03
+
+class DistortedPolarMapping(Mapping):
+    """
+
+    Examples
+
+    """
+    _expressions = {'x': '3.0*(x1 + 1)*cos(x2)*(cos(x2)**2+1)',
+                    'y': '(x1 + 1)*sin(x2)*(cos(x2)**2+1)'}
+
+    _ldim        = 2
+    _pdim        = 2
+
+def _create_distorted_annulus_and_derham() -> Tuple[Domain, Derham]:
+    logical_domain = Square(name='logical_domain', bounds1=(0,1), bounds2=(0,2*np.pi))
+    boundary_logical_domain = Union(logical_domain.get_boundary(axis=0, ext=-1),
+                                    logical_domain.get_boundary(axis=0, ext=1))
+    logical_domain = Domain(name='logical_domain',
+                            interiors=logical_domain.interior,
+                            boundaries=boundary_logical_domain,
+                            dim=2)
+    distorted_polar_mapping = DistortedPolarMapping(name='distorted_polar_mapping', dim=2)
+    domain = distorted_polar_mapping(logical_domain)
+    derham = Derham(domain=domain, sequence=['H1', 'Hdiv', 'L2'])
+    return domain, derham
+
+def test_magnetostatic_pbm_annuluslike():
+    logger = logging.getLogger("test_magnetostatic_pbm_annuluslike")
+    domain, derham = _create_distorted_annulus_and_derham()
+    ncells = [16,16]
+    domain_h = discretize(domain, ncells=ncells, periodic=[False, True])
+    derham_h = discretize(derham, domain_h, degree=[2,2])
+    assert isinstance(derham_h, DiscreteDerham)
+
+    psi = lambda alpha, theta : 2*alpha if alpha <= 0.5 else 1.0
+    h1_proj = Projector_H1(derham_h.V0)
+    psi_h = h1_proj(psi) 
+    x, y = sympy.symbols(names='x, y')
+    J = 1e-10
+
+    does_plot_psi = True
+    if does_plot_psi:
+        output_manager = OutputManager('magnetostatic_V0.yml',
+                                             'psi_h.h5')
+        output_manager.add_spaces(V0=derham_h.V0)
+        output_manager.export_space_info()
+        output_manager.set_static()
+        output_manager.export_fields(psi_h=psi_h)
+        post_processor = PostProcessManager(domain=domain,
+                                            space_file='magnetostatic_V0.yml',
+                                            fields_file='psi_h.h5')
+        post_processor.export_to_vtk('psi_h_vtk', npts_per_cell=5, fields='psi_h')
+
+    # Compute right hand side of the curve integral constraint
+    logical_domain_gamma = Square(name='logical_domain_gamma', bounds1=(0,0.5), bounds2=(0,2*np.pi))
+    boundary_logical_domain_gamma = Union(logical_domain_gamma.get_boundary(axis=0, ext=-1),
+                                    logical_domain_gamma.get_boundary(axis=0, ext=1))
+    logical_domain_gamma = Domain(name='logical_domain_gamma',
+                            interiors=logical_domain_gamma.interior,
+                            boundaries=boundary_logical_domain_gamma,
+                            dim=2)
+    distorted_polar_mapping = DistortedPolarMapping(name='polar_mapping', dim=2)
+    omega_gamma = distorted_polar_mapping(logical_domain_gamma)
+    derham_gamma = Derham(domain=omega_gamma, sequence=['H1', 'Hdiv', 'L2'])
+    omega_gamma_h = discretize(omega_gamma, ncells=[8,16], periodic=[False, True])
+    derham_gamma_h = discretize(derham_gamma, omega_gamma_h, degree=[2,2])
+    h1_proj_gamma = Projector_H1(derham_gamma_h.V0)
+    assert isinstance(derham_h, DiscreteDerham)
+
+    sigma, tau = top.elements_of(derham_gamma.V0, names='sigma tau')
+    inner_prod_J = LinearForm(tau, integral(omega_gamma, J*tau))
+    inner_prod_J_h = discretize(inner_prod_J, omega_gamma_h, space=derham_gamma_h.V0)
+    assert isinstance(inner_prod_J_h, DiscreteLinearForm)
+    inner_prod_J_h_stencil = inner_prod_J_h.assemble()
+    # Try changing this to the evaluation using the dicrete linear form directly
+    assert isinstance(inner_prod_J_h_stencil, StencilVector)
+    inner_prod_J_h_vec = inner_prod_J_h_stencil.toarray()
+    logger.debug("np.linalg.norm(inner_prod_J_h_vec):%s\n", np.linalg.norm(inner_prod_J_h_vec))
+
+    psi_h_gamma = h1_proj_gamma(psi)
+    psi_h_gamma_coeffs = psi_h_gamma.coeffs.toarray()
+    c_0 = -4*np.pi
+    rhs_curve_integral = c_0 + np.dot(inner_prod_J_h_vec, psi_h_gamma_coeffs)
+    logger.debug("rhs_curve_integral + 4*np.pi:%s\n", rhs_curve_integral + 4*np.pi)
+
+    h_div_projector = Projector_Hdiv(derham_h.V1)
+    B_exact_1 = lambda x,y: -2*y/(x**2 + y**2) 
+    B_exact_2 = lambda x,y: 2*x/(x**2 + y**2)
+    B_exact = (B_exact_1, B_exact_2)
+    P0, P1, P2 = derham_h.projectors()
+    boundary_data = P1(B_exact) 
+
+
+    B_h_coeffs_arr = solve_magnetostatic_pbm_distorted_annulus(J, psi_h=psi_h, rhs_curve_integral=rhs_curve_integral,
+                                                     boundary_data=boundary_data,
+                                                     derham=derham,
+                                                     derham_h=derham_h,
+                                                     domain_h=domain_h)
+    
+    B_h_coeffs = array_to_psydac(B_h_coeffs_arr, derham_h.V1.vector_space)
+    B_h = FemField(derham_h.V1, coeffs=B_h_coeffs)
+
+    does_plot = True
+    if does_plot:
+        output_manager = OutputManager('spaces_magnetostatic_distorted_annulus.yml', 
+                                       'fields_magnetostatic_distorted_annulus.h5')
+        output_manager.add_spaces(V1=derham_h.V1)
+        output_manager.export_space_info()
+        output_manager.set_static()
+        output_manager.export_fields(B_h=B_h, B_exact=boundary_data)
+        post_processor = PostProcessManager(domain=domain, 
+                                            space_file='spaces_magnetostatic_distorted_annulus.yml',
+                                            fields_file='fields_magnetostatic_distorted_annulus.h5')
+        post_processor.export_to_vtk('magnetostatic_pbm_distorted_annulus_vtk', npts_per_cell=3,
+                                        fields=("B_h", "B_exact"))
+
+
+    eval_grid = [np.array([0.25, 0.5, 0.75]), np.array([np.pi/2, np.pi])]
+    V1h = derham_h.V1
+    assert isinstance(V1h, VectorFemSpace)
+    B_h_eval = V1h.eval_fields(eval_grid, B_h)
+    B_exact_logical = pull_2d_hdiv(B_exact, distorted_polar_mapping.get_callable_mapping())
+    print(B_h_eval)
+    assert abs( B_h_eval[0][0][0,1] - B_exact_logical[0](eval_grid[0][0], eval_grid[1][1])) < 0.01, f"B_h_eval[0][0][0,1] - B_exact[0](eval_grid[0,0], eval_grid[1,1]):{B_h_eval[0][0][0,1] - B_exact_logical[0](eval_grid[0][0], eval_grid[1][1])}"
+    assert abs(B_h_eval[0][1][0,1] - B_exact_logical[1](eval_grid[0][0], eval_grid[1][1])) < 0.01, f"B_h_eval[0][1][0,1] - B_exact[1](eval_grid[0,0], eval_grid[1,1]):{B_h_eval[0][1][0,1] - B_exact_logical[1](eval_grid[0][0], eval_grid[1][1])}"
+    assert abs( B_h_eval[0][1][2,0] - B_exact_logical[1](eval_grid[0][2], eval_grid[1][0])) < 0.01, f"B_h_eval[0][0][2,0] - B_exact[1](eval_grid[0,2], eval_grid[1,0]):{B_h_eval[0][1][2,0] - B_exact_logical[1](eval_grid[0][2], eval_grid[1][0])}"
+
 
 
 if __name__ == '__main__':
     logging.basicConfig(filename='mydebug.log', level=logging.DEBUG, filemode='w')
     # test_magnetostatic_pbm_homogeneous()
     # test_magnetostatic_pbm_manufactured()
-    test_constant_one()
+    # test_constant_one()
+    test_magnetostatic_pbm_annuluslike()
