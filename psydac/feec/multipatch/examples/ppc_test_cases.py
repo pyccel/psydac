b3d1451cb19a93ad1ffc4ec345a27238e9bdc119
Frederik Schnack <frederik.schnack@ipp.mpg.de>
2023-07-27 17:31:52 +0200

    add time-domain Maxwell

--- psydac/feec/multipatch/examples/ppc_test_cases.py
+++ psydac/feec/multipatch/examples/ppc_test_cases.py
@@ -5,7 +5,7 @@ from mpi4py import MPI
 import os
 import numpy as np
 
-from sympy import pi, cos, sin, Tuple, exp
+from sympy import pi, cos, sin, Tuple, exp, atan, atan2
 
 from sympde.topology import Derham
 
@@ -16,18 +16,145 @@ from psydac.feec.multipatch.plotting_utilities          import plot_field
 from psydac.feec.multipatch.plotting_utilities          import get_plotting_grid, my_small_plot, my_small_streamplot
 from psydac.feec.multipatch.multipatch_domain_utilities import build_multipatch_domain
 
-from psydac.feec.multipatch.utilities import sol_ref_fn, error_fn, get_method_name, get_fem_name, get_load_dir
-
 comm = MPI.COMM_WORLD
 
 
 # todo [MCP, 12/02/2022]:  add an 'equation' argument to be able to return 'exact solution'
+
+def get_phi_pulse(x_0, y_0, domain=None):
+    x,y    = domain.coordinates
+    ds2_0 = (0.02)**2
+    sigma_0 = (x-x_0)**2 + (y-y_0)**2
+    phi_0 = exp(-sigma_0**2/(2*ds2_0))
+
+    return phi_0
+
+def get_div_free_pulse(x_0, y_0, domain=None):
+    x,y    = domain.coordinates
+    ds2_0 = (0.02)**2
+    sigma_0 = (x-x_0)**2 + (y-y_0)**2
+    phi_0 = exp(-sigma_0**2/(2*ds2_0))
+    dx_sig_0 = 2*(x-x_0)
+    dy_sig_0 = 2*(y-y_0)
+    dx_phi_0 = - dx_sig_0 * sigma_0 / ds2_0 * phi_0
+    dy_phi_0 = - dy_sig_0 * sigma_0 / ds2_0 * phi_0
+    f_x    =   dy_phi_0
+    f_y    = - dx_phi_0
+    f_vect = Tuple(f_x, f_y)
+
+    return f_vect
+
+def get_curl_free_pulse(x_0, y_0, domain=None, pp=False):
+    # return -grad phi_0
+    x,y    = domain.coordinates
+    if pp:
+        # psi=phi
+        ds2_0 = (0.02)**2
+    else:
+        ds2_0 = (0.1)**2
+    sigma_0 = (x-x_0)**2 + (y-y_0)**2
+    phi_0 = exp(-sigma_0**2/(2*ds2_0))
+    dx_sig_0 = 2*(x-x_0)
+    dy_sig_0 = 2*(y-y_0)
+    dx_phi_0 = - dx_sig_0 * sigma_0 / ds2_0 * phi_0
+    dy_phi_0 = - dy_sig_0 * sigma_0 / ds2_0 * phi_0
+    f_x    = -dx_phi_0
+    f_y    = -dy_phi_0
+    f_vect = Tuple(f_x, f_y)
+
+    return f_vect
+
+def get_Delta_phi_pulse(x_0, y_0, domain=None, pp=False):
+    # return -Delta phi_0, with same phi_0 as in get_curl_free_pulse()
+    x,y    = domain.coordinates
+    if pp:
+        # psi=phi
+        ds2_0 = (0.02)**2
+    else:
+        ds2_0 = (0.1)**2
+    sigma_0 = (x-x_0)**2 + (y-y_0)**2
+    phi_0 = exp(-sigma_0**2/(2*ds2_0))
+    dx_sig_0 = 2*(x-x_0)
+    dy_sig_0 = 2*(y-y_0)
+    dxx_sig_0 = 2
+    dyy_sig_0 = 2
+    dxx_phi_0 = ((dx_sig_0 * sigma_0 / ds2_0)**2 - ((dx_sig_0)**2 + dxx_sig_0 * sigma_0)/ds2_0 ) * phi_0
+    dyy_phi_0 = ((dy_sig_0 * sigma_0 / ds2_0)**2 - ((dy_sig_0)**2 + dyy_sig_0 * sigma_0)/ds2_0 ) * phi_0
+    f    = - dxx_phi_0 - dyy_phi_0
+
+    return f
+
+def get_Gaussian_beam(x_0, y_0, domain=None):
+    # return E = cos(k*x) exp( - x^2 + y^2 / 2 sigma^2) v
+    x,y    = domain.coordinates
+    x = x - x_0
+    y = y - y_0
+    
+    k = 2*pi 
+    sigma = 0.7
+
+    xy = x**2 + y**2
+    ef = exp( - xy/(2*sigma**2) )
+
+    E = cos(k * y) * ef
+    B = y/(sigma**2) * E - sin(k * y) * ef 
+
+    return Tuple(E, 0), B
+
+def get_Gaussian_beam2(x_0, y_0, domain=None):
+    # return E = cos(k*x) exp( - x^2 + y^2 / 2 sigma^2) v
+    x,y    = domain.coordinates
+
+
+    x0 = x_0
+    y0 = y_0
+    theta = pi/2
+    w0 = 1
+
+    t = [(x-x0)*cos(theta) - (y - y0) * sin(theta), (x-x0)*sin(theta) + (y-y0) * cos(theta)]
+
+    ## Gaussian beam
+    '''Beam inciding from the left, centered and normal to wall:
+        x: axial normalized distance to the beam's focus
+        y: radial normalized distance to the center axis of the beam
+    '''
+    EW0 = 1.0  # amplitude at the waist
+    k0 = 2 * pi # free-space wavenumber
+
+    x_ray = pi * w0 ** 2 # Rayleigh range
+
+    w = w0 * ( 1 + t[0]**2/x_ray**2 )**0.5 #width
+    curv = t[0] / ( t[0]**2 + x_ray**2 ) #curvature
+
+    gouy_psi = -0.5 * atan2(t[0] / x_ray, 1.)  # corresponds to atan(x / x_ray), which is the Gouy phase
+
+    EW_mod = EW0 * (w0 / w)**0.5 * exp(-(t[1] ** 2) / (w ** 2)) # Amplitude
+    phase = k0 * t[0] + 0.5 * k0 * curv * t[1] ** 2 + gouy_psi # Phase
+
+    EW_r = EW_mod * cos(phase) # Real part
+    EW_i = EW_mod * sin(phase) # Imaginary part
+
+    B = 0#t[1]/(w**2) * EW_r
+
+    return Tuple(0,EW_r), B
+
 
 def get_source_and_sol_for_magnetostatic_pbm(
     source_type=None,
     domain=None, domain_name=None,
     refsol_params=None
 ):
+    """
+    provide source, and exact solutions when available, for:
+    
+    Find u=B in H(curl) such that
+    
+        div B = 0
+        curl B = j
+
+    written as a mixed problem, see solve_magnetostatic_pbm()
+    """
+    u_ex = None  # exact solution
     x,y    = domain.coordinates
     if source_type == 'dipole_J':
         # we compute two possible source terms:
@@ -62,22 +189,170 @@ def get_source_and_sol_for_magnetostatic_pbm(
     else:
         raise ValueError(source_type)
 
-    # ref solution in V1h:
-    uh_ref = get_sol_ref_V1h(source_type, domain, domain_name, refsol_params)
+    return f_scal, f_vect, j_scal, u_ex
 
-    return f_scal, f_vect, j_scal, uh_ref
+    
+def get_source_and_solution_hcurl(
+    source_type=None, eta=0, mu=0, nu=0,
+    domain=None, domain_name=None):
+    """
+    provide source, and exact solutions when available, for:
+    
+    Find u in H(curl) such that
+    
+      A u = f             on \Omega
+      n x u = n x u_bc    on \partial \Omega
+
+    with
+
+      A u := eta * u  +  mu * curl curl u  -  nu * grad div u
+    
+    see solve_hcurl_source_pbm()
+    """
+
+    # exact solutions (if available)
+    u_ex = None
+    curl_u_ex = None
+    div_u_ex = None
+
+    # bc solution: describe the bc on boundary. Inside domain, values should not matter. Homogeneous bc will be used if None
+    u_bc = None
+
+    # source terms
+    f_vect = None
+    
+    # auxiliary term (for more diagnostics)
+    grad_phi = None
+    phi = None
+
+    x,y    = domain.coordinates
+
+    if source_type == 'manu_maxwell_inhom':
+        # used for Maxwell equation with manufactured solution
+        f_vect  = Tuple(eta*sin(pi*y) - pi**2*sin(pi*y)*cos(pi*x) + pi**2*sin(pi*y),
+                        eta*sin(pi*x)*cos(pi*y) + pi**2*sin(pi*x)*cos(pi*y))
+        if nu == 0:
+            u_ex  = Tuple(sin(pi*y), sin(pi*x)*cos(pi*y))
+            curl_u_ex = pi*(cos(pi*x)*cos(pi*y) - cos(pi*y))
+            div_u_ex = -pi*sin(pi*x)*sin(pi*y)
+        else:
+            raise NotImplementedError
+        u_bc = u_ex
+
+    elif source_type == 'elliptic_J':
+        # no manufactured solution for Maxwell pbm
+        x0 = 1.5
+        y0 = 1.5
+        s  = (x-x0) - (y-y0)
+        t  = (x-x0) + (y-y0)
+        a = (1/1.9)**2
+        b = (1/1.2)**2
+        sigma2 = 0.0121
+        tau = a*s**2 + b*t**2 - 1
+        phi = exp(-tau**2/(2*sigma2))
+        dx_tau = 2*( a*s + b*t)
+        dy_tau = 2*(-a*s + b*t)
+
+        f_x =   dy_tau * phi
+        f_y = - dx_tau * phi
+        f_vect = Tuple(f_x, f_y)
+
+    else:
+        raise ValueError(source_type)
+
+    # u_ex = Tuple(0, 1)  # DEBUG
+    return f_vect, u_bc, u_ex, curl_u_ex, div_u_ex #, phi, grad_phi
+
+def get_source_and_solution_h1(source_type=None, eta=0, mu=0,
+                            domain=None, domain_name=None):
+    """
+    provide source, and exact solutions when available, for:
+    
+    Find u in H^1, such that
+
+      A u = f             on \Omega
+        u = u_bc          on \partial \Omega
+
+    with
+
+      A u := eta * u  -  mu * div grad u
+    
+    see solve_h1_source_pbm()
+    """
+
+    # exact solutions (if available)
+    u_ex = None
+
+    # bc solution: describe the bc on boundary. Inside domain, values should not matter. Homogeneous bc will be used if None
+    u_bc = None
+
+    # source terms
+    f_scal = None
+
+    # auxiliary term (for more diagnostics)
+    grad_phi = None
+    phi = None
+
+    x,y    = domain.coordinates
+
+    if source_type in ['manu_poisson_elliptic']:
+        x0 = 1.5
+        y0 = 1.5
+        s  = (x-x0) - (y-y0)
+        t  = (x-x0) + (y-y0)
+        a = (1/1.9)**2
+        b = (1/1.2)**2
+        sigma2 = 0.0121
+        tau = a*s**2 + b*t**2 - 1
+        phi = exp(-tau**2/(2*sigma2))
+        dx_tau = 2*( a*s + b*t)
+        dy_tau = 2*(-a*s + b*t)
+        dxx_tau = 2*(a + b)
+        dyy_tau = 2*(a + b)
+
+        dx_phi = (-tau*dx_tau/sigma2)*phi
+        dy_phi = (-tau*dy_tau/sigma2)*phi
+        grad_phi = Tuple(dx_phi, dy_phi)
+
+        f_scal = -( (tau*dx_tau/sigma2)**2 - (tau*dxx_tau + dx_tau**2)/sigma2
+                   +(tau*dy_tau/sigma2)**2 - (tau*dyy_tau + dy_tau**2)/sigma2 )*phi
+
+        # exact solution of  -p'' = f  with hom. bc's on pretzel domain
+        if mu == 1 and eta == 0:
+            u_ex = phi
+        else:
+            print('WARNING (54375385643): exact solution not available in this case!') 
+
+        if not domain_name in ['pretzel', 'pretzel_f']:
+            # we may have non-hom bc's            
+            u_bc = u_ex
+
+    elif source_type == 'manu_poisson_2':
+        f_scal = -4
+        if mu == 1 and eta == 0:
+            u_ex = x**2+y**2
+        else:
+            raise NotImplementedError
+        u_bc   = u_ex
+
+    elif source_type == 'manu_poisson_sincos':
+        u_ex    = sin(pi*x)*cos(pi*y)
+        f_scal  = (eta + 2*mu*pi**2) * u_ex
+        u_bc = u_ex
+
+    else:
+        raise ValueError(source_type)
+
+    return f_scal, u_bc, u_ex
 
 
-def get_source_and_solution(source_type=None, eta=0, mu=0, nu=0,
+
+def get_source_and_solution_OBSOLETE(source_type=None, eta=0, mu=0, nu=0,
                             domain=None, domain_name=None,
                             refsol_params=None):
     """
-    compute source and reference solution (exact, or reference values) when possible, depending on the source_type
+    OBSOLETE: kept for some test-cases
     """
-
-    # ref solution (values on diag grid)
-    ph_ref = None
-    uh_ref = None
 
     # exact solutions (if available)
     u_ex = None
@@ -119,7 +394,7 @@ def get_source_and_solution(source_type=None, eta=0, mu=0, nu=0,
 
     elif source_type == 'manutor_poisson':
         # todo: remove if not used ?
-        # same as manu_poisson, with arbitrary value for tor
+        # same as manu_poisson_ellip, with arbitrary value for tor
         x0 = 1.5
         y0 = 1.5
         s  = (x-x0) - (y-y0)
@@ -173,6 +448,9 @@ def get_source_and_solution(source_type=None, eta=0, mu=0, nu=0,
 
         # exact solution of  -p'' = f  with hom. bc's on pretzel domain
         p_ex = phi
+
+        if source_type == 'manu_poisson' and mu == 1 and eta == 0:
+            u_ex = phi
 
         if not domain_name in ['pretzel', 'pretzel_f']:
             print("WARNING (87656547) -- I'm not sure we have an exact solution -- check the bc's on the domain "+domain_name)
@@ -325,40 +603,6 @@ def get_source_and_solution(source_type=None, eta=0, mu=0, nu=0,
     else:
         raise ValueError(source_type)
 
-    if u_ex is None:
-        uh_ref = get_sol_ref_V1h(source_type, domain, domain_name, refsol_params)
-
-    return f_scal, f_vect, u_bc, ph_ref, uh_ref, p_ex, u_ex, phi, grad_phi
+    return f_scal, f_vect, u_bc, p_ex, u_ex, phi, grad_phi
 
 
-def get_sol_ref_V1h( source_type=None, domain=None, domain_name=None, refsol_params=None ):
-    """
-    get a reference solution as a V1h FemField
-    """
-    uh_ref = None
-    if refsol_params is not None:
-        N_diag, method_ref, source_proj_ref = refsol_params
-        u_ref_filename = ( get_load_dir(method=method_ref, domain_name=domain_name,nc=None,deg=None,data='solutions')
-                         + sol_ref_fn(source_type, N_diag, source_proj=source_proj_ref) )
-        print("no exact solution for this test-case, looking for ref solution values in file {}...".format(u_ref_filename))
-        if os.path.isfile(u_ref_filename):
-            print("-- file found")
-            with open(u_ref_filename, 'rb') as file:
-                ncells_degree = np.load(file)
-                ncells   = [int(i) for i in ncells_degree['ncells_degree'][0]]
-                degree   = [int(i) for i in ncells_degree['ncells_degree'][1]]
-
-            derham   = Derham(domain, ["H1", "Hcurl", "L2"])
-            domain_h = discretize(domain, ncells=ncells, comm=comm)
-            V1h      = discretize(derham.V1, domain_h, degree=degree, basis='M')
-            uh_ref   = FemField(V1h)
-            for i,Vi in enumerate(V1h.spaces):
-                for j,Vij in enumerate(Vi.spaces):
-                    filename = u_ref_filename+'_%d_%d'%(i,j)
-                    uij = Vij.import_fields(filename, 'phi')
-                    uh_ref.fields[i].fields[j].coeffs._data = uij[0].coeffs._data
-
-        else:
-            print("-- no file, skipping it")
-
-    return uh_ref
---
+++ psydac/feec/multipatch/examples_nc/hcurl_eigen_pbms_dg.py
@@ -0,0 +1,228 @@
+import os
+from mpi4py import MPI
+from collections import OrderedDict
+
+import numpy as np
+import matplotlib.pyplot
+from scipy.sparse.linalg import spsolve, inv
+from psydac.feec.multipatch.utilities                   import time_count, get_run_dir, get_plot_dir, get_mat_dir, get_sol_dir, diag_fn
+from psydac.feec.multipatch.api                         import discretize
+from psydac.api.settings                                import PSYDAC_BACKENDS
+from sympde.calculus      import grad, dot, curl, cross
+from sympde.calculus      import minus, plus
+from sympde.topology      import VectorFunctionSpace
+from sympde.topology      import elements_of
+from sympde.topology      import NormalVector
+from sympde.topology      import Square
+from sympde.topology      import IdentityMapping, PolarMapping
+from sympde.expr.expr     import LinearForm, BilinearForm
+from sympde.expr.expr     import integral
+from sympde.expr.expr     import Norm
+from sympde.expr.equation import find, EssentialBC
+from scipy.sparse.linalg import LinearOperator, eigsh, minres
+from psydac.linalg.utilities import array_to_psydac
+
+from psydac.api.tests.build_domain   import build_pretzel
+from psydac.fem.basic                import FemField
+from psydac.api.settings             import PSYDAC_BACKEND_GPYCCEL
+from psydac.feec.pull_push           import pull_2d_hcurl
+
+from psydac.feec.multipatch.non_matching_multipatch_domain_utilities import create_square_domain
+from psydac.api.postprocessing import OutputManager, PostProcessManager
+
+def hcurl_solve_eigen_pbm_multipatch_dg(ncells=[[2,2], [2,2]], degree=[3,3], domain=[[0, np.pi],[0, np.pi]], domain_name='refined_square', backend_language='pyccel-gcc', mu=1, nu=0, gamma_h=0,
+                          generalized_pbm=False, sigma=None, ref_sigmas=[], nb_eigs_solve=8, nb_eigs_plot=5, skip_eigs_threshold=1e-7,
+                          plot_dir=None, hide_plots=True, m_load_dir="",):
+
+    diags = {}
+    
+    if sigma is None:
+        raise ValueError('please specify a value for sigma')
+
+    print('---------------------------------------------------------------------------------------------------------')
+    print('Starting hcurl_solve_eigen_pbm function with: ')
+    print(' ncells = {}'.format(ncells))
+    print(' degree = {}'.format(degree))
+    print(' domain_name = {}'.format(domain_name))
+    print(' backend_language = {}'.format(backend_language))
+    print('---------------------------------------------------------------------------------------------------------')
+    t_stamp = time_count()
+    print('building symbolic and discrete domain...')
+
+    int_x, int_y = domain
+    
+    if domain_name == 'refined_square' or domain_name =='square_L_shape':
+        domain = create_square_domain(ncells, int_x, int_y, mapping='identity')
+        ncells_h = {patch.name: [ncells[int(patch.name[2])][int(patch.name[4])], ncells[int(patch.name[2])][int(patch.name[4])]] for patch in domain.interior}
+    elif domain_name == 'curved_L_shape':
+        domain = create_square_domain(ncells, int_x, int_y, mapping='polar')
+        ncells_h = {patch.name: [ncells[int(patch.name[2])][int(patch.name[4])], ncells[int(patch.name[2])][int(patch.name[4])]] for patch in domain.interior}
+    elif domain_name == 'pretzel_f':
+        domain = build_multipatch_domain(domain_name=domain_name) 
+        ncells_h = {patch.name: [ncells[i], ncells[i]] for (i,patch) in enumerate(domain.interior)}
+
+    else:
+        ValueError("Domain not defined.")
+
+   # domain = build_multipatch_domain(domain_name = 'curved_L_shape')
+   #                     
+   # ncells = np.array([4,8,4])
+   # ncells_h = {patch.name: [ncells[i], ncells[i]] for (i,patch) in enumerate(domain.interior)}
+    mappings = OrderedDict([(P.logical_domain, P.mapping) for P in domain.interior])
+    mappings_list = list(mappings.values())
+
+
+    t_stamp = time_count(t_stamp)
+    print(' .. discrete domain...')
+
+    V  = VectorFunctionSpace('V', domain, kind='hcurl')
+
+    u, v, F  = elements_of(V, names='u, v, F')
+    nn       = NormalVector('nn')
+
+    I        = domain.interfaces
+    boundary = domain.boundary
+
+    kappa   = 10
+    k       = 1
+
+    jump = lambda w:plus(w)-minus(w)
+    avr  = lambda w:0.5*plus(w) + 0.5*minus(w)
+
+    expr1_I  =  cross(nn, jump(v))*curl(avr(u))\
+               +k*cross(nn, jump(u))*curl(avr(v))\
+               +kappa*cross(nn, jump(u))*cross(nn, jump(v))
+
+    expr1   = curl(u)*curl(v) 
+    expr1_b = -cross(nn, v) * curl(u) -k*cross(nn, u)*curl(v)  + kappa*cross(nn, u)*cross(nn, v)
+    ## curl curl u = - omega**2 u 
+
+    expr2   = dot(u,v)
+    #expr2_I = kappa*cross(nn, jump(u))*cross(nn, jump(v))
+    #expr2_b = -k*cross(nn, u)*curl(v) + kappa * cross(nn, u) * cross(nn, v)
+
+    # Bilinear form a: V x V --> R
+    a      = BilinearForm((u,v),  integral(domain, expr1) + integral(I, expr1_I) + integral(boundary, expr1_b))
+    
+    # Linear form l: V --> R
+    b     = BilinearForm((u,v), integral(domain, expr2))# + integral(I, expr2_I) + integral(boundary, expr2_b))
+
+    #+++++++++++++++++++++++++++++++
+    # 2. Discretization
+    #+++++++++++++++++++++++++++++++
+
+    domain_h = discretize(domain, ncells=ncells_h)
+    Vh       = discretize(V, domain_h, degree=degree)
+
+    ah = discretize(a, domain_h, [Vh, Vh])
+    Ah_m = ah.assemble().tosparse()
+
+    bh = discretize(b, domain_h, [Vh, Vh])
+    Bh_m = bh.assemble().tosparse()
+
+    all_eigenvalues_2, all_eigenvectors_transp_2 = get_eigenvalues(nb_eigs_solve, sigma, Ah_m, Bh_m)
+
+    #Eigenvalue processing
+    t_stamp = time_count(t_stamp)
+    print('sorting out eigenvalues...')
+    zero_eigenvalues = []
+    if skip_eigs_threshold is not None:
+        eigenvalues = []
+        eigenvectors2 = []
+        for val, vect in zip(all_eigenvalues_2, all_eigenvectors_transp_2.T):
+            if abs(val) < skip_eigs_threshold: 
+                zero_eigenvalues.append(val)
+                # we skip the eigenvector
+            else:
+                eigenvalues.append(val)
+                eigenvectors2.append(vect)
+    else:
+        eigenvalues = all_eigenvalues_2
+        eigenvectors2 = all_eigenvectors_transp_2.T
+    diags['DG'] = True 
+    for k, val in enumerate(eigenvalues):
+        diags['eigenvalue2_{}'.format(k)] = val #eigenvalues[k]
+    
+    for k, val in enumerate(zero_eigenvalues):
+        diags['skipped eigenvalue2_{}'.format(k)] = val
+
+    t_stamp = time_count(t_stamp)
+    print('plotting the eigenmodes...')     
+
+   # OM = OutputManager('spaces.yml', 'fields.h5')
+   # OM.add_spaces(V1h=V1h)
+
+    nb_eigs = len(eigenvalues)
+    for i in range(min(nb_eigs_plot, nb_eigs)):
+        OM = OutputManager(plot_dir+'/spaces2.yml', plot_dir+'/fields2.h5')
+        OM.add_spaces(V1h=Vh)
+        print('looking at emode i = {}... '.format(i))
+        lambda_i  = eigenvalues[i]
+        emode_i = np.real(eigenvectors2[i])
+        norm_emode_i = np.dot(emode_i,Bh_m.dot(emode_i))
+        eh_c = emode_i/norm_emode_i 
+        stencil_coeffs = array_to_psydac(eh_c, Vh.vector_space)
+        vh = FemField(Vh, coeffs=stencil_coeffs)
+        OM.set_static()
+        #OM.add_snapshot(t=i , ts=0) 
+        OM.export_fields(vh = vh)
+
+        #print('norm of computed eigenmode: ', norm_emode_i)
+        # plot the broken eigenmode:
+        OM.export_space_info()
+        OM.close()
+
+        PM = PostProcessManager(domain=domain, space_file=plot_dir+'/spaces2.yml', fields_file=plot_dir+'/fields2.h5' )
+        PM.export_to_vtk(plot_dir+"/eigen2_{}".format(i),grid=None, npts_per_cell=[6]*2,snapshots='all', fields='vh' )
+        PM.close()
+
+        t_stamp = time_count(t_stamp)
+
+    return diags, eigenvalues
+
+
+def get_eigenvalues(nb_eigs, sigma, A_m, M_m):
+    print('-----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  -----  ----- ')
+    print('computing {0} eigenvalues (and eigenvectors) close to sigma={1} with scipy.sparse.eigsh...'.format(nb_eigs, sigma) )
+    mode = 'normal'
+    which = 'LM'
+    # from eigsh docstring:
+    #   ncv = number of Lanczos vectors generated ncv must be greater than k and smaller than n;
+    #   it is recommended that ncv > 2*k. Default: min(n, max(2*k + 1, 20))
+    ncv = 4*nb_eigs
+    print('A_m.shape = ', A_m.shape)
+    try_lgmres = True
+    max_shape_splu = 24000   # OK for nc=20, deg=6 on pretzel_f
+    if A_m.shape[0] < max_shape_splu:
+        print('(via sparse LU decomposition)')
+        OPinv = None
+        tol_eigsh = 0
+    else:
+
+        OP_m = A_m - sigma*M_m
+        tol_eigsh = 1e-7
+        if try_lgmres:
+            print('(via SPILU-preconditioned LGMRES iterative solver for A_m - sigma*M1_m)')
+            OP_spilu = spilu(OP_m, fill_factor=15, drop_tol=5e-5)
+            preconditioner = LinearOperator(OP_m.shape, lambda x: OP_spilu.solve(x) )
+            tol = tol_eigsh
+            OPinv = LinearOperator(
+                matvec=lambda v: lgmres(OP_m, v, x0=None, tol=tol, atol=tol, M=preconditioner,
+                                    callback=lambda x: print('cg -- residual = ', norm(OP_m.dot(x)-v))
+                                    )[0],
+                shape=M_m.shape,
+                dtype=M_m.dtype
+            )
+
+        else:
+            # from https://docs.scipy.org/doc/scipy/reference/generated/scipy.sparse.linalg.eigsh.html:
+            # the user can supply the matrix or operator OPinv, which gives x = OPinv @ b = [A - sigma * M]^-1 @ b.
+            # > here, minres: MINimum RESidual iteration to solve Ax=b
+            # suggested in https://github.com/scipy/scipy/issues/4170
+            print('(with minres iterative solver for A_m - sigma*M1_m)')
+            OPinv = LinearOperator(matvec=lambda v: minres(OP_m, v, tol=1e-10)[0], shape=M_m.shape, dtype=M_m.dtype)
+
+    eigenvalues, eigenvectors = eigsh(A_m, k=nb_eigs, M=M_m, sigma=sigma, mode=mode, which=which, ncv=ncv, tol=tol_eigsh, OPinv=OPinv)
+
+    print("done: eigenvalues found: " + repr(eigenvalues))
+    return eigenvalues, eigenvectors
