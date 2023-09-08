0ac209997d78484a9039d2b24de9bc556b3b9ccc
vcarlier <valentin.carlier@ens.fr>
2023-09-08 11:17:12 +0200

    little fix after merge

--- psydac/api/discretization.py
+++ psydac/api/discretization.py
@@ -386,7 +386,7 @@ def discretize_space(V, domain_h, *, degree=None, multiplicity=None, knots=None,
 
 
         carts    = create_cart(ddms, spaces)
-        g_spaces = {inter:TensorFemSpace( ddms[i], *spaces[i], cart=carts[i], nquads=nquads, dtype=dtype) for i,inter in enumerate(interiors)}
+        g_spaces = {inter:TensorFemSpace( ddms[i], *spaces[i], cart=carts[i], nquads=quad_order, dtype=dtype) for i,inter in enumerate(interiors)}
 
 
         for i,j in connectivity:
