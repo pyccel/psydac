88abcb59564a65c3dae59d3f298ea9bc3cbfd271
e-moral-sanchez <e.moral.sanchez@gmail.com>
2023-09-28 20:33:03 +0200

    missed a comma

--- psydac/linalg/tests/test_solvers.py
+++ psydac/linalg/tests/test_solvers.py
@@ -59,7 +59,7 @@ def define_data(n, p, matrix_data, dtype=float):
 @pytest.mark.parametrize( 'n', [5, 10, 13] )
 @pytest.mark.parametrize('p', [2, 3])
 @pytest.mark.parametrize('dtype', [float, complex])
-@pytest.mark.parametrize('solver', ['cg', 'pcg' 'bicg', 'bicgstab', 'minres', 'lsmr', 'gmres'])
+@pytest.mark.parametrize('solver', ['cg', 'pcg', 'bicg', 'bicgstab', 'minres', 'lsmr', 'gmres'])
 
 def test_solver_tridiagonal(n, p, dtype, solver, verbose=False):
 
