991e3e563bea8de6e5bb6eae01a4f7b5ed32eb83
vcarlier <valentin.carlier@ens.fr>
2023-10-13 11:02:31 +0200

    changing make_knots and adapting the rest

--- psydac/fem/splines.py
+++ psydac/fem/splines.py
@@ -133,7 +133,6 @@ class SplineSpace( FemSpace ):
         self._ncells        = len(grid) - 1
         self._greville      = greville(knots, degree, periodic, multiplicity = multiplicity)
         self._ext_greville  = greville(elevate_knots(knots, degree, periodic, multiplicity=multiplicity), degree+1, periodic, multiplicity = multiplicity)
-
         self._scaling_array = scaling_array
 
         self._parent_multiplicity  = parent_multiplicity
