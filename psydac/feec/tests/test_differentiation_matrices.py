49df6ee902e71ad2b3d65aee865fbd914d17d9b4
vcarlier <valentin.carlier@ens.fr>
2023-10-23 14:03:50 +0200

    update tests

--- psydac/feec/tests/test_differentiation_matrices.py
+++ psydac/feec/tests/test_differentiation_matrices.py
@@ -911,7 +911,7 @@ def test_Divergence_3D(domain, ncells, degree, periodic, seed, multiplicity):
 
 #==============================================================================
 if __name__ == '__main__':
-        
+    
     test_Derivative_1D(domain=[0, 1], ncells=3, degree=3, periodic=False, seed=1, multiplicity=1)
     test_Derivative_1D(domain=[0, 1], ncells=12, degree=3, periodic=True, seed=1, multiplicity=1)
 
