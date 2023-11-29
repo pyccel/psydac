f6c5721b61c04c50a8405fa73cb26a9588eb92ef
vcarlier <valentin.carlier@ens.fr>
2023-11-29 21:36:18 +0100

    resolve merge issue

--- psydac/api/feec.py
+++ psydac/api/feec.py
@@ -131,7 +131,6 @@ class DiscreteDerham(BasicDiscrete):
 
     #--------------------------------------------------------------------------
     def projectors(self, *, kind='global', nquads=None, get_reference=False):
-    def projectors(self, *, kind='global', nquads=None, get_reference=False):
 
         if not (kind == 'global'):
             raise NotImplementedError('only global projectors are available')
@@ -139,7 +138,6 @@ class DiscreteDerham(BasicDiscrete):
         if self.dim == 1:
             P0 = Projector_H1(self.V0)
             P1 = Projector_L2(self.V1, nquads)
-            if self.mapping and not get_reference:
             if self.mapping and not get_reference:
                 P0_m = lambda f: P0(pull_1d_h1(f, self.callable_mapping))
                 P1_m = lambda f: P1(pull_1d_l2(f, self.callable_mapping))
@@ -161,7 +159,6 @@ class DiscreteDerham(BasicDiscrete):
             if self.has_vec : 
                 Pvec = Projector_H1vec(self.H1vec, nquads)
 
-            if self.mapping and not get_reference:
             if self.mapping and not get_reference:
                 P0_m = lambda f: P0(pull_2d_h1(f, self.callable_mapping))
                 P2_m = lambda f: P2(pull_2d_l2(f, self.callable_mapping))
