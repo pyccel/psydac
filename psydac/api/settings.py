b4b843568a952dd9454eb5025945734195ee0e72
Yaman Güçlü <yaman.guclu@gmail.com>
2024-02-07 12:02:19 +0100

    Cleanup backend settings

--- psydac/api/settings.py
+++ psydac/api/settings.py
@@ -1,10 +1,5 @@
 # coding: utf-8
-
-# ... Determine Pyccel version: compiler names changed with version 1.3.0
-import pyccel
-pyccel_version = tuple(map(int, pyccel.__version__.split('.')))
-pyccel_legacy  = pyccel_version < (1, 3, 0)
-# ...
+import platform
 
 
 PSYDAC_DEFAULT_FOLDER = {'name':'__psydac__'}
@@ -12,27 +7,31 @@ PSYDAC_DEFAULT_FOLDER = {'name':'__psydac__'}
 # ... defining PSYDAC backends
 PSYDAC_BACKEND_PYTHON = {'name': 'python', 'tag':'python', 'openmp':False}
 
-PSYDAC_BACKEND_GPYCCEL = {'name':     'pyccel',
-                      'compiler': 'gfortran' if pyccel_legacy else 'GNU',
-                      'flags':    '-O3 -march=native -mtune=native  -mavx -ffast-math -ffree-line-length-none',
-                      'folder': '__gpyccel__',
-                      'tag':'gpyccel',
-                      'openmp':False}
+PSYDAC_BACKEND_GPYCCEL  = {'name': 'pyccel',
+                       'compiler': 'GNU',
+                       'flags'   : '-O3 -march=native -mtune=native -ffast-math -ffree-line-length-none',
+                       'folder'  : '__gpyccel__',
+                       'tag'     : 'gpyccel',
+                       'openmp'  : False}
 
-PSYDAC_BACKEND_IPYCCEL = {'name':     'pyccel',
-                      'compiler': 'ifort' if pyccel_legacy else 'intel',
-                      'flags':    '-O3',
-                      'folder': '__ipyccel__',
-                      'tag':'ipyccel',
-                      'openmp':False}
+PSYDAC_BACKEND_IPYCCEL  = {'name': 'pyccel',
+                       'compiler': 'intel',
+                       'flags'   : '-O3',
+                       'folder'  : '__ipyccel__',
+                       'tag'     :'ipyccel',
+                       'openmp'  : False}
 
-PSYDAC_BACKEND_PGPYCCEL = {'name':     'pyccel',
-                      'compiler': 'pgfortran' if pyccel_legacy else 'PGI',
-                      'flags':    '-O3',
-                      'folder': '__pgpyccel__',
-                       'tag':'pgpyccel',
-                       'openmp':False}
+PSYDAC_BACKEND_PGPYCCEL = {'name': 'pyccel',
+                       'compiler': 'PGI',
+                       'flags'   : '-O3',
+                       'folder'  : '__pgpyccel__',
+                       'tag'     : 'pgpyccel',
+                       'openmp'  : False}
 # ...
+
+# Platform-dependent flags
+if platform.machine() == 'x86_64':
+    PSYDAC_BACKEND_GPYCCEL['flags'] += ' -mavx'
 
 # List of all available backends for accelerating Python code
 PSYDAC_BACKENDS = {
