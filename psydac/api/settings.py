d09e43e96c627f11633b7bb780a3de76c778df66
Yaman Güçlü <yaman.guclu@gmail.com>
2024-02-13 11:36:37 +0100

    Fix GFortran compiler flags on ARM processors (#376)
    - Give the flag `-mavx` to the GFortran compiler only if the processor's
    architecture is `x86_64`;
    - Clean up the backends settings in `psydac.api.settings`;
    - Update `.gitignore`.

--- psydac/api/settings.py
+++ psydac/api/settings.py
@@ -1,43 +1,56 @@
 # coding: utf-8
+import platform
 
-# ... Determine Pyccel version: compiler names changed with version 1.3.0
-import pyccel
-pyccel_version = tuple(map(int, pyccel.__version__.split('.')))
-pyccel_legacy  = pyccel_version < (1, 3, 0)
-# ...
 
+__all__ = ('PSYDAC_DEFAULT_FOLDER', 'PSYDAC_BACKENDS')
+
+#==============================================================================
 
 PSYDAC_DEFAULT_FOLDER = {'name':'__psydac__'}
 
 # ... defining PSYDAC backends
 PSYDAC_BACKEND_PYTHON = {'name': 'python', 'tag':'python', 'openmp':False}
 
-PSYDAC_BACKEND_GPYCCEL = {'name':     'pyccel',
-                      'compiler': 'gfortran' if pyccel_legacy else 'GNU',
-                      'flags':    '-O3 -march=native -mtune=native  -mavx -ffast-math -ffree-line-length-none',
-                      'folder': '__gpyccel__',
-                      'tag':'gpyccel',
-                      'openmp':False}
+PSYDAC_BACKEND_GPYCCEL  = {'name': 'pyccel',
+                       'compiler': 'GNU',
+                       'flags'   : '-O3 -march=native -mtune=native -ffast-math',
+                       'folder'  : '__gpyccel__',
+                       'tag'     : 'gpyccel',
+                       'openmp'  : False}
 
-PSYDAC_BACKEND_IPYCCEL = {'name':     'pyccel',
-                      'compiler': 'ifort' if pyccel_legacy else 'intel',
-                      'flags':    '-O3',
-                      'folder': '__ipyccel__',
-                      'tag':'ipyccel',
-                      'openmp':False}
+PSYDAC_BACKEND_IPYCCEL  = {'name': 'pyccel',
+                       'compiler': 'intel',
+                       'flags'   : '-O3',
+                       'folder'  : '__ipyccel__',
+                       'tag'     :'ipyccel',
+                       'openmp'  : False}
 
-PSYDAC_BACKEND_PGPYCCEL = {'name':     'pyccel',
-                      'compiler': 'pgfortran' if pyccel_legacy else 'PGI',
-                      'flags':    '-O3',
-                      'folder': '__pgpyccel__',
-                       'tag':'pgpyccel',
-                       'openmp':False}
+PSYDAC_BACKEND_PGPYCCEL = {'name': 'pyccel',
+                       'compiler': 'PGI',
+                       'flags'   : '-O3 -Munroll',
+                       'folder'  : '__pgpyccel__',
+                       'tag'     : 'pgpyccel',
+                       'openmp'  : False}
+
+PSYDAC_BACKEND_NVPYCCEL = {'name': 'pyccel',
+                       'compiler': 'nvidia',
+                       'flags'   : '-O3 -Munroll',
+                       'folder'  : '__nvpyccel__',
+                       'tag'     : 'nvpyccel',
+                       'openmp'  : False}
 # ...
+
+# Platform-dependent flags
+if platform.machine() == 'x86_64':
+    PSYDAC_BACKEND_GPYCCEL['flags'] += ' -mavx'
+
+#==============================================================================
 
 # List of all available backends for accelerating Python code
 PSYDAC_BACKENDS = {
-    'python'      : PSYDAC_BACKEND_PYTHON,
-    'pyccel-gcc'  : PSYDAC_BACKEND_GPYCCEL,
-    'pyccel-intel': PSYDAC_BACKEND_IPYCCEL,
-    'pyccel-pgi'  : PSYDAC_BACKEND_PGPYCCEL,
+    'python'       : PSYDAC_BACKEND_PYTHON,
+    'pyccel-gcc'   : PSYDAC_BACKEND_GPYCCEL,
+    'pyccel-intel' : PSYDAC_BACKEND_IPYCCEL,
+    'pyccel-pgi'   : PSYDAC_BACKEND_PGPYCCEL,
+    'pyccel-nvidia': PSYDAC_BACKEND_NVPYCCEL,
 }
