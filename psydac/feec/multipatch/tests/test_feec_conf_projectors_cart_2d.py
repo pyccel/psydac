2af29a874eddae7ee8bac4bb2f676cfba2d09563
Frederik Schnack <frederik.schnack@ipp.mpg.de>
2024-03-05 16:25:49 +0100

    adapt tests

--- psydac/feec/multipatch/tests/test_feec_conf_projectors_cart_2d.py
+++ psydac/feec/multipatch/tests/test_feec_conf_projectors_cart_2d.py
@@ -11,6 +11,7 @@ from sympde.topology                    import Derham
 from psydac.feec.multipatch.api         import discretize
 from psydac.feec.multipatch.operators   import HodgeOperator
 from psydac.feec.multipatch.multipatch_domain_utilities import build_multipatch_domain, create_domain
+from sympde.topology import IdentityMapping, PolarMapping
 
 from psydac.feec.multipatch.non_matching_operators      import construct_scalar_conforming_projection, construct_vector_conforming_projection
 
@@ -47,9 +48,8 @@ def get_polynomial_function(degree, hom_bc_axes, domain):
 @pytest.mark.parametrize('nc', [4])
 @pytest.mark.parametrize('reg', [[0,0]])
 @pytest.mark.parametrize('hom_bc', [[False, False]])
-@pytest.mark.parametrize('mom_pres', [[-1, -1]])
-@pytest.mark.parametrize('domain_name', ["4patch_nc"])
-@pytest.mark.parametrize('nonconforming', [True])
+@pytest.mark.parametrize('domain_name', ["4patch_nc", "2patch_nc"])
+@pytest.mark.parametrize("nonconforming, full_mom_pres", [(True, False), (False, True)])
 
 
 def test_conf_projectors_2d(
@@ -58,7 +58,7 @@ def test_conf_projectors_2d(
         nc, 
         reg,
         hom_bc,
-        mom_pres,
+        full_mom_pres,
         domain_name,  
         nonconforming      
     ):
@@ -67,18 +67,7 @@ def test_conf_projectors_2d(
     print(' .. multi-patch domain...')
 
 
-    if domain_name == '2patch_nc_mapped':
-
-        A = Square('A', bounds1=(0.5, 1), bounds2=(0,       np.pi/2))
-        B = Square('B', bounds1=(0.5, 1), bounds2=(np.pi/2, np.pi))
-        M1 = PolarMapping('M1', 2, c1=0, c2=0, rmin=0., rmax=1.)
-        M2 = PolarMapping('M2', 2, c1=0, c2=0, rmin=0., rmax=1.)
-        A = M1(A)
-        B = M2(B)
-
-        domain = create_domain([A, B], [[A.get_boundary(axis=1, ext=1), B.get_boundary(axis=1, ext=-1), 1]], name='domain')
-
-    elif domain_name == '2patch_nc':
+    if domain_name == '2patch_nc':
 
         A = Square('A', bounds1=(0, 0.5), bounds2=(0, 1))
         B = Square('B', bounds1=(0.5, 1.), bounds2=(0, 1))
@@ -170,7 +159,11 @@ def test_conf_projectors_2d(
     p_V2h = p_derham_h.V2
 
     # full moment preservation only possible if enough interior functions in a patch (<=> enough cells)
-    full_mom_pres = (mom_pres[0] >= degree[0] and mom_pres[1] >= degree[1]) and (nc >= 3 + 2*reg[0]) and (nc >= 3 + 2*reg[1])
+    if full_mom_pres and (nc >= 3 + 2*reg[0]) and (nc >= 3 + 2*reg[1]):
+        mom_pres = degree 
+    else:
+        mom_pres = [-1,-1]
+
     # NOTE: if mom_pres but not full_mom_pres we could test reduced order moment preservation...
 
     # geometric projections (operators)
@@ -200,17 +193,12 @@ def test_conf_projectors_2d(
     D0  = bD0 @ cP0               # Conga grad
     D1  = bD1 @ cP1               # Conga curl or div
 
-    np.allclose(sp_norm(cP0 - cP0@cP0), 0, 1e-12, 1e-12) # cP0 is a projection
-    print(sp_norm(cP0 - cP0@cP0))
-    np.allclose(sp_norm(cP1 - cP1@cP1), 0, 1e-12, 1e-12) # cP1 is a projection
-    print(sp_norm(cP1 - cP1@cP1))
-    np.allclose(sp_norm(cP2 - cP2@cP2), 0, 1e-12, 1e-12) # cP2 is a projection
-    print(sp_norm(cP2 - cP2@cP2))
+    assert np.allclose(sp_norm(cP0 - cP0@cP0), 0, 1e-12, 1e-12) # cP0 is a projection
+    assert np.allclose(sp_norm(cP1 - cP1@cP1), 0, 1e-12, 1e-12) # cP1 is a projection
+    assert np.allclose(sp_norm(cP2 - cP2@cP2), 0, 1e-12, 1e-12) # cP2 is a projection
 
-    np.allclose(sp_norm( D0 - cP1@D0),  0, 1e-12, 1e-12) # D0 maps in the conforming V1 space (where cP1 coincides with Id)
-    print(sp_norm( D0 - cP1@D0))
-    np.allclose(sp_norm( D1 - cP2@D1),  0, 1e-12, 1e-12) # D1 maps in the conforming V2 space (where cP2 coincides with Id)
-    print(sp_norm( D1 - cP2@D1))
+    assert np.allclose(sp_norm( D0 - cP1@D0),  0, 1e-12, 1e-12) # D0 maps in the conforming V1 space (where cP1 coincides with Id)
+    assert np.allclose(sp_norm( D1 - cP2@D1),  0, 1e-12, 1e-12) # D1 maps in the conforming V2 space (where cP2 coincides with Id)
 
     # comparing projections of polynomials which should be exact
     
@@ -222,10 +210,9 @@ def test_conf_projectors_2d(
     tilde_g0_c = p_derham_h.get_dual_dofs(space='V0', f=g0, return_format='numpy_array')
     g0_L2_c = M0_inv @ tilde_g0_c
 
-    np.allclose(g0_c,     g0_L2_c, 1e-12, 1e-12) # (P0_geom - P0_L2) polynomial = 0
-    np.allclose(g0_c, cP0@g0_L2_c, 1e-12, 1e-12) # (P0_geom - confP0 @ P0_L2) polynomial= 0
-    print(np.linalg.norm(g0_c-    g0_L2_c))
-    print(np.linalg.norm(g0_c-    cP0@g0_L2_c))
+    assert np.allclose(g0_c,     g0_L2_c, 1e-12, 1e-12) # (P0_geom - P0_L2) polynomial = 0
+    assert np.allclose(g0_c, cP0@g0_L2_c, 1e-12, 1e-12) # (P0_geom - confP0 @ P0_L2) polynomial= 0
+
     if full_mom_pres:
         # testing that polynomial moments are preserved: 
         #   the following projection should be exact for polynomials of proper degree (no bc)
@@ -236,8 +223,7 @@ def test_conf_projectors_2d(
 
         tilde_g0_c = p_derham_h.get_dual_dofs(space='V0', f=g0, return_format='numpy_array')
         g0_star_c = M0_inv @ cP0.transpose() @ tilde_g0_c
-        np.allclose(g0_c, g0_star_c, 1e-12, 1e-12)  #  (P10_geom - P0_star) polynomial = 0 
-        print(np.linalg.norm(g0_c- g0_star_c))
+        assert np.allclose(g0_c, g0_star_c, 1e-12, 1e-12)  #  (P10_geom - P0_star) polynomial = 0 
         
     # tests on cP1:
 
@@ -254,11 +240,8 @@ def test_conf_projectors_2d(
     tilde_G1_c = p_derham_h.get_dual_dofs(space='V1', f=G1, return_format='numpy_array')
     G1_L2_c = M1_inv @ tilde_G1_c
 
-    np.allclose(G1_c,       G1_L2_c, 1e-12, 1e-12)  
-    print(np.linalg.norm(G1_c-      G1_L2_c))# (P1_geom - P1_L2) polynomial = 0
-    np.allclose(G1_c, cP1 @ G1_L2_c, 1e-12, 1e-12)  # (P1_geom - confP1 @ P1_L2) polynomial= 0
-    print(np.linalg.norm(G1_c-      cP1 @ G1_L2_c))
-
+    assert np.allclose(G1_c,       G1_L2_c, 1e-12, 1e-12)  
+    assert np.allclose(G1_c, cP1 @ G1_L2_c, 1e-12, 1e-12)  # (P1_geom - confP1 @ P1_L2) polynomial= 0
 
     if full_mom_pres:
         # as above
@@ -272,8 +255,7 @@ def test_conf_projectors_2d(
 
         tilde_G1_c = p_derham_h.get_dual_dofs(space='V1', f=G1, return_format='numpy_array')
         G1_star_c = M1_inv @ cP1.transpose() @ tilde_G1_c
-        np.allclose(G1_c, G1_star_c, 1e-12, 1e-12) # (P1_geom - P1_star) polynomial = 0 
-        print(np.linalg.norm(G1_c- G1_star_c))
+        assert np.allclose(G1_c, G1_star_c, 1e-12, 1e-12) # (P1_geom - P1_star) polynomial = 0 
 
     # tests on cP2 (non trivial for reg = 1):
     g2 = get_polynomial_function(degree=[degree[0]-1,degree[1]-1], hom_bc_axes=[False,False], domain=domain)        
@@ -283,38 +265,11 @@ def test_conf_projectors_2d(
     tilde_g2_c = p_derham_h.get_dual_dofs(space='V2', f=g2, return_format='numpy_array')
     g2_L2_c = M2_inv @ tilde_g2_c
 
-    np.allclose(g2_c,       g2_L2_c, 1e-12, 1e-12) # (P2_geom - P2_L2) polynomial = 0
-    np.allclose(g2_c, cP2 @ g2_L2_c, 1e-12, 1e-12) # (P2_geom - confP2 @ P2_L2) polynomial = 0
+    assert np.allclose(g2_c,       g2_L2_c, 1e-12, 1e-12) # (P2_geom - P2_L2) polynomial = 0
+    assert np.allclose(g2_c, cP2 @ g2_L2_c, 1e-12, 1e-12) # (P2_geom - confP2 @ P2_L2) polynomial = 0
 
     if full_mom_pres:                
         # as above, here with same degree and bc as 
         # tilde_g2_c = p_derham_h.get_dual_dofs(space='V2', f=g2, return_format='numpy_array', nquads=nquads)
         g2_star_c = M2_inv @ cP2.transpose() @ tilde_g2_c
-        np.allclose(g2_c, g2_star_c, 1e-12, 1e-12) # (P2_geom - P2_star) polynomial = 0
-
-# if __name__ == '__main__':
-#     V1_type = "Hcurl"
-#     nc = 7
-#     deg = 3
-#     nonconforming = False
-
-#     degree = [deg, deg]
-#     reg=[0,0]
-#     mom_pres=[4,4]
-#     hom_bc = [False, False]
-    
-#     # domain_name = 'square_6'
-#     # domain_name = 'curved_L_shape'
-#     # domain_name = '2patch_nc_mapped'
-#     domain_name = '2patch_nc'
-
-#     test_conf_projectors_2d(
-#         V1_type,
-#         degree,
-#         nc, 
-#         reg,
-#         hom_bc,
-#         mom_pres,   
-#         domain_name,
-#         nonconforming    
-#     )
+        assert np.allclose(g2_c, g2_star_c, 1e-12, 1e-12) # (P2_geom - P2_star) polynomial = 0
