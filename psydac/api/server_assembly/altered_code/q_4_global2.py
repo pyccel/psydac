9f94db99ad91021ee9274f5b95ac4539e70e281b
Julian Owezarek <julian.owezarek@tum.de>
2024-03-15 15:46:43 +0100

    uncomment code

--- psydac/api/server_assembly/altered_code/q_4_global2.py
+++ psydac/api/server_assembly/altered_code/q_4_global2.py
@@ -129,18 +129,16 @@ def assemble_matrix_q_4_global2(global_basis_v1_0_1 : "float64[:,:,:,:]", global
                             v1_0_2 = global_basis_v1_0_2[k_2, i_2, 0, q_2]
                             u1_0_2 = global_basis_u1_0_2[k_2, j_2, 0, q_2]
                             c = v1_0_2 * u1_0_2
-                            a2_u1_0_v1_0[span_v1_0_2 - 4 + i_2, :, 4 - i_2 + j_2, :] += c * a3_u1_0_v1_0[:, :]
-                            #for r_3 in range(0, n_element_3 + 4, 1):
-                            #    a2_u1_0_v1_0[span_v1_0_2 - 4 + i_2, r_3, 4 - i_2 + j_2, :] += c * a3_u1_0_v1_0[r_3, :]
+                            for r_3 in range(0, n_element_3 + 4, 1):
+                                a2_u1_0_v1_0[span_v1_0_2 - 4 + i_2, r_3, 4 - i_2 + j_2, :] += c * a3_u1_0_v1_0[r_3, :]
             for i_1 in range(0, 4, 1):
                 for j_1 in range(0, 4, 1):
                     v1_0_1 = global_basis_v1_0_1[k_1, i_1, 0, q_1]
                     u1_0_1 = global_basis_u1_0_1[k_1, j_1, 0, q_1]
                     c = v1_0_1 * u1_0_1
-                    g_mat_u1_0_v1_0[pad1 + span_v1_0_1 - 3 + i_1, pad2:, pad3:, 3 - i_1 + j_1, :, :] += c * a2_u1_0_v1_0[:, :, :, :]
-                    #for r_2 in range(0, n_element_2 + 4, 1):
-                    #    for r_3 in range(0, n_element_3 + 4, 1):
-                    #        g_mat_u1_0_v1_0[pad1 + span_v1_0_1 - 3 + i_1, pad2 + r_2, pad3 + r_3, 3 - i_1 + j_1, :, :] += c * a2_u1_0_v1_0[r_2, r_3, :, :]
+                    for r_2 in range(0, n_element_2 + 4, 1):
+                        for r_3 in range(0, n_element_3 + 4, 1):
+                            g_mat_u1_0_v1_0[pad1 + span_v1_0_1 - 3 + i_1, pad2 + r_2, pad3 + r_3, 3 - i_1 + j_1, :, :] += c * a2_u1_0_v1_0[r_2, r_3, :, :]
 
     for k_1 in range(0, n_element_1, 1):
         span_v1_0_1 = global_span_v1_0_1[k_1]
