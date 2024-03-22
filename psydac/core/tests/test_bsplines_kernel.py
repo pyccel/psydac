8e0fce18406a2ed9fced0e066f1b88bce9ce276e
Martin Campos Pinto <martin.campos-pinto@ipp.mpg.de>
2024-03-22 15:25:51 +0100

    add test comparing with np.searchsorted

--- psydac/core/tests/test_bsplines_kernel.py
+++ psydac/core/tests/test_bsplines_kernel.py
@@ -8,7 +8,6 @@ from psydac.core.bsplines_kernels import cell_index_p
 
 def test_cell_index_p():
     breaks = np.array([0. , 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.])
-    # i_grid = np.asarray(i_grid)
     breaks = np.ascontiguousarray(breaks, dtype=float)
     out = np.zeros_like(breaks, dtype=np.int64)
     tol = 1e-15
@@ -25,4 +24,12 @@ def test_cell_index_p():
         i_grid[:] += offset
         status = cell_index_p(breaks, i_grid, tol, out)
         assert status == expected_status
+
+    # checking that the values match those of searchsorted (-1) for arbitrary grid points
+    i_grid = np.random.random(30)
+    out = np.zeros_like(i_grid, dtype=np.int64)
+    status = cell_index_p(breaks, i_grid, tol, out)
+    assert status == 0
+    nps = np.searchsorted(breaks, i_grid)-1
+    assert np.allclose(out, nps)
 
