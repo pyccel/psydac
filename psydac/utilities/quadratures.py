0286e51228e206232d3cd2e8b5838a4ca463a372
Yaman Güçlü <yaman.guclu@gmail.com>
2024-04-10 16:35:17 +0200

    Improve function `gauss_legendre` of module `psydac.utilities.quadratures`:
    * Get number of quadrature points `m` as input, not some unclear "quadrature
      order";
    * Properly format default tolerance `tol` of Newton-Raphson method;
    * Return quadrature points abscissas in ascending order, not descending.
    * Add extended docstring.

--- psydac/utilities/quadratures.py
+++ psydac/utilities/quadratures.py
@@ -9,45 +9,70 @@ with weights equal to 1
 
 import numpy as np
 
+from math import cos, pi
+from numpy import zeros
+
+
 __all__ = ('gauss_legendre', 'gauss_lobatto', 'quadrature')
 
-# ....
-def gauss_legendre(ordergl,tol=10e-14):
-    """
-    Returns nodal abscissas {x} and weights {A} of
-    Gauss-Legendre m-point quadrature.
-    """
-    assert ordergl is not None
-    
-    m = ordergl + 1
-    from math import cos,pi
-    from numpy import zeros
 
-    def legendre(t,m):
-        p0 = 1.0; p1 = t
-        for k in range(1,m):
-            p = ((2.0*k + 1.0)*t*p1 - k*p0)/(1.0 + k )
-            p0 = p1; p1 = p
+def gauss_legendre(m, tol=1e-13):
+    """
+    Compute Gauss-Legendre quadrature points and weights on [-1, 1].
+
+    Returns nodal abscissas {x} and weights {A} of a Gauss-Legendre m-point
+    quadrature over the canonical interval [-1, 1].
+
+    Parameters
+    ----------
+    m : int
+        Number of quadrature points in the quadrature rule.
+
+    tol : float
+        Tolerance for the Newton-Raphson root-searching method.
+
+    Returns
+    -------
+    x : numpy.ndarray[float]
+        Abscissas of the quadrature points, in ascending order.
+
+    A : numpy.ndarray[float]
+        Weights of the quadrature points corresponding to the abscissas above.
+
+    """
+    assert isinstance(m, int)
+    assert isinstance(tol, float)
+    assert m >= 1
+    assert tol >= 0
+
+    def legendre(t, m):
+        p0 = 1.0
+        p1 = t
+        for k in range(1, m):
+            p = ((2.0*k + 1.0)*t*p1 - k*p0)/(1.0 + k)
+            p0 = p1
+            p1 = p
         dp = m*(p0 - t*p1)/(1.0 - t**2)
-        return p1,dp
+        return p1, dp
 
     A = zeros(m)
     x = zeros(m)
-    nRoots = (m + 1)// 2          # Number of non-neg. roots
+    nRoots = (m + 1) // 2          # Number of non-neg. roots
     for i in range(nRoots):
         t = cos(pi*(i + 0.75)/(m + 0.5))  # Approx. root
         for j in range(30):
-            p,dp = legendre(t,m)          # Newton-Raphson
-            dt = -p/dp; t = t + dt        # method
+            p, dp = legendre(t, m)        # Newton-Raphson
+            dt = -p/dp                    # method
+            t = t + dt
             if abs(dt) < tol:
-                x[i] = t; x[m-i-1] = -t
-                A[i] = 2.0/(1.0 - t**2)/(dp**2) # Eq.(6.25)
+                x[i]     = -t
+                x[m-i-1] =  t
+                A[i]     = 2.0/(1.0 - t**2)/(dp**2) # Eq.(6.25)
                 A[m-i-1] = A[i]
                 break
-    return x,A
-# ...
+    return x, A
 
-# ...
+
 def gauss_lobatto(k):
     """
     Returns nodal abscissas {x} and weights {A} of
@@ -61,9 +86,9 @@ def gauss_lobatto(k):
     w = 2 * (V[0, :]) ** 2; # weights
 
     return xg, w[i]
-# ....
 
-# ....
+
+
 def quadrature(a, k, method="legendre"):
     """
     this routine generates a quad pts on the grid linspace(a,b,N)
@@ -87,4 +112,3 @@ def quadrature(a, k, method="legendre"):
         wgl[i, :] = 0.5 * ( xmax - xmin ) * w
 
     return xgl,wgl
-# ....
