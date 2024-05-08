c7f6aae8aee0ea8d44093197c0644f7c02b6ab42
kvrigor <kvrigor@users.noreply.github.com>
2024-05-08 11:56:13 +0200

    Remove deprecated distutils (Python >=3.12)
    * Removed finalize_options() since this method does not
      differ from its base class implementation, therefore
      making the override unnecessary.
    * For run(), it's sufficient to call the base class run()
      and then add the custom pyccel commands.
    * Formatted code to conform with Pylint and PEP8 checks

--- setup.py
+++ setup.py
@@ -1,57 +1,35 @@
-import setuptools.command.build_py
-import distutils.command.build_py as orig
-import distutils.log
-import setuptools
-from subprocess import run as sub_run
-import shutil
+import logging
 import os
+import shutil
+from subprocess import run as sub_run
+from setuptools import setup
+from setuptools.command.build_py import build_py
 
 
-class BuildPyCommand(setuptools.command.build_py.build_py):
+class BuildPyCommand(build_py):
     """Custom build command to pyccelise _kernels files in the build directory."""
 
-    # Copy the setuptools.command.build_py.build_py.finalize_options to recreate the __updated_files variable
-    def finalize_options(self):
-        orig.build_py.finalize_options(self)
-        self.package_data = self.distribution.package_data
-        self.exclude_package_data = self.distribution.exclude_package_data or {}
-        if 'data_files' in self.__dict__:
-            del self.__dict__['data_files']
-        self.__updated_files = []
-
-    # Rewrite the build_module function to copy each module in the build repository and pyccelise the modules ending with _kernels
+    # Rewrite the build_module function to copy each module in the build
+    # repository and pyccelise the modules ending with _kernels
     def build_module(self, module, module_file, package):
-        # This part is copied from distutils.command.build_py.build_module
-        if isinstance(package, str):
-            package = package.split('.')
-        elif not isinstance(package, (list, tuple)):
-            raise TypeError(
-                "'package' must be a string (dot-separated), list, or tuple"
-            )
-        outfile = self.get_module_outfile(self.build_lib, package, module)
-        dirname = os.path.dirname(outfile)
-        self.mkpath(dirname)
-        outfile, copied = self.copy_file(module_file, outfile, preserve_mode=0)
+        outfile, copied = super().build_module(module, module_file, package)
 
-
-        # This part check if the module is pyccelisable and pyccelise it in case
+        # This part check if the module is pyccelisable and pyccelise it in
+        # case
         if module.endswith('_kernels'):
-            self.announce(
-                '\nPyccelise module: %s' % str(module),
-                level=distutils.log.INFO)
-            sub_run([shutil.which('pyccel'), outfile, '--language', 'fortran', '--openmp'], shell=False)
-
-        # This part is copy from setuptools.command.build_py.build_module
-        if copied:
-            self.__updated_files.append(outfile)
+            self.announce(f"\nPyccelise module: {module}", level=logging.INFO)
+            sub_run([shutil.which('pyccel'), outfile,
+                     '--language', 'fortran',
+                     '--openmp'],
+                    shell=False, check=True)
 
         return outfile, copied
 
     def run(self):
-        setuptools.command.build_py.build_py.run(self)
+        super().run()
 
         # Remove __pyccel__ directories
-        sub_run(['pyccel-clean', self.build_lib], shell=False)
+        sub_run(['pyccel-clean', self.build_lib], shell=False, check=True)
 
         # Remove useless .lock files
         for path, subdirs, files in os.walk(self.build_lib):
@@ -60,7 +38,7 @@ class BuildPyCommand(setuptools.command.build_py.build_py):
                     os.remove(os.path.join(path, name))
 
 
-setuptools.setup(
+setup(
     cmdclass={
         'build_py': BuildPyCommand,
     },
