5371468957e06a45a0189052bfe363204f88dcb7
Frederik Schnack <frederik.schnack@ipp.mpg.de>
2024-05-23 16:52:36 +0200

    update tests

--- psydac/feec/multipatch/tests/test_feec_maxwell_multipatch_2d.py
+++ psydac/feec/multipatch/tests/test_feec_maxwell_multipatch_2d.py
@@ -9,54 +9,54 @@ from psydac.feec.multipatch.examples.hcurl_eigen_pbms_conga_2d import hcurl_solv
 from psydac.feec.multipatch.examples_nc.hcurl_eigen_pbms_nc import hcurl_solve_eigen_pbm_nc
 from psydac.feec.multipatch.examples_nc.hcurl_eigen_pbms_dg import hcurl_solve_eigen_pbm_dg
 
+
 def test_time_harmonic_maxwell_pretzel_f():
-    nc,deg      = 10,2
+    nc, deg = 10, 2
     source_type = 'manu_maxwell'
     domain_name = 'pretzel_f'
 
-    omega = np.sqrt(170) # source
-    roundoff = 1e4
-    eta = int(-omega**2 * roundoff)/roundoff
+    eta = -170.0  # source
 
     l2_error = solve_hcurl_source_pbm(
         nc=nc, deg=deg,
         eta=eta,
         nu=0,
-        mu=1, #1,
+        mu=1,
         domain_name=domain_name,
         source_type=source_type,
         backend_language='pyccel-gcc')
 
-    assert abs(l2_error - 0.06246693595198972)<1e-10
+    assert abs(l2_error - 0.06247745643640749) < 1e-10
+
 
 def test_time_harmonic_maxwell_pretzel_f_nc():
-    deg      = 2
-    nc = np.array([20, 20, 20, 20, 20, 10, 10, 10, 10, 10, 10, 10, 10, 20, 20, 20, 10, 10])
-    
+    deg = 2
+    nc = np.array([20, 20, 20, 20, 20, 10, 10, 10, 10,
+                  10, 10, 10, 10, 20, 20, 20, 10, 10])
+
     source_type = 'manu_maxwell'
     domain_name = 'pretzel_f'
     source_proj = 'tilde_Pi'
 
-    omega = np.sqrt(170) # source
-    roundoff = 1e4
-    eta = int(-omega**2 * roundoff)/roundoff
+    eta = -170.0
 
     l2_error = solve_hcurl_source_pbm_nc(
         nc=nc, deg=deg,
         eta=eta,
         nu=0,
-        mu=1, 
+        mu=1,
         domain_name=domain_name,
         source_type=source_type,
         source_proj=source_proj,
         plot_dir='./plots/th_maxell_nc',
-        backend_language='pyccel-gcc', 
+        backend_language='pyccel-gcc',
         test=True)
 
-    assert abs(l2_error - 0.04753982587323614)<1e-10
+    assert abs(l2_error - 0.04753613858909066) < 1e-10
+
 
 def test_maxwell_eigen_curved_L_shape():
-    domain_name = 'curved_L_shape' 
+    domain_name = 'curved_L_shape'
 
     nc = 10
     deg = 2
@@ -67,7 +67,7 @@ def test_maxwell_eigen_curved_L_shape():
         0.100656015004E+02,
         0.101118862307E+02,
         0.124355372484E+02,
-        ]
+    ]
     sigma = 7
     nb_eigs_solve = 7
     nb_eigs_plot = 7
@@ -90,17 +90,18 @@ def test_maxwell_eigen_curved_L_shape():
     error = 0
     n_errs = min(len(ref_sigmas), len(eigenvalues))
     for k in range(n_errs):
-        error += (eigenvalues[k]-ref_sigmas[k])**2
+        error += (eigenvalues[k] - ref_sigmas[k])**2
     error = np.sqrt(error)
 
-    assert abs(error - 0.023413963252245817)<1e-10
+    assert abs(error - 0.023395836648441557) < 1e-10
+
 
 def test_maxwell_eigen_curved_L_shape_nc():
-    domain_name = 'curved_L_shape' 
-    domain=[[1, 3],[0, np.pi/4]] 
+    domain_name = 'curved_L_shape'
+    domain = [[1, 3], [0, np.pi / 4]]
 
     ncells = np.array([[None, 10],
-                   [10,   20]])
+                       [10, 20]])
 
     degree = [2, 2]
 
@@ -110,7 +111,7 @@ def test_maxwell_eigen_curved_L_shape_nc():
         0.100656015004E+02,
         0.101118862307E+02,
         0.124355372484E+02,
-        ]
+    ]
     sigma = 7
     nb_eigs_solve = 7
     nb_eigs_plot = 7
@@ -135,17 +136,18 @@ def test_maxwell_eigen_curved_L_shape_nc():
     error = 0
     n_errs = min(len(ref_sigmas), len(eigenvalues))
     for k in range(n_errs):
-        error += (eigenvalues[k]-ref_sigmas[k])**2
+        error += (eigenvalues[k] - ref_sigmas[k])**2
     error = np.sqrt(error)
-    
-    assert abs(error - 0.004289103786542442)<1e-10
+
+    assert abs(error - 0.004301175400024398) < 1e-10
+
 
 def test_maxwell_eigen_curved_L_shape_dg():
-    domain_name = 'curved_L_shape' 
-    domain=[[1, 3],[0, np.pi/4]] 
+    domain_name = 'curved_L_shape'
+    domain = [[1, 3], [0, np.pi / 4]]
 
     ncells = np.array([[None, 10],
-                   [10,   20]])
+                       [10, 20]])
 
     degree = [2, 2]
 
@@ -155,7 +157,7 @@ def test_maxwell_eigen_curved_L_shape_dg():
         0.100656015004E+02,
         0.101118862307E+02,
         0.124355372484E+02,
-        ]
+    ]
     sigma = 7
     nb_eigs_solve = 7
     nb_eigs_plot = 7
@@ -180,18 +182,20 @@ def test_maxwell_eigen_curved_L_shape_dg():
     error = 0
     n_errs = min(len(ref_sigmas), len(eigenvalues))
     for k in range(n_errs):
-        error += (eigenvalues[k]-ref_sigmas[k])**2
+        error += (eigenvalues[k] - ref_sigmas[k])**2
     error = np.sqrt(error)
 
-    assert abs(error - 0.004208158031148591)<1e-10
+    assert abs(error - 0.004208158031148591) < 1e-10
 
-#==============================================================================
+# ==============================================================================
 # CLEAN UP SYMPY NAMESPACE
-#==============================================================================
+# ==============================================================================
+
 
 def teardown_module():
     from sympy.core import cache
     cache.clear_cache()
+
 
 def teardown_function():
     from sympy.core import cache
--- psydac/feec/multipatch/tests/test_feec_poisson_multipatch_2d.py
+++ psydac/feec/multipatch/tests/test_feec_poisson_multipatch_2d.py
@@ -3,50 +3,55 @@ import numpy as np
 from psydac.feec.multipatch.examples.h1_source_pbms_conga_2d import solve_h1_source_pbm
 from psydac.feec.multipatch.examples_nc.h1_source_pbms_nc import solve_h1_source_pbm_nc
 
+
 def test_poisson_pretzel_f():
 
     source_type = 'manu_poisson_2'
     domain_name = 'pretzel_f'
-    nc  = 10
+    nc = 10
     deg = 2
     run_dir = '{}_{}_nc={}_deg={}/'.format(domain_name, source_type, nc, deg)
     l2_error = solve_h1_source_pbm(
-                nc=nc, deg=deg,
-                eta=0,
-                mu=1,
-                domain_name=domain_name,
-                source_type=source_type,
-                backend_language='pyccel-gcc',
-                plot_source=False,
-                plot_dir='./plots/h1_tests_source_february/'+run_dir)
+        nc=nc, deg=deg,
+        eta=0,
+        mu=1,
+        domain_name=domain_name,
+        source_type=source_type,
+        backend_language='pyccel-gcc',
+        plot_source=False,
+        plot_dir='./plots/h1_tests_source_february/' + run_dir)
 
-    assert abs(l2_error-0.1173467869129417)<1e-10
+    assert abs(l2_error - 8.054935880166114e-05) < 1e-10
+
 
 def test_poisson_pretzel_f_nc():
 
     source_type = 'manu_poisson_2'
     domain_name = 'pretzel_f'
-    nc  = np.array([20, 20, 20, 20, 20, 10, 10, 10, 10, 10, 10, 10, 10, 20, 20, 20, 10, 10])
+    nc = np.array([20, 20, 20, 20, 20, 10, 10, 10, 10,
+                  10, 10, 10, 10, 20, 20, 20, 10, 10])
     deg = 2
     run_dir = '{}_{}_nc={}_deg={}/'.format(domain_name, source_type, nc, deg)
     l2_error = solve_h1_source_pbm_nc(
-                nc=nc, deg=deg,
-                eta=0,
-                mu=1,
-                domain_name=domain_name,
-                source_type=source_type,
-                backend_language='pyccel-gcc',
-                plot_source=False,
-                plot_dir='./plots/h1_tests_source_february/'+run_dir)
-    print(l2_error)
-    assert abs(l2_error-0.03821274975800339)<1e-10
-#==============================================================================
+        nc=nc, deg=deg,
+        eta=0,
+        mu=1,
+        domain_name=domain_name,
+        source_type=source_type,
+        backend_language='pyccel-gcc',
+        plot_source=False,
+        plot_dir='./plots/h1_tests_source_february/' + run_dir)
+
+    assert abs(l2_error - 4.6086851224995065e-05) < 1e-10
+# ==============================================================================
 # CLEAN UP SYMPY NAMESPACE
-#==============================================================================
+# ==============================================================================
+
 
 def teardown_module():
     from sympy.core import cache
     cache.clear_cache()
+
 
 def teardown_function():
     from sympy.core import cache
