69b984e7610c32cc0a25e60d4ef866d5b581f2c3
Yaman Güçlü <yaman.guclu@gmail.com>
2025-03-20 12:28:13 +0100

    Print generated assembly function before calling epyccel on it

--- psydac/api/fem.py
+++ psydac/api/fem.py
@@ -8,10 +8,13 @@ import numpy as np
 import string
 import random
 import importlib
+import inspect
 
 from sympy                  import ImmutableDenseMatrix, Matrix, Symbol, sympify
 from sympy.tensor.indexed   import Indexed, IndexedBase
 from sympy.simplify         import cse_main
+
+from pyccel.epyccel import epyccel
 
 from sympde.expr          import BilinearForm as sym_BilinearForm
 from sympde.expr          import LinearForm as sym_LinearForm
@@ -1532,7 +1535,12 @@ class DiscreteBilinearForm(BasicDiscrete):
         #    self._func = new_func
 
         package = importlib.import_module(f'__psydac__.assemble_{id}')
-        from pyccel.epyccel import epyccel
+
+        print("\n\n" + "#" + "-"*78)
+        print("Source code of the generated assembly method:")
+        print("#" + "-"*78 + "\n")
+        print(inspect.getsource(package.assemble_matrix))
+
         new_func = epyccel(package.assemble_matrix, language='fortran')
         self._func = new_func
 
