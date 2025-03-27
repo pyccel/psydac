116e74ad1bdbab6c03bbae5721bffddd9853ac8c
Stefan Possanner <spossann@ipp.mpg.de>
2025-03-27 13:36:47 +0100

    remove psydac/pyccel

--- psydac/pyccel/ast/singleton.py
+++
@@ -1,8 +0,0 @@
-class Singleton(type):
-    """ Indicates that there is only one instance of the class
-    """
-    _instances = {}
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super().__call__(*args, **kwargs)
-        return cls._instances[cls]
