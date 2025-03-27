923666ad57ab493e872d5203680b83cfd0fd5c6a
Stefan Possanner <spossann@ipp.mpg.de>
2025-03-27 14:03:46 +0100

    clean __init__

--- psydac/fem/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: UTF-8 -*-
-
-from psydac.fem import basic
-from psydac.fem import context
-from psydac.fem import splines
-from psydac.fem import tensor
-from psydac.fem import vector
-from psydac.fem import partitioning
--- psydac/utilities/__init__.py
+++
@@ -1,3 +0,0 @@
-__all__ = ['quadratures']
-
-from psydac.utilities import quadratures
