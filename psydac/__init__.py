eb4d7d7d591c8f2706818c6281071e2494fd6cd3
Stefan Possanner <spossann@ipp.mpg.de>
2025-03-27 12:02:13 +0100

    clear out psydac/api

--- psydac/__init__.py
+++ psydac/__init__.py
@@ -1,10 +1,9 @@
 # -*- coding: UTF-8 -*-
-__all__     = ['__version__', 'api', 'cad', 'core', 'ddm', 'feec', 'fem',
+__all__     = ['__version__', 'cad', 'core', 'ddm', 'feec', 'fem',
                'linalg', 'mapping', 'utilities']
 
 from psydac.version import __version__
 
-from psydac import api
 from psydac import cad
 from psydac import core
 from psydac import ddm
--- psydac/api/ast/__init__.py
+++
@@ -1,8 +0,0 @@
-from psydac.api.ast import nodes
-from psydac.api.ast import parser
-from psydac.api.ast import utilities
-from psydac.api.ast import fem
-from psydac.api.ast import glt
-from psydac.api.ast import expr
-from psydac.api.ast import evaluation
-
