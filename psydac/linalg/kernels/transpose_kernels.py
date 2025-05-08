374ecb504e0876ef9d52a18482e0b4d10087748a
Max <max.lindqvist@ipp.mpg.de>
2025-05-08 15:32:30 +0200

    Remove OpenMP annotations (#30)
    * # --> #$ omp
    
    * Commented out openmp annotations
    
    * Trigger CI

--- psydac/linalg/kernels/transpose_kernels.py
+++ psydac/linalg/kernels/transpose_kernels.py
@@ -16,10 +16,10 @@ def transpose_1d(M  : "T[:,:]",
                  sk : "int64[:]",
                  sl : "int64[:]"):
 
-    #$omp parallel default(private) shared(Mt,M) firstprivate( n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
+    # -- removed omp: #$ omp parallel default(private) shared(Mt,M) firstprivate( n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
     d1 = gp[0] - p[0]
     e1 = nd[0] - sl[0]
-    #$omp for schedule(static) collapse(1)
+    # -- removed omp: #$ omp for schedule(static) collapse(1)
     for x1 in range(n[0]):
 
         j1 = dm[0] * gp[0] + x1
@@ -31,7 +31,7 @@ def transpose_1d(M  : "T[:,:]",
 
             if k1 < ndT[0] and k1 > -1 and l1  < e1 and i1 < nc[0]:
                 Mt[j1, l1 + sl[0]] = M[i1, k1]
-    #$omp end parallel
+    # -- removed omp: #$ omp end parallel
     return
 
 #========================================================================================================
@@ -50,14 +50,14 @@ def transpose_2d(M  : "T[:,:,:,:]",
                  sk : "int64[:]",
                  sl : "int64[:]"):
 
-    #$omp parallel default(private) shared(Mt,M) firstprivate( n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
+    # -- removed omp: #$ omp parallel default(private) shared(Mt,M) firstprivate( n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
     d1 = gp[0] - p[0]
     d2 = gp[1] - p[1]
 
     e1 = nd[0] - sl[0]
     e2 = nd[1] - sl[1]
 
-    #$omp for schedule(static) collapse(2)
+    # -- removed omp: #$ omp for schedule(static) collapse(2)
     for x1 in range(n[0]):
         for x2 in range(n[1]):
 
@@ -75,7 +75,7 @@ def transpose_2d(M  : "T[:,:,:,:]",
 
                     if k1<ndT[0] and k1>-1 and k2<ndT[1] and k2>-1 and l1<e1 and l2<e2 and i1<nc[0] and i2<nc[1]:
                         Mt[j1,j2, l1 + sl[0],l2 + sl[1]] = M[i1,i2, k1,k2]
-    #$omp end parallel
+    # -- removed omp: #$ omp end parallel
     return
 
 #========================================================================================================
@@ -94,7 +94,7 @@ def transpose_3d(M  : "T[:,:,:,:,:,:]",
                  sk : "int64[:]",
                  sl : "int64[:]"):
 
-    #$omp parallel default(private) shared(Mt,M) firstprivate(n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
+    # -- removed omp: #$ omp parallel default(private) shared(Mt,M) firstprivate(n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
     d1 = gp[0] - p[0]
     d2 = gp[1] - p[1]
     d3 = gp[2] - p[2]
@@ -103,7 +103,7 @@ def transpose_3d(M  : "T[:,:,:,:,:,:]",
     e2 = nd[1] - sl[1]
     e3 = nd[2] - sl[2]
 
-    #$omp for schedule(static) collapse(3)
+    # -- removed omp: #$ omp for schedule(static) collapse(3)
     for x1 in range(n[0]):
         for x2 in range(n[1]):
             for x3 in range(n[2]):
@@ -127,7 +127,7 @@ def transpose_3d(M  : "T[:,:,:,:,:,:]",
                             if k1<ndT[0] and k1>-1 and k2<ndT[1] and k2>-1 and k3<ndT[2] and k3>-1\
                                 and l1<e1 and l2<e2 and l3<e3 and i1<nc[0] and i2<nc[1] and i3<nc[2]:
                                 Mt[j1,j2,j3, l1 + sl[0],l2 + sl[1],l3 + sl[2]] = M[i1,i2,i3, k1,k2,k3]
-    #$omp end parallel
+    # -- removed omp: #$ omp end parallel
     return
 
 #========================================================================================================
@@ -146,11 +146,11 @@ def interface_transpose_1d(M  : "T[:,:]",
                            sk : "int64[:]",
                            sl : "int64[:]"):
 
-    #$ omp parallel default(private) shared(Mt,M) firstprivate(n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
+    # -- removed omp: #$ omp parallel default(private) shared(Mt,M) firstprivate(n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
     d1 = gp[0] - p [0]
     e1 = nd[0] - sl[0]
 
-    #$ omp for schedule(static) collapse(1)
+    # -- removed omp: #$ omp for schedule(static) collapse(1)
     for x1 in range(n[0]):
 
                 j1 = gp[0] + x1
@@ -164,7 +164,7 @@ def interface_transpose_1d(M  : "T[:,:]",
                             if k1<ndT[0] and k1>-1 and l1<e1 and i1<nc[0]:
                                 Mt[j1, l1+sl[0]] = M[i1, k1]
 
-    #$ omp end parallel
+    # -- removed omp: #$ omp end parallel
     return
 
 #========================================================================================================
@@ -183,14 +183,14 @@ def interface_transpose_2d(M  : "T[:,:,:,:]",
                            sk : "int64[:]",
                            sl : "int64[:]"):
 
-    #$ omp parallel default(private) shared(Mt,M) firstprivate(n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
+    # -- removed omp: #$ omp parallel default(private) shared(Mt,M) firstprivate(n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
     d1 = gp[0] - p[0]
     d2 = gp[1] - p[1]
 
     e1 = nd[0] - sl[0]
     e2 = nd[1] - sl[1]
 
-    #$ omp for schedule(static) collapse(2)
+    # -- removed omp: #$ omp for schedule(static) collapse(2)
     for x1 in range(n[0]):
         for x2 in range(n[1]):
 
@@ -210,7 +210,7 @@ def interface_transpose_2d(M  : "T[:,:,:,:]",
                               and l1<e1 and l2<e2 and i1<nc[0] and i2<nc[1]:
                                 Mt[j1,j2, l1+sl[0],l2+sl[1]] = M[i1,i2, k1,k2]
 
-    #$ omp end parallel
+    # -- removed omp: #$ omp end parallel
     return
 
 #========================================================================================================
@@ -229,7 +229,7 @@ def interface_transpose_3d(M  : "T[:,:,:,:,:,:]",
                            sk : "int64[:]",
                            sl : "int64[:]"):
 
-    #$ omp parallel default(private) shared(Mt,M) firstprivate(n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
+    # -- removed omp: #$ omp parallel default(private) shared(Mt,M) firstprivate(n,nc,gp,p,dm,cm,nd,ndT,si,sk,sl)
     d1 = gp[0] - p[0]
     d2 = gp[1] - p[1]
     d3 = gp[2] - p[2]
@@ -238,7 +238,7 @@ def interface_transpose_3d(M  : "T[:,:,:,:,:,:]",
     e2 = nd[1] - sl[1]
     e3 = nd[2] - sl[2]
 
-    #$ omp for schedule(static) collapse(3)
+    # -- removed omp: #$ omp for schedule(static) collapse(3)
     for x1 in range(n[0]):
         for x2 in range(n[1]):
             for x3 in range(n[2]):
@@ -263,5 +263,5 @@ def interface_transpose_3d(M  : "T[:,:,:,:,:,:]",
                               and l1<e1 and l2<e2 and l3<e3 and i1<nc[0] and i2<nc[1] and i3<nc[2]:
                                 Mt[j1,j2,j3, l1+sl[0],l2+sl[1],l3+sl[2]] = M[i1,i2,i3, k1,k2,k3]
 
-    #$ omp end parallel
+    # -- removed omp: #$ omp end parallel
     return
