a16e5989c678f18fb4441fbc33e275cdaf78e7d2
Yaman Güçlü <yaman.guclu@gmail.com>
2025-08-14 15:38:15 +0200

    Reactivate all unit tests in test_3d_poisson.py

--- psydac/api/tests/test_3d_poisson.py
+++ psydac/api/tests/test_3d_poisson.py
@@ -17,19 +17,23 @@ from sympde.expr     import find, EssentialBC
 from psydac.api.discretization import discretize
 
 #==============================================================================
-def run_poisson_3d_dir(solution, f, ncells, degree, comm=None):
+def run_poisson_3d_dir(solution, f, *, ncells, degree, comm=None, backend=None,
+                       timing=None):
+
+    # The dictionary for the timings is modified in-place
+    profile = (timing is not None)
+    if profile:
+        assert isinstance(timing, dict)
 
     # ... abstract model
     domain = Cube()
 
     V = ScalarFunctionSpace('V', domain)
 
-    x,y,z = domain.coordinates
-
     v = element_of(V, name='v')
     u = element_of(V, name='u')
 
-    int_0 = lambda expr: integral(domain , expr)
+    int_0 = lambda expr: integral(domain, expr)
 
     expr = dot(grad(v), grad(u))
     a = BilinearForm((v,u), int_0(expr))
@@ -42,7 +46,7 @@ def run_poisson_3d_dir(solution, f, ncells, degree, comm=None):
     h1norm = SemiNorm(error, domain, kind='h1')
 
     bc = EssentialBC(u, 0, domain.boundary)
-    equation = find(u, forall=v, lhs=a(u,v), rhs=l(v), bc=bc)
+    equation = find(u, forall=v, lhs=a(u, v), rhs=l(v), bc=bc)
     # ...
 
     # ... create the computational domain from a topological domain
@@ -54,12 +58,12 @@ def run_poisson_3d_dir(solution, f, ncells, degree, comm=None):
     # ...
 
     # ... dsicretize the equation using Dirichlet bc
-    equation_h = discretize(equation, domain_h, [Vh, Vh])
+    equation_h = discretize(equation, domain_h, [Vh, Vh], backend=backend)
     # ...
 
     # ... discretize norms
-    l2norm_h = discretize(l2norm, domain_h, Vh)
-    h1norm_h = discretize(h1norm, domain_h, Vh)
+    l2norm_h = discretize(l2norm, domain_h, Vh, backend=backend)
+    h1norm_h = discretize(h1norm, domain_h, Vh, backend=backend)
     # ...
 
     # ... solve the discrete equation
@@ -67,8 +71,8 @@ def run_poisson_3d_dir(solution, f, ncells, degree, comm=None):
     # ...
 
     # ... compute norms
-    l2_error = l2norm_h.assemble(u = uh)
-    h1_error = h1norm_h.assemble(u = uh)
+    l2_error = l2norm_h.assemble(u=uh)
+    h1_error = h1norm_h.assemble(u=uh)
     # ...
 
     return l2_error, h1_error
@@ -90,18 +94,16 @@ def run_poisson_3d_dirneu(solution, f, boundary, ncells, degree, comm=None):
     else:
         B_neumann = Union(*B_neumann)
 
-    x,y,z = domain.coordinates
-
     v = element_of(V, name='v')
     u = element_of(V, name='u')
 
     nn = NormalVector('nn')
 
-    int_0 = lambda expr: integral(domain , expr)
-    int_1 = lambda expr: integral(B_neumann , expr)
+    int_0 = lambda expr: integral(domain, expr)
+    int_1 = lambda expr: integral(B_neumann, expr)
 
     expr = dot(grad(v), grad(u))
-    a = BilinearForm((v,u), int_0(expr))
+    a = BilinearForm((v, u), int_0(expr))
 
     expr = f*v
     l0 = LinearForm(v, int_0(expr))
@@ -119,7 +121,7 @@ def run_poisson_3d_dirneu(solution, f, boundary, ncells, degree, comm=None):
     B_dirichlet = domain.boundary.complement(B_neumann)
     bc = EssentialBC(u, 0, B_dirichlet)
 
-    equation = find(u, forall=v, lhs=a(u,v), rhs=l(v), bc=bc)
+    equation = find(u, forall=v, lhs=a(u, v), rhs=l(v), bc=bc)
     # ...
 
     # ... create the computational domain from a topological domain
@@ -144,18 +146,16 @@ def run_poisson_3d_dirneu(solution, f, boundary, ncells, degree, comm=None):
     # ...
 
     # ... compute norms
-    l2_error = l2norm_h.assemble(u = uh)
-    h1_error = h1norm_h.assemble(u = uh)
+    l2_error = l2norm_h.assemble(u=uh)
+    h1_error = h1norm_h.assemble(u=uh)
     # ...
 
     return l2_error, h1_error
-
 
 ###############################################################################
 #            SERIAL TESTS
 ###############################################################################
 
-#==============================================================================
 def test_api_poisson_3d_dir_1():
 
     from sympy import symbols
@@ -165,7 +165,8 @@ def test_api_poisson_3d_dir_1():
     f        = 3*pi**2*sin(pi*x1)*sin(pi*x2)*sin(pi*x3)
 
     l2_error, h1_error = run_poisson_3d_dir(solution, f,
-                                            ncells=[2**2,2**2,2**2], degree=[2,2,2])
+                                            ncells=[2**2, 2**2, 2**2],
+                                            degree=[2, 2, 2])
 
     expected_l2_error =  0.0017546148822053188
     expected_h1_error =  0.048189500102744275
@@ -173,22 +174,25 @@ def test_api_poisson_3d_dir_1():
     assert( abs(l2_error - expected_l2_error) < 1.e-7)
     assert( abs(h1_error - expected_h1_error) < 1.e-7)
 
-##==============================================================================
-#def test_api_poisson_3d_dirneu_1():
-#
-#    from sympy.abc import x,y,z
-#
-#    solution = sin(0.5*pi*x)*sin(pi*y)*sin(pi*z)
-#    f        = (9./4.)*pi**2*solution
-#
-#    l2_error, h1_error = run_poisson_3d_dirneu(solution, f, [{'axis': 0, 'ext': -1}],
-#                                               ncells=[2**2,2**2,2**2], degree=[2,2,2])
-#
-#    expected_l2_error =  0.0014388350122198999
-#    expected_h1_error =  0.03929404299152041
-#
-#    assert( abs(l2_error - expected_l2_error) < 1.e-7)
-#    assert( abs(h1_error - expected_h1_error) < 1.e-7)
+#==============================================================================
+def test_api_poisson_3d_dirneu_1():
+
+    from sympy import symbols
+    x1, x2, x3 = symbols('x1, x2, x3', real=True)
+
+    solution = cos(0.5*pi*x1)*sin(pi*x2)*sin(pi*x3)
+    f        = (9./4.)*pi**2*solution
+
+    l2_error, h1_error = run_poisson_3d_dirneu(solution, f,
+                                               [{'axis': 0, 'ext': -1}],
+                                               ncells=[2**2, 2**2, 2**2],
+                                               degree=[2, 2, 2])
+
+    expected_l2_error =  0.0014388350122198999
+    expected_h1_error =  0.03929404299152041
+
+    assert( abs(l2_error - expected_l2_error) < 1.e-7)
+    assert( abs(h1_error - expected_h1_error) < 1.e-7)
 
 #==============================================================================
 def test_api_poisson_3d_dirneu_2():
@@ -199,8 +203,10 @@ def test_api_poisson_3d_dirneu_2():
     solution = sin(0.5*pi*x1)*sin(pi*x2)*sin(pi*x3)
     f        = (9./4.)*pi**2*solution
 
-    l2_error, h1_error = run_poisson_3d_dirneu(solution, f, [{'axis': 0, 'ext': 1}],
-                                               ncells=[2**2,2**2,2**2], degree=[2,2,2])
+    l2_error, h1_error = run_poisson_3d_dirneu(solution, f,
+                                               [{'axis': 0, 'ext': 1}],
+                                               ncells=[2**2, 2**2, 2**2],
+                                               degree=[2, 2, 2])
 
     expected_l2_error =  0.0014388350122198999
     expected_h1_error =  0.03929404299152041
@@ -220,7 +226,8 @@ def test_api_poisson_3d_dirneu_13():
     l2_error, h1_error = run_poisson_3d_dirneu(solution, f,
                                                [{'axis': 0, 'ext': -1},
                                                 {'axis': 1, 'ext': -1}],
-                                               ncells=[2**2,2**2,2**2], degree=[2,2,2])
+                                               ncells=[2**2, 2**2, 2**2],
+                                               degree=[2, 2, 2])
 
     expected_l2_error =  0.0010275451113309177
     expected_h1_error =  0.027938446826372313
@@ -240,7 +247,8 @@ def test_api_poisson_3d_dirneu_24():
     l2_error, h1_error = run_poisson_3d_dirneu(solution, f,
                                                [{'axis': 0, 'ext': 1},
                                                 {'axis': 1, 'ext': 1}],
-                                               ncells=[2**2,2**2,2**2], degree=[2,2,2])
+                                               ncells=[2**2, 2**2, 2**2],
+                                               degree=[2, 2, 2])
 
     expected_l2_error =  0.0010275451113330345
     expected_h1_error =  0.027938446826372445
@@ -248,54 +256,55 @@ def test_api_poisson_3d_dirneu_24():
     assert( abs(l2_error - expected_l2_error) < 1.e-7)
     assert( abs(h1_error - expected_h1_error) < 1.e-7)
 
-##==============================================================================
-## TODO DEBUG, not working since merge with devel
-#def test_api_poisson_3d_dirneu_123():
-#
-#    from sympy.abc import x,y,z
-#
-#    solution = cos(0.25*pi*x)*cos(0.5*pi*y)*sin(pi*z)
-#    f        = (21./16.)*pi**2*solution
-#
-#    l2_error, h1_error = run_poisson_3d_dirneu(solution, f,
-#                                               [{'axis': 0, 'ext': -1},
-#                                                {'axis': 0, 'ext': 1},
-#                                                {'axis': 1, 'ext': -1}],
-#                                               ncells=[2**2,2**2,2**2], degree=[2,2,2])
-#
-#    expected_l2_error =  0.0013124098938818625
-#    expected_h1_error =  0.035441679549891296
-#
-#    assert( abs(l2_error - expected_l2_error) < 1.e-7)
-#    assert( abs(h1_error - expected_h1_error) < 1.e-7)
+#==============================================================================
+def test_api_poisson_3d_dirneu_123():
 
-##==============================================================================
-## TODO DEBUG, not working since merge with devel
-#def test_api_poisson_3d_dirneu_1235():
-#
-#    from sympy.abc import x,y,z
-#
-#    solution = cos(0.25*pi*x)*cos(0.5*pi*y)*cos(0.5*pi*z)
-#    f        = (9./16.)*pi**2*solution
-#
-#    l2_error, h1_error = run_poisson_3d_dirneu(solution, f,
-#                                               [{'axis': 0, 'ext': -1},
-#                                                {'axis': 0, 'ext': 1},
-#                                                {'axis': 1, 'ext': -1},
-#                                                {'axis': 2, 'ext': -1}],
-#                                               ncells=[2**2,2**2,2**2], degree=[2,2,2])
-#
-#    expected_l2_error =  0.00019677816055503394
-#    expected_h1_error =  0.0058786142515821265
-#
-#    assert( abs(l2_error - expected_l2_error) < 1.e-7)
-#    assert( abs(h1_error - expected_h1_error) < 1.e-7)
+    from sympy import symbols
+    x1, x2, x3 = symbols('x1, x2, x3', real=True)
+
+    solution = cos(0.25*pi*x1)*cos(0.5*pi*x2)*sin(pi*x3)
+    f        = (21./16.)*pi**2*solution
+
+    l2_error, h1_error = run_poisson_3d_dirneu(solution, f,
+                                               [{'axis': 0, 'ext': -1},
+                                                {'axis': 0, 'ext': 1},
+                                                {'axis': 1, 'ext': -1}],
+                                               ncells=[2**2, 2**2, 2**2],
+                                               degree=[2, 2, 2])
+
+    expected_l2_error =  0.0013124098938818625
+    expected_h1_error =  0.035441679549891296
+
+    assert( abs(l2_error - expected_l2_error) < 1.e-7)
+    assert( abs(h1_error - expected_h1_error) < 1.e-7)
+
+#==============================================================================
+def test_api_poisson_3d_dirneu_1235():
+
+    from sympy import symbols
+    x1, x2, x3 = symbols('x1, x2, x3', real=True)
+
+    solution = cos(0.25*pi*x1)*cos(0.5*pi*x2)*cos(0.5*pi*x3)
+    f        = (9./16.)*pi**2*solution
+
+    l2_error, h1_error = run_poisson_3d_dirneu(solution, f,
+                                               [{'axis': 0, 'ext': -1},
+                                                {'axis': 0, 'ext': 1},
+                                                {'axis': 1, 'ext': -1},
+                                                {'axis': 2, 'ext': -1}],
+                                               ncells=[2**2, 2**2, 2**2],
+                                               degree=[2, 2, 2])
+
+    expected_l2_error =  0.00019677816055503394
+    expected_h1_error =  0.0058786142515821265
+
+    assert( abs(l2_error - expected_l2_error) < 1.e-7)
+    assert( abs(h1_error - expected_h1_error) < 1.e-7)
 
 ###############################################################################
 #            PARALLEL TESTS
 ###############################################################################
 
-#==============================================================================
 @pytest.mark.parallel
 def test_api_poisson_3d_dir_1_parallel():
 
@@ -306,7 +315,8 @@ def test_api_poisson_3d_dir_1_parallel():
     f        = 3*pi**2*sin(pi*x1)*sin(pi*x2)*sin(pi*x3)
 
     l2_error, h1_error = run_poisson_3d_dir(solution, f,
-                                            ncells=[2**2,2**2,2**2], degree=[2,2,2],
+                                            ncells=[2**2, 2**2, 2**2],
+                                            degree=[2, 2, 2],
                                             comm=MPI.COMM_WORLD)
 
     expected_l2_error =  0.0017546148822053188
@@ -314,7 +324,6 @@ def test_api_poisson_3d_dir_1_parallel():
 
     assert( abs(l2_error - expected_l2_error) < 1.e-7)
     assert( abs(h1_error - expected_h1_error) < 1.e-7)
-
 
 #==============================================================================
 # CLEAN UP SYMPY NAMESPACE
