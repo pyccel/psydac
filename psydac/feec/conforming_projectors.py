2cf7a3b83c9215f655f34026636b49456a915534
Frederik Schnack <frederik.schnack@ipp.mpg.de>
2025-11-05 12:55:11 +0100

    add docstrings to conforming projections, change DiscreteDeRhamMultipatch to MultipatchDiscreteDeRham

--- psydac/feec/conforming_projectors.py
+++ psydac/feec/conforming_projectors.py
@@ -1211,6 +1211,10 @@ def construct_h1_singlepatch_conforming_projection(Vh, reg_orders=0, p_moments=-
 
 
     def get_vertex_index(coords):
+        """
+            Calculate the global index of the vertex basis function
+            from the geometric coordinates of a vertex in the domain.
+        """
         nbasis0 = Vh.spaces[0].nbasis - 1
         nbasis1 = Vh.spaces[1].nbasis - 1
 
@@ -1223,6 +1227,11 @@ def construct_h1_singlepatch_conforming_projection(Vh, reg_orders=0, p_moments=-
         return l2g.get_index(0, 0, multi_index)
 
     def vertex_moment_indices(axis, coords, p_moments):
+        """
+            Calculate the global indices of the basis functions
+            adjacent to the vertex basis function along axis
+            from the geometric coordinates of a vertex in the domain.
+        """
         if coords[axis] == 0:
             return range(1, p_moments + 2)
         else:
@@ -1441,9 +1450,13 @@ class ConformingProjectionV0(FemLinearOperator):
     ----------
     V0h: <FemSpace>
      The discrete space
+    
+    mom_pres: <bool>
+        If True, preserve polynomial moments of maximal order in the projection.
 
     p_moments: <int>
         Number of polynomial moments to be preserved in the projection.
+        (Gets overwritten if the parameter mom_pres equals True)
 
     hom_bc : <bool>
      Apply homogenous boundary conditions if True
@@ -1482,8 +1495,12 @@ class ConformingProjectionV1(FemLinearOperator):
     V1h: <FemSpace>
      The discrete space
 
+    mom_pres: <bool>
+        If True, preserve polynomial moments of maximal order in the projection.
+
     p_moments: <int>
         Number of polynomial moments to be preserved in the projection.
+        (Gets overwritten if the parameter mom_pres equals True)
 
     hom_bc : <bool>
      Apply homogenous boundary conditions if True
