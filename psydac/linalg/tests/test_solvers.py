1e6a114f72719256bbbdf478d771ed2c51ab992f
Julian Owezarek <julian.owezarek@tum.de>
2025-11-21 18:25:02 +0100

    Add Dirichlet Boundary Projectors (#536)
    We implement the class `DirichletProjector` and
    `MultipatchDirichletProjector` and include tests.
    
    DirichletProjector
    ---------------------------
    is a subclass of `LinearOperator`. Projects coefficients of functions
    belonging to a `fem_space` of `space_kind` $\in$ `{"h1", "hcurl",
    "hdiv", "l2"}` to coefficients of functions satisfying the corresponding
    homogeneous Dirichlet boundary conditions. These projectors **respect
    periodicity**, i.e., coefficients corresponding to "periodic boundaries"
    **will not be changed**.
    
    Take for example the constant function $f(x, y) = 1$ on an Annulus.
    ```python
    P0, _, _ = derham_h.projectors()
    f        = lambda x, y : 1
    F        = P0(f)
    plot_field_2d(fem_field=F, domain=domain, N_vis=100, cmap='plasma')
    ```
    See [image](https://github.com/user-attachments/assets/83dc42c6-5ecc-46ae-a0a0-6ceae85526fe).
    
    Applying the `DirichletProjector` sets correctly only the DOFs to 0 that
    belong to basis functions that are different from 0 on the
    "x1-direction-boundary".
    
    ```python
    DP0, _ = derham_h.dirichlet_projectors(kind='linop')
    F_dp   = FemField(derham_h.V0, DP0 @ F.coeffs)
    plot_field_2d(fem_field=F_dp, domain=domain, N_vis=100, cmap='plasma')
    ```
    See [image](https://github.com/user-attachments/assets/a117c5c6-8379-4bcf-b8a8-71e59d2d0eeb).
    
    Usage
    ---------
    Obtain these projectors in the **FEEC context** directly from a
    `DiscreteDeRham` object
    ```python
    DP0, DP1 = derham_h.dirichlet_projectors(kind='linop') # in 1D
    DP0, DP1, DP2 = derham_h.dirichlet_projectors(kind='linop') # in 2D
    DP0, DP1, DP2, DP3 = derham_h.dirichlet_projectors(kind='linop') # in 3D
    ```
    The last projector is simply an `IdentityOperator`.
    
    When instead working with **FEM** objects only: 
    ```python
    from psydac.fem.projectors import DirichletProjector
    
    # working with a space that has a kind
    V   = ScalarFunctionSpace('V', domain, kind='h1')
    Vh  = discretize(V, domain_h, degree=degree)
    DP  = DirichletProjector(Vh)
    
    # working with a space that has no kind
    V   = VectorFunctionSpace('V', domain)
    Vh  = discretize(V, domain_h, degree=degree)
    DP  = DirichletProjector(Vh, kind='hcurl') # or 'hdiv'
    ```
    
    Similarity with `conforming_projectors`
    --------------------------------------------------------
    Identical to `derham_h.conforming_projectors(kind='linop', hom_bc=True)`
    **on a single-patch**.
    On **multi-patch** domains, `derham_h.conforming_projectors` also affect
    the solution at the **interfaces** between patches, as required by the
    broken-FEEC theory. If required, they have the additional ability to
    preserve moments of the solution.
    Unfortunately, the conforming projectors are only implemented in 2D.
    
    ---------
    
    Co-authored-by: Frederik Schnack <frederik.schnack@ipp.mpg.de>

--- psydac/linalg/tests/test_solvers.py
+++ psydac/linalg/tests/test_solvers.py
@@ -1,11 +1,9 @@
+import  numpy as np
+import  pytest
 
-import numpy as np
-import pytest
-from psydac.linalg.solvers import inverse
-from psydac.linalg.stencil import StencilVectorSpace, StencilMatrix, StencilVector
-from psydac.linalg.basic import LinearSolver
-from psydac.ddm.cart import DomainDecomposition, CartDecomposition
-
+from    psydac.ddm.cart             import DomainDecomposition, CartDecomposition
+from    psydac.linalg.solvers       import inverse
+from    psydac.linalg.stencil       import StencilVectorSpace, StencilMatrix, StencilVector
 
 def define_data_hermitian(n, p, dtype=float):
     domain_decomposition = DomainDecomposition([n - p], [False])
@@ -54,9 +52,8 @@ def define_data(n, p, matrix_data, dtype=float):
     xe[s:e + 1] = np.random.random(e + 1 - s)
     return(V, A, xe)
 
-
 #===============================================================================
-@pytest.mark.parametrize( 'n', [5, 10, 13] )
+@pytest.mark.parametrize('n', [5, 10, 13] )
 @pytest.mark.parametrize('p', [2, 3])
 @pytest.mark.parametrize('dtype', [float, complex])
 @pytest.mark.parametrize('solver', ['cg', 'pcg', 'bicg', 'bicgstab', 'pbicgstab', 'minres', 'lsmr', 'gmres'])
@@ -203,6 +200,7 @@ def test_solver_tridiagonal(n, p, dtype, solver, verbose=False):
         assert errt_norm < tol
         assert errh_norm < tol
         assert solver == 'pcg' or errc_norm < tol
+
 
 # ===============================================================================
 # SCRIPT FUNCTIONALITY
