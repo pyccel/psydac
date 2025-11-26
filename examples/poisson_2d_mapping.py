a2458a7cc8456d4ae76c049bf18579d0c46345bb
Yaman Güçlü <yaman.guclu@ipp.mpg.de>
2025-11-26 13:39:22 +0100

    Remove useless header line from poisson_2d_mapping.py

--- examples/poisson_2d_mapping.py
+++ examples/poisson_2d_mapping.py
@@ -3,7 +3,6 @@
 # LICENSE file or go to https://github.com/pyccel/psydac/blob/devel/LICENSE #
 # for full license details.                                                 #
 #---------------------------------------------------------------------------#
-# coding: utf-8
 from time import time, sleep
 
 from   mpi4py import MPI
