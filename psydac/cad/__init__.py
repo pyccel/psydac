d5d829433722608fcd5a38eb697bbc4d0f534db5
Yaman Güçlü <yaman.guclu@gmail.com>
2025-11-27 11:08:48 +0100

    Remove old header line from psydac/cad/__init__.py
    Co-authored-by: Frederik Schnack <frederik.schnack@tum.de>

--- psydac/cad/__init__.py
+++ psydac/cad/__init__.py
@@ -3,7 +3,6 @@
 # LICENSE file or go to https://github.com/pyccel/psydac/blob/devel/LICENSE #
 # for full license details.                                                 #
 #---------------------------------------------------------------------------#
-# coding = utf-8
 
 __all__ = ['geometry']
 
