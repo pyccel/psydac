6c7c072e03e1e54fd3bd2a1e86146d2c122c35a8
Alisa Kirkinskaia <alisa.kirkinskaia@tum.de>
2026-01-20 13:54:35 +0100

    fix a bug in test

--- psydac/feec/polar/tests/test_conga_projections.py
+++ psydac/feec/polar/tests/test_conga_projections.py
@@ -4,6 +4,8 @@ from numpy import pi
 import pytest
 from sympde import ScalarFunctionSpace
 
+from mpi4py import MPI
+
 from sympde.topology import Square
 from sympde.topology.analytical_mapping import PolarMapping
 
@@ -13,12 +15,12 @@ from psydac.fem.basic import FemField
 
 
 @pytest.mark.parametrize( 'R', [1])
-@pytest.mark.parametrize( 'ncells', [[2, 4]])
-@pytest.mark.parametrize( 'degree', [[1, 1]])
+@pytest.mark.parametrize( 'ncells', [[4, 8], [12, 12]])
+@pytest.mark.parametrize( 'degree', [[1, 1], [2, 2]])
 @pytest.mark.parallel
 
 def test_C0PolarProjection_V0(R, ncells, degree):
-    print('Testing C0PolarProjection_V0...')
+    mpi_comm = MPI.COMM_WORLD
 
     # Build physical domain - disk of radius R
     logical_domain = Square('Omega', bounds1=[0, R], bounds2=[0, 2 * pi])
@@ -26,7 +28,7 @@ def test_C0PolarProjection_V0(R, ncells, degree):
     domain = mapping(logical_domain)
 
     # Discrete physical domain and discrete space
-    domain_h = discretize(domain, ncells=ncells, periodic=[False, True], comm=None)
+    domain_h = discretize(domain, ncells=ncells, periodic=[False, True], comm=mpi_comm)
     V0 = ScalarFunctionSpace('V0', domain)
     V0_h = discretize(V0, domain_h, degree=degree)
 
@@ -50,5 +52,8 @@ def test_C0PolarProjection_V0(R, ncells, degree):
     # Comparing results of dot and tosparse
     sp_P0 = P0.tosparse()
     y_sp = sp_P0 @ x.toarray()
-    print(np.allclose(y_sp, y.toarray()))
-    assert np.allclose(y_sp, y.toarray())
+    y = mpi_comm.allreduce(y.toarray(), op=MPI.SUM)
+    y_sp = mpi_comm.allreduce(y_sp, op=MPI.SUM)
+
+    print(np.allclose(y_sp, y))
+    assert np.allclose(y_sp, y)
