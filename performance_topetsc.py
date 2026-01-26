aeb99106f74467848c5638a4ba745f15fdbb2254
elmosa <Elena.Moral.Sanchez@ipp.mpg.de>
2026-01-26 16:25:58 +0100

    add script for perfomance topetsc

---
+++ performance_topetsc.py
@@ -0,0 +1,38 @@
+
+ncells_list = [((2**k)*10, (2**k)*10, (2**k)*10) for k in range(2,7)] 
+
+for ncells in ncells_list:
+
+    from mpi4py                         import MPI
+    from psydac.api.discretization      import discretize
+    from psydac.api.settings            import PSYDAC_BACKENDS
+    from sympde.topology                import Cube, element_of, Derham
+    from sympde.expr                    import BilinearForm, integral
+    
+    per = [False, False, False]
+    deg = [3,3,3]
+    mult = [1,1,1]
+    comm = MPI.COMM_WORLD
+
+    D = Cube('D')
+    derham = Derham(D)
+
+    Dh = discretize(D, ncells=ncells, periodic=per, comm=comm)
+    derham_h = discretize(derham, Dh, degree=deg, multiplicity=mult)
+    V0h = derham_h.V0 #TensorFemSpace
+    V1h = derham_h.V1 #VectorFemSpace
+
+    u = element_of(derham.V0, name='u') # trial function
+    w = element_of(derham.V0, name='w') # test function
+
+    be=PSYDAC_BACKENDS['pyccel-gcc']
+
+
+    a = BilinearForm((u,w), integral(D, u*w))
+    ah = discretize(a, Dh, [derham_h.V0, derham_h.V0], backend=be)
+    M = ah.assemble(sum_factorization = True)
+    M.remove_spurious_entries()
+    M.update_ghost_regions()
+
+    Mp = M.topetsc('performance_petsc')
+
