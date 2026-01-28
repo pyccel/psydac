ef309760c3e696fb40cc246b77256cce8b26bd57
elmosa <Elena.Moral.Sanchez@ipp.mpg.de>
2026-01-28 18:32:00 +0100

    add performance timings and plot utility

--- plot_performance_topetsc.py
+++ plot_performance_topetsc.py
@@ -7,52 +7,27 @@ nprocs_list = [1,4,8,16,32]
 nrows_list = [1728, 10648, 74088, 551368, 4251528]
 ncells_list = [((2**k)*10, (2**k)*10, (2**k)*10) for k in range(0,5)] #tok cluster only k=0,1,2,3,4, MAX 185GB per node
 
-time_kernel = np.zeros((len(nprocs_list), len(nrows_list)))
-time_setValuesIJV = np.zeros((len(nprocs_list), len(nrows_list)))
-time_assemble = np.zeros((len(nprocs_list), len(nrows_list)))
+time_kernel = np.empty((len(nprocs_list), len(nrows_list), nprocs_list[-1]), dtype=object)
+time_setValuesIJV = np.empty((len(nprocs_list), len(nrows_list), nprocs_list[-1]), dtype=object)
+time_assemble = np.empty((len(nprocs_list), len(nrows_list), nprocs_list[-1]), dtype=object)
 
 for i in range(len(nprocs_list)):
     for j in range(len(nrows_list)):
+        for k in range(nprocs_list[i]):
 
-        data = np.load(f'performance_petsc_serial/petsc_performance_nprocs={nprocs_list[i]}_nrows={nrows_list[j]}.npz', allow_pickle=True)
+            data = np.load(f'performance_petsc/petsc_performance_proc={k}_of_{nprocs_list[i]}_nrows={nrows_list[j]}.npz', allow_pickle=True)
         
-        time_kernel[i,j] = data['time_kernel']
-        time_setValuesIJV[i,j] = data['time_setValuesIJV']
-        time_assemble[i,j] = data['time_assemble']
+            time_kernel[i,j,k] = data['time_kernel'].item()
+            time_setValuesIJV[i,j,k] = data['time_setValuesIJV'].item()
+            time_assemble[i,j,k] = data['time_assemble'].item()
 
-time_kernel = np.array([
-    [3.44014168e-03, 1.86841488e-02, 1.36070251e-01, 1.05291104e+00, 8.26110077e+00],
-    [1.80935860e-03, 6.01798296e-03, 3.86480689e-02, 2.93982983e-01, 2.30626690e+00],
-    [5.11139631e-04, 2.96166539e-03, 2.09342837e-02, 1.54421955e-01, 1.20589867e+00],
-    [6.44862652e-04, 1.44958496e-03, 1.16356909e-02, 8.47364962e-02, 6.64496660e-01],
-    [2.72303820e-04, 7.27772713e-04, 7.04875588e-03, 5.09125888e-02, 4.01131377e-01]
-])
 
-time_setValuesIJV = np.array([
-    [6.64901733e-02, 4.99193907e-01, 4.21932697e+00, 3.56401129e+01, 3.17807595e+02],
-    [2.22587585e-02, 1.25872850e-01, 1.06521857e+00, 8.99232519e+00, 7.71693090e+01],
-    [8.34125280e-03, 6.30261004e-02, 5.33503979e-01, 4.47380212e+00, 3.90298276e+01],
-    [4.84825671e-03, 3.05047482e-02, 2.65323520e-01, 2.29217426e+00, 1.95350892e+01],
-    [2.24738568e-03, 1.51470453e-02, 1.28675453e-01, 1.18095405e+00, 1.00693423e+01]
-])
-
-time_assemble = np.array([
-    [2.02119350e-02, 1.47285938e-01, 1.17021513e+00, 9.42194057e+00, 9.30189772e+01],
-    [2.15317011e-02, 4.92064357e-02, 3.36610734e-01, 2.56101841e+00, 2.39285755e+01],
-    [7.41338730e-03, 2.86393166e-02, 1.81091219e-01, 1.38666886e+00, 1.42942967e+01],
-    [2.26006955e-02, 1.77826732e-02, 1.08603835e-01, 8.27372923e-01, 6.90622087e+00],
-    [6.20394945e-03, 1.05539784e-02, 6.29525781e-02, 4.60187472e-01, 4.19238978e+00]
-])
-
-time_kernel_total = np.array([time_kernel[i,:]*nprocs_list[i] for i in range(len(nprocs_list))])
-time_setValuesIJV_total = np.array([time_setValuesIJV[i,:]*nprocs_list[i] for i in range(len(nprocs_list))])
-time_assemble_total = np.array([time_assemble[i,:]*nprocs_list[i] for i in range(len(nprocs_list))])
-
+"""
 # Plotting Configuration
 datasets = [
-    (time_kernel_total, "Kernel time"),
-    (time_setValuesIJV_total, "SetValuesIJV time"),
-    (time_assemble_total, "Assembly time")
+    (time_kernel, "Kernel time"),
+    (time_setValuesIJV, "SetValuesIJV time"),
+    (time_assemble, "Assembly time")
 ]
 # Change global font size
 plt.rcParams.update({'font.size': 11})  # Set the default font size
@@ -64,7 +39,9 @@ for i, (data, title) in enumerate(datasets):
     ax = axes[i]
 
     for j, nproc in enumerate(nprocs_list):
-        ax.plot(nrows_list, data[j,:], label=f'#procs={nproc}', marker='^', markerfacecolor='none', markersize=10)
+        ax.plot(nrows_list, np.sum(data[j,:,:nproc], axis=-1), label=f'#procs={nproc}', marker='^', markerfacecolor='none', markersize=10)
+        #for k in range(nproc):
+        #    ax.plot(nrows_list, data[j,:,k])#, label=f'#procs={nproc}', marker='^', markerfacecolor='none', markersize=10)
     
     ax.set_xscale('log')
     ax.set_yscale('log')
@@ -80,8 +57,9 @@ plt.tight_layout()
 plt.show()
 
 
-
-
+time_kernel_sum = np.max(np.where(time_kernel == None, 0., time_kernel), axis=-1) #np.sum(np.where(time_kernel == None, 0., time_kernel), axis=-1)
+time_setValuesIJV_sum = np.max(np.where(time_kernel == None, 0., time_kernel), axis=-1) #np.sum(np.where(time_setValuesIJV == None, 0., time_setValuesIJV), axis=-1)
+time_assemble_sum = np.max(np.where(time_kernel == None, 0., time_kernel), axis=-1) #np.sum(np.where(time_assemble == None, 0., time_assemble), axis=-1)
 
 
 
@@ -93,11 +71,13 @@ for i in range(len(nrows_list)):
     # --- 1. Strong Scaling Plot ---
     # speedup (T_ref / T_p) for the largest problem size (last column)
 
-    speedup_kernel = time_kernel[0,i]*nprocs_list[0] / (time_kernel[:,i]*nprocs_list[i])
+    speedup_kernel = time_kernel_sum[0,i] / time_kernel_sum[:,i]
     axes[0].plot(nprocs_list, speedup_kernel, label='kernel', color='b', marker='^', markerfacecolor='none', markersize=10)
-    speedup_setValuesIJV = time_setValuesIJV[0,i]*nprocs_list[0] / (time_setValuesIJV[:,i]*nprocs_list[i])
+
+    speedup_setValuesIJV = time_setValuesIJV_sum[0,i] / time_setValuesIJV_sum[:,i]
     axes[0].plot(nprocs_list, speedup_setValuesIJV, label='setValuesIJV', color='g', marker='^', markerfacecolor='none', markersize=10)
-    speedup_assemble = time_assemble[0,i]*nprocs_list[0] / (time_assemble[:,i]*nprocs_list[i])
+
+    speedup_assemble = time_assemble_sum[0,i] / time_assemble_sum[:,i]
     axes[0].plot(nprocs_list, speedup_assemble, label='assemble', color='r', marker='^', markerfacecolor='none', markersize=10)
 
 
@@ -120,10 +100,17 @@ for i in range(len(nrows_list)):
     # For simplicity, let's plot the time for specific nrows across all procs.
 
 
-    axes[1].plot(nprocs_list, time_kernel[:, i]*nprocs_list[:], label='kernel', color='b', marker='^', markerfacecolor='none', markersize=10)
-    axes[1].plot(nprocs_list, time_setValuesIJV[:, i]*nprocs_list[:], label='setValuesIJV', color='g', marker='^', markerfacecolor='none', markersize=10)
-    axes[1].plot(nprocs_list, time_assemble[:, i]*nprocs_list[:], label='assemble', color='r', marker='^', markerfacecolor='none', markersize=10)
-
+    '''axes[1].plot(nprocs_list, time_kernel_sum[:, i], label='kernel', color='b', marker='^', markerfacecolor='none', markersize=10)
+    axes[1].plot(nprocs_list, time_setValuesIJV_sum[:, i], label='setValuesIJV', color='g', marker='^', markerfacecolor='none', markersize=10)
+    axes[1].plot(nprocs_list, time_assemble_sum[:, i], label='assemble', color='r', marker='^', markerfacecolor='none', markersize=10)'''
+    for k in range(len(nprocs_list)):
+        p = np.arange(0, nprocs_list[k])
+        axes[1].scatter(p, time_kernel[:, i, k])#, label='kernel', color='b', marker='^', markerfacecolor='none', markersize=10)
+    #axes[1].scatter(nprocs_list, time_setValuesIJV_sum[:, i], label='setValuesIJV', color='g', marker='^', markerfacecolor='none', markersize=10)
+    #axes[1].scatter(nprocs_list, time_assemble_sum[:, i], label='assemble', color='r', marker='^', markerfacecolor='none', markersize=10)
+    '''axes[1].boxplot(time_kernel[:, i,:])#, label='kernel', color='b', marker='^', markerfacecolor='none', markersize=10)
+    axes[1].boxplot(time_setValuesIJV[:, i,:])#, label='setValuesIJV', color='g', marker='^', markerfacecolor='none', markersize=10)
+    axes[1].boxplot(time_assemble[:, i,:])#, label='assemble', color='r', marker='^', markerfacecolor='none', markersize=10)'''
 
 
     axes[1].set_title(f"Time vs number of processors (#rows={nrows})")
@@ -143,5 +130,173 @@ for i in range(len(nrows_list)):
     axes[1].tick_params(axis='x', which='minor', length=0)
 
     plt.tight_layout()
-    plt.savefig(f'scaling_nrows={nrows}.png')
+    plt.savefig(f'scaling_nrows={nrows}_scatter.png')
     plt.close()
+"""
+
+#######################################
+
+def plot_time_vs_nrows(nrows_list, nprocs_list, array, title):
+    plt.rcParams.update({'font.size': 11})  
+    fig = plt.figure(figsize=(10, 6))
+    
+    for i, nprocs in enumerate(nprocs_list):
+        x_vals = []
+        y_vals = []
+        
+        for j, nrows in enumerate(nrows_list):
+            # Extract the list of times for this specific nprocs and nrows
+            process_times = array[i][j]
+            
+            # Filter out 'None' values and keep only floats
+            valid_times = [t for t in process_times if t is not None]
+            
+            # Append nrows for every valid timing found
+            x_vals.extend([nrows] * len(valid_times))
+            y_vals.extend(valid_times)
+        
+        # Plot each nprocs group as a separate scatter series
+        plt.scatter(x_vals, y_vals, label=f'nprocs={nprocs}', alpha=0.9, s=40)
+
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('number of rows')
+    plt.ylabel('Time [s]')
+    plt.title(f'Time of {title} vs. number of processes')
+    #plt.grid(True, which="both", ls="-", alpha=0.2)
+
+    plt.grid(True, which="major", ls="-", alpha=1)
+    plt.grid(True, which="minor", ls="-", alpha=0.5)
+    
+    plt.xticks(nrows_list, [str(n) for n in nrows_list], minor=False)
+    plt.xticks(nrows_list, ['' for n in nrows_list], minor=True)
+    plt.tick_params(axis='x', which='minor', length=0)
+
+    plt.legend()
+    plt.savefig(f'time_vs_nrows_{title}.pdf')
+
+plot_time_vs_nrows(nrows_list, nprocs_list, time_kernel, 'kernel')
+plot_time_vs_nrows(nrows_list, nprocs_list, time_setValuesIJV, 'setValuesIJV')
+plot_time_vs_nrows(nrows_list, nprocs_list, time_assemble, 'assemble')
+
+def plot_time_vs_nprocs(nrows_list, nprocs_list, array, title):
+    plt.rcParams.update({'font.size': 11}) 
+    plt.figure(figsize=(10, 6))
+    
+    # Iterate through nprocs (first dimension)
+    for i, nprocs in enumerate(nprocs_list):
+        x_vals = []
+        y_vals = []
+        
+        # Iterate through nrows (second dimension)
+        for j, nrows in enumerate(nrows_list):
+            # Iterate through individual process times (third dimension)
+            # We filter out None values here
+            times = [t for t in array[i, j] if t is not None]
+            
+            # Create a list of the current nprocs for every valid time found
+            x_vals.extend([nprocs] * len(times))
+            y_vals.extend(times)
+        
+        # Plot each nprocs group with a label
+        plt.scatter(x_vals, y_vals, alpha=0.9, label=f'nprocs={nprocs}', s=40)
+    plt.title(f'Time of {title} vs. number of rows')
+    plt.xlabel('number of processes')
+    plt.ylabel('Time [s]')
+    plt.yscale('log') # Log scale is recommended due to the wide range in your data
+    plt.legend()
+
+    plt.grid(True, which="major", ls="-", alpha=1)
+    plt.grid(True, which="minor", ls="-", alpha=0.5)
+    
+    plt.xticks(nprocs_list, [str(n) for n in nprocs_list], minor=False)
+    plt.xticks(nprocs_list, ['' for n in nprocs_list], minor=True)
+    plt.tick_params(axis='x', which='minor', length=0)
+
+    plt.savefig(f'time_vs_nprocs_{title}.pdf')
+    plt.close()
+
+
+plot_time_vs_nprocs(nrows_list, nprocs_list, time_kernel, 'kernel')
+plot_time_vs_nprocs(nrows_list, nprocs_list, time_setValuesIJV, 'setValuesIJV')
+plot_time_vs_nprocs(nrows_list, nprocs_list, time_assemble, 'assemble')
+
+def plot_strong_scaling(nrows_list, nprocs_list, array, title):
+    plt.rcParams.update({'font.size': 11})
+    # Data preparation: Filtering None values and calculating statistics
+    # time_kernel[nprocs_idx][nrows_idx][process_idx]
+    data = [] 
+    for p_idx in range(len(nprocs_list)):
+        p_data = []
+        for r_idx in range(len(nrows_list)):
+            # Extract times for active processes only
+            times = [t for t in array[p_idx][r_idx] if t is not None]
+            p_data.append(np.array(times))
+        data.append(p_data)
+
+
+    ## 1. Strong Scaling Plot
+    plt.figure(figsize=(10, 6))
+    for r_idx, nrows in enumerate(nrows_list):
+        # Strong scaling uses the maximum time (bottleneck) among all processes
+        max_times = [np.max(data[p_idx][r_idx]) for p_idx in range(len(nprocs_list))]
+        
+        # Calculate speedup: T(1) / T(N)
+        t1 = max_times[0]
+        speedup = [t1 / tn for tn in max_times]
+        
+        plt.plot(nprocs_list, speedup, marker='^', label=f'#rows={nrows}', markerfacecolor='none', markersize=10)
+
+    # Ideal scaling line
+    plt.plot(nprocs_list, nprocs_list, '--', color='gray', label='ideal speedup')
+
+    plt.xscale('log', base=2)
+    plt.yscale('log', base=2)
+    plt.xticks(nprocs_list, nprocs_list)
+    plt.yticks([2**k for k in range(len(nprocs_list)+1)], [2**k for k in range(len(nprocs_list)+1)])
+    plt.xlabel('number of processes')
+    plt.ylabel('speedup')
+    plt.title(f'Strong scaling (maximum over processes) {title}')
+    plt.legend()
+    plt.grid(True, which="both", ls="-", alpha=0.5)
+    plt.tight_layout()
+    plt.savefig(f'strong_scaling_{title}.pdf')
+    plt.close()
+
+plot_strong_scaling(nrows_list, nprocs_list, time_kernel, 'kernel')
+plot_strong_scaling(nrows_list, nprocs_list, time_setValuesIJV, 'setValuesIJV')
+plot_strong_scaling(nrows_list, nprocs_list, time_assemble, 'assemble')
+
+def plot_load_imbalance(nrows_list, nprocs_list, array, title):
+    data = [] 
+    for p_idx in range(len(nprocs_list)):
+        p_data = []
+        for r_idx in range(len(nrows_list)):
+            # Extract times for active processes only
+            times = [t for t in array[p_idx][r_idx] if t is not None]
+            p_data.append(np.array(times))
+        data.append(p_data)
+    plt.rcParams.update({'font.size': 11})
+    ## 2. Load Imbalance Plot (Boxplots)
+    # We will create a subplot for each nrows_list entry to see imbalance evolution
+    fig, axes = plt.subplots(1, len(nrows_list), figsize=(20, 6), sharey=False)
+
+    for r_idx, nrows in enumerate(nrows_list):
+        # Group data by nprocs for this specific row count
+        plot_data = [data[p_idx][r_idx] for p_idx in range(len(nprocs_list))]
+        
+        axes[r_idx].boxplot(plot_data, labels=nprocs_list)
+        axes[r_idx].set_title(f'#rows={nrows}')
+        axes[r_idx].set_xlabel('#processes')
+        if r_idx == 0:
+            axes[r_idx].set_ylabel('Time [s]')
+        axes[r_idx].grid(axis='y', alpha=0.3)
+
+    plt.suptitle(f'Load imbalance across processes for {title}', fontsize=16)
+    plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.savefig(f'load_imbalance_{title}.pdf')
+    plt.close()
+
+plot_load_imbalance(nrows_list, nprocs_list, time_kernel, 'kernel')
+plot_load_imbalance(nrows_list, nprocs_list, time_setValuesIJV, 'setValuesIJV')
+plot_load_imbalance(nrows_list, nprocs_list, time_assemble, 'assemble')
