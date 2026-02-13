3cd2b73499f7935801102cb88a1325b56de55d16
mcampos IPP <martin.campos-pinto@ipp.mpg.de>
2026-02-13 15:21:07 +0100

    change compilation flags for M4

--- psydac/api/settings.py
+++ psydac/api/settings.py
@@ -20,10 +20,18 @@ PSYDAC_BACKEND_PYTHON = {'name': 'python', 'tag':'python', 'openmp':False}
 
 PSYDAC_BACKEND_GPYCCEL  = {'name': 'pyccel',
                        'compiler_family': 'GNU',
-                       'flags'   : '-O3 -ffast-math',
+                       'flags'   : '-O3 -ffast-math -march=native',
                        'folder'  : '__gpyccel__',
                        'tag'     : 'gpyccel',
                        'openmp'  : False}
+
+# [MCP 10.02.2026] this flag was used previously for intel mac, should we add it ?
+# if not platform.system() == "Darwin":
+#     PSYDAC_BACKEND_GPYCCEL['flags'] += ' -mtune=native'
+
+# [MCP 10.02.2026] YG's suggestion:
+if platform.machine() == 'x86_64':
+    PSYDAC_BACKEND_GPYCCEL['flags'] += ' -mavx'
 
 PSYDAC_BACKEND_IPYCCEL  = {'name': 'pyccel',
                        'compiler_family': 'intel',
@@ -53,26 +61,31 @@ gfortran_version_string = re.search(r"(\d+\.\d+\.\d+)", gfortran_version_output)
 gfortran_version = Version(gfortran_version_string)
 
 # Platform-dependent flags
-if platform.system() == "Darwin" and platform.machine() == 'arm64' and gfortran_version >= Version("14"):
+# if platform.system() == "Darwin" and platform.machine() == 'arm64':
+#     PSYDAC_BACKEND_GPYCCEL['flags'] += ' -march=native'
 
-    # Apple silicon requires architecture-specific flags (see https://github.com/pyccel/psydac/pull/411)
-    # which are only available on GCC version >= 14
-    cpu_brand = subprocess.check_output(['sysctl','-n','machdep.cpu.brand_string']).decode('utf-8').strip() # nosec B603, B607
-    if cpu_brand.startswith("Apple M"):
-        # Example: "Apple M3 Pro (virtual)" --> " -mcpu=apple-m3"
-        cpu_flag = '-'.join(cpu_brand.lower().split()[:2])
-        PSYDAC_BACKEND_GPYCCEL['flags'] += f' -mcpu={cpu_flag}'
-    else:
-        # TODO: Support later Apple CPU models. Perhaps the CPU naming scheme could be easily guessed
-        # based on the output of 'sysctl -n machdep.cpu.brand_string', but I wouldn't rely on this
-        # guess unless it has been manually verified. Loud errors are better than silent failures!
-        raise SystemError(f"Unsupported Apple CPU '{cpu_brand}'.")
+# [MCP] version before PR #569, delete if OK:
+# if platform.system() == "Darwin" and platform.machine() == 'arm64' and gfortran_version >= Version("14"):
+#
+#     # Apple silicon requires architecture-specific flags (see https://github.com/pyccel/psydac/pull/411)
+#     # which are only available on GCC version >= 14
+#     cpu_brand = subprocess.check_output(['sysctl','-n','machdep.cpu.brand_string']).decode('utf-8').strip() # nosec B603, B607
+#     if cpu_brand.startswith("Apple M"):
+#         # Example: "Apple M3 Pro (virtual)" --> " -mcpu=apple-m3"
+#         cpu_flag = '-'.join(cpu_brand.lower().split()[:2])
+#         cpu_flag = cpu_flag.replace('4', '3')
+#         PSYDAC_BACKEND_GPYCCEL['flags'] += f' -mcpu={cpu_flag}'
+#     else:
+#         # TODO: Support later Apple CPU models. Perhaps the CPU naming scheme could be easily guessed
+#         # based on the output of 'sysctl -n machdep.cpu.brand_string', but I wouldn't rely on this
+#         # guess unless it has been manually verified. Loud errors are better than silent failures!
+#         raise SystemError(f"Unsupported Apple CPU '{cpu_brand}'.")
 
-else:
-    # Default architecture flags
-    PSYDAC_BACKEND_GPYCCEL['flags'] += ' -march=native -mtune=native'
-    if platform.machine() == 'x86_64':
-        PSYDAC_BACKEND_GPYCCEL['flags'] += ' -mavx'
+# else:
+#     # Default architecture flags
+#     PSYDAC_BACKEND_GPYCCEL['flags'] += ' -march=native -mtune=native'
+#     if platform.machine() == 'x86_64':
+#         PSYDAC_BACKEND_GPYCCEL['flags'] += ' -mavx'
 
 #==============================================================================
 
