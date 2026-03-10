7bc86549c60cbc6c7fb711727aff787ccb71333d
Yaman Güçlü <yaman.guclu@gmail.com>
2026-03-10 08:54:26 +0100

    Clean up test_2d_multipatch_poisson.py

--- psydac/api/tests/test_2d_multipatch_poisson.py
+++ psydac/api/tests/test_2d_multipatch_poisson.py
@@ -75,11 +75,11 @@ def run_poisson_2d(solution, f, domain, ncells, degree):
 
 #------------------------------------------------------------------------------
 def test_poisson_2d_2_patch_dirichlet_0():
-    A = Square('A',bounds1=(0, 0.5), bounds2=(0, 1))
-    B = Square('B',bounds1=(0.5, 1.), bounds2=(0, 1))
+    A = Square('A', bounds1=(0, 0.5), bounds2=(0, 1))
+    B = Square('B', bounds1=(0.5, 1), bounds2=(0, 1))
 
     connectivity = [((0,0,1), (1,0,-1), 1)]
-    patches = [A,B]
+    patches = [A, B]
     domain = Domain.join(patches, connectivity, 'domain')
 
     x,y = domain.coordinates
@@ -92,16 +92,16 @@ def test_poisson_2d_2_patch_dirichlet_0():
     expected_l2_error = 2.176726763610992e-09
     expected_h1_error = 2.9725703533101877e-09
 
-    assert ( abs(l2_error - expected_l2_error) < 1e-7 )
-    assert ( abs(h1_error - expected_h1_error) < 1e-7 )
+    assert abs(l2_error - expected_l2_error) < 1e-7
+    assert abs(h1_error - expected_h1_error) < 1e-7
 
 #------------------------------------------------------------------------------
 def test_poisson_2d_2_patch_dirichlet_1():
-    A = Square('A',bounds1=(0, 0.5), bounds2=(0, 1))
-    B = Square('B',bounds1=(0.5, 1.), bounds2=(0, 1))
+    A = Square('A', bounds1=(0, 0.5), bounds2=(0, 1))
+    B = Square('B', bounds1=(0.5, 1), bounds2=(0, 1))
 
     connectivity = [((0,0,1), (1,0,-1), 1)]
-    patches = [A,B]
+    patches = [A, B]
     domain = Domain.join(patches, connectivity, 'domain')
 
     x,y = domain.coordinates
@@ -113,16 +113,16 @@ def test_poisson_2d_2_patch_dirichlet_1():
     expected_l2_error = 0.002035229666394183
     expected_h1_error = 0.056796387991647795
 
-    assert ( abs(l2_error - expected_l2_error) < 1e-7 )
-    assert ( abs(h1_error - expected_h1_error) < 1e-7 )
+    assert abs(l2_error - expected_l2_error) < 1e-7
+    assert abs(h1_error - expected_h1_error) < 1e-7
 
 #------------------------------------------------------------------------------
 def test_poisson_2d_2_patch_dirichlet_2():
-    A = Square('A',bounds1=(0, 0.5), bounds2=(0, 1))
-    B = Square('B',bounds1=(0.5, 1.), bounds2=(0, 1))
+    A = Square('A', bounds1=(0, 0.5), bounds2=(0, 1))
+    B = Square('B', bounds1=(0.5, 1), bounds2=(0, 1))
 
     connectivity = [((0,0,1), (1,0,-1), 1)]
-    patches = [A,B]
+    patches = [A, B]
     domain = Domain.join(patches, connectivity, 'domain')
 
     x,y = domain.coordinates
@@ -134,24 +134,24 @@ def test_poisson_2d_2_patch_dirichlet_2():
     expected_l2_error = 0.002035229666394183
     expected_h1_error = 0.056796387991647795
 
-    assert ( abs(l2_error - expected_l2_error) < 1e-7 )
-    assert ( abs(h1_error - expected_h1_error) < 1e-7 )
+    assert abs(l2_error - expected_l2_error) < 1e-7
+    assert abs(h1_error - expected_h1_error) < 1e-7
 
 #------------------------------------------------------------------------------
 def test_poisson_2d_2_patch_dirichlet_3():
-    A = Square('A',bounds1=(0, 0.5), bounds2=(0, 1))
-    B = Square('B',bounds1=(0, 0.5), bounds2=(0, 1))
+    A = Square('A', bounds1=(0, 0.5), bounds2=(0, 1))
+    B = Square('B', bounds1=(0, 0.5), bounds2=(0, 1))
 
-    M1 = IdentityMapping('M1',2)
-    M2 = AffineMapping('M2',2, c1=1, c2=0,
+    M1 = IdentityMapping('M1', 2)
+    M2 = AffineMapping('M2', 2, c1=1, c2=0,
         a11=-1, a12=0,
-        a21=0, a22=1)
+        a21= 0, a22=1)
 
     D1 = M1(A)
     D2 = M2(B)
 
     connectivity = [((0,0,1), (1,0,1), 1)]
-    patches = [D1,D2]
+    patches = [D1, D2]
     domain = Domain.join(patches, connectivity, 'domain')
 
     x,y = domain.coordinates
@@ -163,16 +163,21 @@ def test_poisson_2d_2_patch_dirichlet_3():
     expected_l2_error = 0.0020352296663948295
     expected_h1_error = 0.05679638799164739
 
-    assert ( abs(l2_error - expected_l2_error) < 1e-7 )
-    assert ( abs(h1_error - expected_h1_error) < 1e-7 )
+    assert abs(l2_error - expected_l2_error) < 1e-7
+    assert abs(h1_error - expected_h1_error) < 1e-7
 
 #------------------------------------------------------------------------------
 def test_poisson_2d_2_patch_dirichlet_4():
-    A = Square('A',bounds1=(0, 0.5), bounds2=(0, 1))
-    B = Square('B',bounds1=(0, 0.5), bounds2=(0, 1))
+    A = Square('A', bounds1=(0, 0.5), bounds2=(0, 1))
+    B = Square('B', bounds1=(0, 0.5), bounds2=(0, 1))
 
-    M1 = AffineMapping('M1',2, c1=0.5, c2=0.,a11=1,  a22=1, a12=0, a21=0)
-    M2 = AffineMapping('M2',2, c1=0.5, c2=0, a11=-1, a12=0, a21=0, a22=1)
+    M1 = AffineMapping('M1', 2, c1=0.5, c2=0,
+        a11=1, a12=0,
+        a21=0, a22=1)
+
+    M2 = AffineMapping('M2', 2, c1=0.5, c2=0,
+        a11=-1, a12=0,
+        a21= 0, a22=1)
 
     D1 = M1(A)
     D2 = M2(B)
@@ -190,8 +195,8 @@ def test_poisson_2d_2_patch_dirichlet_4():
     expected_l2_error = 0.0020352296663934746
     expected_h1_error = 0.05679638799164659
 
-    assert ( abs(l2_error - expected_l2_error) < 1e-7 )
-    assert ( abs(h1_error - expected_h1_error) < 1e-7 )
+    assert abs(l2_error - expected_l2_error) < 1e-7
+    assert abs(h1_error - expected_h1_error) < 1e-7
 
 #==============================================================================
 # CLEAN UP SYMPY NAMESPACE
@@ -204,4 +209,3 @@ def teardown_module():
 def teardown_function():
     from sympy.core import cache
     cache.clear_cache()
-
