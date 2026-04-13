85bcec6d4b8f17fd3323b2d31216df75b00bbf75
Alisa Kirkinskaia <alisa.kirkinskaia@tum.de>
2026-04-13 16:45:04 +0200

    clean up check_regular_ring_map

--- psydac/feec/polar/examples/utils_congapol.py
+++ psydac/feec/polar/examples/utils_congapol.py
@@ -68,36 +68,46 @@ def print_map_polar_coeffs(map_discrete):
 
 
 def check_regular_ring_map(map_discrete, verbose=False):
+    """
+    Check the first two radial rings of a 2D spline mapping
+
+    Performs 3 checks:
+    1. that the pole ring (i=0) collapses to a single point
+    2. the 1st ring (i=1) has constant radius
+    3. the 1st ring is uniformly spaced in angle
+    and prints diagnostic information
+
+    """
     n_s = map_discrete._fields[0]._space._spaces[0]._nbasis
     n_theta = map_discrete._fields[0]._space._spaces[1]._nbasis
     assert n_s == map_discrete._fields[1]._space._spaces[0]._nbasis
     assert n_theta == map_discrete._fields[1]._space._spaces[1]._nbasis
-    # deg_s = degree[0]
-    # print('ncells_s = ', n_s-deg_s, ' = ', ncells[0])
+
     if verbose:
         print('n_s = ', n_s)
         print('n_theta = ', n_theta)
         print()
 
+    # read mapping coefficients
     map_0_c = map_discrete._fields[0]._coeffs.toarray()
     map_1_c = map_discrete._fields[1]._coeffs.toarray()
 
-    # print('pole: ', map_0_c[0], map_1_c[0])
+    # shift
     map_0_c -= map_0_c[0]
     map_1_c -= map_1_c[0]
 
-    # print(map_0_c**2 + map_1_c**2)
+    # distance of every coefficient in the pole ring from the first pole coefficient (should be 0)
     radius_pole = np.sqrt(map_0_c[:n_theta] ** 2 + map_1_c[:n_theta] ** 2)
+    # distance from the pole of the first radial ring
     radius_first_ring = np.sqrt(
         map_0_c[n_theta:2 * n_theta] ** 2 + map_1_c[n_theta:2 * n_theta] ** 2
     )
     rho_1 = radius_first_ring[0]
 
+    # compute angles of the 1st ring
     cs = map_0_c[n_theta:2 * n_theta] / rho_1
     sn = map_1_c[n_theta:2 * n_theta] / rho_1
     theta = np.arctan2(sn, cs)
-    # print('cos theta_j ?', map_0_c[4:8]/rho_1)
-    # print('sin theta_j ?', map_1_c[4:8]/rho_1)
 
     delta_theta = np.mod(theta[1:] - theta[:-1], 2 * np.pi)
     if verbose:
@@ -107,11 +117,14 @@ def check_regular_ring_map(map_discrete, verbose=False):
         print()
 
     circle_error = np.linalg.norm(radius_pole[1:] - radius_pole[:-1])
+    #check that 1st ring points are uniformly spaced in angle
     regularity_error = np.linalg.norm(delta_theta[1:] - delta_theta[:-1])
-    regularity_check = abs(circle_error) + abs(regularity_error) < 1e-12
+    first_ring_radius_error = np.linalg.norm(radius_first_ring[1:] - radius_first_ring[:-1])
+    regularity_check = circle_error + regularity_error + first_ring_radius_error < 1e-12
 
     print('CHECK: spline mapping is regular: ', regularity_check)
     if not regularity_check:
-        print(' - circle error on 1st ring:', circle_error)
+        print(' - circle error on pole ring:', circle_error)
         print(' - regularity error on 1st ring:', regularity_error)
+        print(' - first ring radius error:', first_ring_radius_error)
     print()
