de8bce840ac0c971045af6929e53838d982d5d1c
Yaman Güçlü <yaman.guclu@gmail.com>
2026-07-30 23:31:46 +0200

    Add license header to scripts in psydac/feec/polar/examples

--- psydac/feec/polar/examples/maxwell_2d.py
+++ psydac/feec/polar/examples/maxwell_2d.py
@@ -1,3 +1,8 @@
+#---------------------------------------------------------------------------#
+# This file is part of PSYDAC which is released under MIT License. See the  #
+# LICENSE file or go to https://github.com/pyccel/psydac/blob/devel/LICENSE #
+# for full license details.                                                 #
+#---------------------------------------------------------------------------#
 """
 Solve the Transverse Electric Time dependent Maxwell Problem on an analytical disk domain.
 
