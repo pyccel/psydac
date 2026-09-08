53423194f7500070d44127a917d4f05db0838fa4
elmosa <Elena.Moral.Sanchez@ipp.mpg.de>
2026-09-08 16:41:22 +0200

    delete forgotten print

--- psydac/core/bsplines_kernels.py
+++ psydac/core/bsplines_kernels.py
@@ -1103,7 +1103,6 @@ def cell_index_p(breaks: 'float[:]', i_grid: 'float[:]', tol: float, out: 'int[:
             i_cell = (low + high)//2
             it += 1
         if it >= max_it:
-            print(current_index, x, i_cell)
             return -2
         
         # Check were we landed with the binary search
